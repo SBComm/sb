69fe72b2f0d53ac2bb599b63eac59041803f70dd
Josh Palmeri <palmerijosh@gmail.com>
2014-09-12 15:37:40 -0400

    ugadmissions homepage scroll time 0

--- undergraduate-admissions/index.php
+++ undergraduate-admissions/index.php
@@ -56,7 +56,7 @@
 
     		$page_scroll      = true;
 	        $page_scroll_el   = 'logo';     //Options are audience-nav, logo, main-nav, bottom
-	        $page_scroll_time = 1500;
+	        $page_scroll_time = 0;
 	        $page_scroll_mobile = true;
 		?>
 
