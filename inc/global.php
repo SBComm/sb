bf928dceb823433069f8483e846845ecda878530
Josh Palmeri <palmerijosh@gmail.com>
2014-09-12 15:25:54 -0400

    removed proofing message for now

--- inc/global.php
+++ inc/global.php
@@ -3,14 +3,14 @@
 <![endif]-->
 <?php
     if($is_proofing_environment) { ?>
-        <div id="proofing-message">
+        <!--<div id="proofing-message">
             <div class="text content"><i class="fa fa-crosshairs fa-spin"></i></i> <strong>Please Note:</strong> You are viewing the <em>Proofing Site</em> for Stony Brook University. This page is not accessible to the general public. <em>DO NOT share this link externally.</em><span class="close-proofing-message"><i class="fa fa-times"></i>Close</span></div>
-        </div>
+        </div>-->
     <?php }
     if($is_local_environment) { ?>
-        <div id="proofing-message">
+        <!--<div id="proofing-message">
             <div class="text content"><i class="fa fa-crosshairs fa-spin"></i></i> <strong>Please Note:</strong> You are viewing the <em>Local Site</em> for Stony Brook University. This page is not accessible to the general public. <em>DO NOT share this link externally.</em><span class="close-proofing-message"><i class="fa fa-times"></i>Close</span></div>
-        </div>
+        </div>-->
     <?php }
 ?>
 <?php if($fade_in_page) { ?>
