a06c9aa5cfee999690585bc94b9cbdab394dd5a9
Josh Palmeri <palmerijosh@gmail.com>
2014-09-18 10:21:31 -0400

    added social/get-involved

---
+++ inc/content/social/students-get-involved.php
@@ -0,0 +1,15 @@
+<article>
+    <section>
+        <div class="wrapper border-box">
+            <h3 class="serif">Calling All Students!</h3>
+            <p class="sbu-text sbu-text-shadow">Stony Brook is looking to recruit a diverse group of student volunteers to assist in the planning and implementation of social media and initiatives for the 2014-15 school year. Students will gain valuable experience in the planning and creation of social content and marketing. We're looking to create fun, engaging, and meaningful online content that will showcase all that makes Stony Brook shine!</p>
+            <h3 class="serif">Who Can Join?</h3>
+            <p class="sbu-text sbu-text-shadow">The team is open to all Stony Brook undergraduate and graduate students who have skills in content planning and creation for social media, web and video.</p>
+            <h3 class="serif">Member Responsibilities</h3>
+            <p class="sbu-text sbu-text-shadow">Team members will assist in the planning and implementation of Stony Brook web, social media and video marketing. The group will meet twice a month for planning and review (time and day TBD); members will be expected to attend at least one meeting per month. Members will also assist in generating content for social media each week and during official university events  (e.g., Wolfstock, athletic events, Roth Regatta, Strawberry Fest, club &amp; organization activities). Members will be expected to sign a confidentiality and intellectual property agreement and follow both the Code of Conduct Standards as well as follow the standards set forth in Stony Brook’s social media acceptable use policy.</p>
+            <p class="sbu-text sbu-text-shadow">Please submit the application below if you're interested in joining our team! If you have any questions, contact Chris D'Orso, Assistant Director of Enrollment Communications, at @cdorso on Twitter or at <a class="email-link sbu-link" href="mailto:christopher.dorso@stonybrook.edu">christopher.dorso@stonybrook.edu</a>.</p>
+
+            <iframe class="social-form" src="https://docs.google.com/a/stonybrook.edu/forms/d/1UXH1PQjPBpAmW7-8ASRtoPyJDO4N90saR4-7OV8b9Rs/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+        </div>
+    </section>
+</article>
