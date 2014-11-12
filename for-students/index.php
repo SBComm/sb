b91950d4e1b91db7bda0cc0ad9c42c9e82e55d2b
Josh Palmeri <palmerijosh@gmail.com>
2014-11-12 12:12:36 -0500

    added auto scroll on mobile to for students

--- for-students/index.php
+++ for-students/index.php
@@ -34,6 +34,11 @@
 		    $page_footer      = true;
 
 		    $carousel 		  = true;
+
+		    $page_scroll        = true;
+		    $page_scroll_el     = 'logo';     //Options are audience-nav, logo, main-nav, bottom
+		    $page_scroll_time   = 0;      //Time for scroll function in milliseconds
+		    $page_scroll_mobile = true;  //Define whether the scroll-on-load occurs only on mobile. False by default, meaning scroll will happen on desktop and mobile.
 		?>
 
 	<!-- <head> -->
