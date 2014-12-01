becf66eaab765f80c1b5b2ebfa06e6e46ce3ea1c
Josh Palmeri <palmerijosh@gmail.com>
2014-12-01 17:11:39 -0500

    link content updates

--- inc/content/second-level/admissions/admissions.php
+++ inc/content/second-level/admissions/admissions.php
@@ -17,11 +17,12 @@
                 <div class="compare-table-col border-box">
                     <h3 role="button" data-accordion-trigger="true" data-target="compare-links-1" data-screen-size="mobile">Undergraduate</h3>
                     <div class="links hide-accessible-mobile equal-height-col compare-links-1">
-                        <a href="" alt="Admission Criteria"><i class="fa fa-check"></i>Admissions</a>
-                        <a href="" alt="Apply Now"><i class="fa fa-paper-plane"></i>Apply Now</a>
-                        <a href="" alt="Majors &amp; Minors"><i class="fa fa-graduation-cap"></i>Majors &amp; Minors</a>
-                        <a href="" alt="Visit Campus"><i class="fa fa-map-marker"></i>Visit Campus</a>
-                        <a href="" alt="International Students"><i class="fa fa-globe"></i>International Students</a>
+                        <a href="http://stonybrook.edu/undergraduate-admissions/" alt="Admission Criteria"><i class="fa fa-check"></i>Admissions</a>
+                        <a href="http://stonybrook.edu/undergraduate-admissions/apply/" alt="Apply Now"><i class="fa fa-paper-plane"></i>Apply Now</a>
+                        <a href="http://stonybrook.edu/undergraduate-admissions/academics/" alt="Majors &amp; Minors"><i class="fa fa-graduation-cap"></i>Majors &amp; Minors</a>
+                        <a href="http://stonybrook.edu/undergraduate-admissions/cost-and-financial-aid/" alt="Costs &amp; Aid"><i class="fa fa-dollar"></i>Costs &amp; Aid</a>
+                        <a href="http://stonybrook.edu/undergraduate-admissions/visit/" alt="Visit Campus"><i class="fa fa-map-marker"></i>Visit Campus</a>
+                        <a href="http://stonybrook.edu/undergraduate-admissions/apply/international/" alt="International Students"><i class="fa fa-globe"></i>International Students</a>
                     </div>
                     <div class="image desktop">
                         <img src="img/students/red-bag-small.jpg" alt="Student walking from behind" />
@@ -30,11 +31,11 @@
                 <div class="compare-table-col border-box">
                     <h3 role="button" data-accordion-trigger="true" data-target="compare-links-2" data-screen-size="mobile">Graduate &amp; Professional</h3>
                     <div class="links hide-accessible-mobile equal-height-col compare-links-2">
-                        <a href="" alt="The Graduate School"><i class="fa fa-university"></i>The Graduate School</a>
-                        <a href="" alt="Colleges and Schools"><i class="fa fa-university"></i>Colleges and Schools</a>
-                        <a href="" alt="School of Professional Development"><i class="fa fa-university"></i>School of Professional Development</a>
-                        <a href="" alt="Master's Education"><i class="fa fa-university"></i>Master's Education</a>
-                        <a href="" alt="Stony Brook Southhampton"><i class="fa fa-university"></i>Stony Brook Southhampton</a>
+                        <a href="https://www.grad.stonybrook.edu/" alt="The Graduate School"><i class="fa fa-university"></i>The Graduate School</a>
+                        <a href="http://www.stonybrook.edu/sb/departments/" alt="Colleges and Schools" target="_blank"><i class="fa fa-university"></i>Colleges and Schools</a>
+                        <a href="http://www.stonybrook.edu/spd/" alt="School of Professional Development" target="_blank"><i class="fa fa-university"></i>School of Professional Development</a>
+                        <a href="http://www.stonybrook.edu/sb/masters/" alt="Master's Education"><i class="fa fa-university"></i>Master's Education</a>
+                        <a href="http://www.stonybrook.edu/southampton" alt="Stony Brook Southhampton"><i class="fa fa-university"></i>Stony Brook Southhampton</a>
                     </div>
                     <div class="image desktop">
                         <img src="img/students/graduate-professional-small.jpg" alt="Student walking from behind" />
@@ -43,13 +44,13 @@
                 <div class="compare-table-col border-box">
                     <h3 role="button" data-accordion-trigger="true" data-target="compare-links-3" data-screen-size="mobile">Health Sciences</h3>
                     <div class="links hide-accessible-mobile equal-height-col compare-links-3">
-                        <a href="" alt="Admission Criteria"><i class="fa fa-check"></i>Admissions</a>
-                        <a href="" alt="Dental Medicine"><i class="fa fa-plus-square"></i>Dental Medicine</a>
-                        <a href="" alt="Health Technology &amp; Management"><i class="fa fa-stethoscope"></i>Health Technology &amp; Management</a>
-                        <a href="" alt="Medicine"><i class="fa fa-user-md"></i>Medicine</a>
-                        <a href="" alt="Nursing"><i class="fa fa-hospital-o"></i>Nursing</a>
-                        <a href="" alt="Social Welfare"><i class="fa fa-users"></i>Social Welfare</a>
-                        <a href="" alt="Public Health"><i class="fa fa-medkit"></i>Public Health</a>
+                        <a href="http://www.stonybrook.edu/hscstudents/hscadmissions.shtml" alt="Admission Criteria" title="_blank"><i class="fa fa-check"></i>Admissions</a>
+                        <a href="http://dentistry.stonybrookmedicine.edu/" alt="Dental Medicine"><i class="fa fa-plus-square"></i>Dental Medicine</a>
+                        <a href="http://healthtechnology.stonybrookmedicine.edu/" alt="Health Technology &amp; Management"><i class="fa fa-stethoscope"></i>Health Technology &amp; Management</a>
+                        <a href="http://medicine.stonybrookmedicine.edu/" alt="Medicine"><i class="fa fa-user-md"></i>Medicine</a>
+                        <a href="http://nursing.stonybrookmedicine.edu/" alt="Nursing"><i class="fa fa-hospital-o"></i>Nursing</a>
+                        <a href="http://socialwelfare.stonybrookmedicine.edu/" alt="Social Welfare"><i class="fa fa-users"></i>Social Welfare</a>
+                        <a href="http://publichealth.stonybrookmedicine.edu/" alt="Public Health"><i class="fa fa-medkit"></i>Public Health</a>
                     </div>
                     <div class="image desktop">
                         <img src="img/students/health-sciences-small.jpg" alt="Student walking from behind" />
@@ -58,27 +59,27 @@
                 <div class="compare-table-col border-box">
                     <h3 role="button" data-accordion-trigger="true" data-target="compare-links-4" data-screen-size="mobile">Summer, Winter &amp; More</h3>
                     <div class="links hide-accessible-mobile equal-height-col compare-links-4">
-                        <a href="" alt="Summer Session"><i class="fa fa-sun-o"></i>Summer Session</a>
-                        <a href="" alt="Winter Session"><i class="fa fa-asterisk"></i>Winter Session</a>
-                        <a href="" alt="High School Programs"><i class="fa fa-graduation-cap"></i>High School Programs</a>
-                        <a href="" alt="Study Abroad &amp; Exchange"><i class="fa fa-globe"></i>Study Abroad &amp; Exchange</a>
-                        <a href="http://www.stonybrook.edu/nse/" alt="National Student Exchange"><i class="fa fa-map-marker"></i>National Student Exchange</a>
-                        <a href="" alt="Semester by the Sea"><i class="fa fa-info-circle"></i>Semester by the Sea</a>
-                        <a href="" alt="Online Education"><i class="fa fa-laptop"></i>Online Education</a>
-                        <a href="" alt="SUNY Korea"><i class="fa fa-university"></i>SUNY Korea</a>
+                        <a href="http://www.stonybrook.edu/summer/" alt="Summer Session"><i class="fa fa-sun-o"></i>Summer Session</a>
+                        <a href="http://www.stonybrook.edu/winter/" alt="Winter Session"><i class="fa fa-asterisk"></i>Winter Session</a>
+                        <a href="http://www.stonybrook.edu/sb/highschool.shtml" alt="High School Programs" target="_blank"><i class="fa fa-graduation-cap"></i>High School Programs</a>
+                        <a href="http://stonybrook.edu/undergraduate-admissions/academics/study-abroad/" alt="Study Abroad &amp; Exchange"><i class="fa fa-globe"></i>Study Abroad &amp; Exchange</a>
+                        <a href="http://www.stonybrook.edu/nse/" alt="National Student Exchange" target="_blank"><i class="fa fa-map-marker"></i>National Student Exchange</a>
+                        <a href="http://www.stonybrook.edu/southampton/sea/marine_home.html" alt="Semester by the Sea"><i class="fa fa-info-circle"></i>Semester by the Sea</a>
+                        <a href="http://www.stonybrook.edu/commcms/provost/onlineed/" alt="Online Education"><i class="fa fa-laptop"></i>Online Education</a>
+                        <a href="http://www.stonybrook.edu/commcms/provost/onlineed/" alt="SUNY Korea" target="_blank"><i class="fa fa-university"></i>SUNY Korea</a>
                     </div>
                     <div class="image desktop">
                         <img src="img/students/summer-winter-small.jpg" alt="Student walking from behind" />
                     </div>
                 </div>
             </div>
-            <a class="border-box floating-button">Financial Aid</a>
+            <a class="border-box floating-button" href="http://www.stonybrook.edu/commcms/finaid/" target="_blank">Financial Aid</a>
         </div>
     </section>
     <section class="virtual-tour arrow-below arrow-below-white">
         <div class="white-fade-bottom">
             <div class="no-pad-wrapper border-box">
-                <a href="" alt="View Stony Brook's virtual tour.">virtual tour</a>
+                <a href="http://stonybrook.edu/undergraduate-admissions/virtual-tour/" alt="View Stony Brook's virtual tour." target="_blank">virtual tour</a>
             </div>
         </div>
     </section>
