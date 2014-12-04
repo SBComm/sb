9958b57a4240fc9aa542414b644bef99d2ba3a01
Josh Palmeri <palmerijosh@gmail.com>
2014-12-04 11:19:42 -0500

    updated robots.txt to exclude dirs, updated sitemap

--- inc/content/sitemap/sitemap.php
+++ inc/content/sitemap/sitemap.php
@@ -5,7 +5,11 @@
                 <ul class="default-bullets">
                     <li><a href="search/">Search Stony Brook</a></li>
                     <li><a href="social/">Social Hub</a></li>
-                    <li><a href="undergraduate-admissions/">Undergraduate Admissions</a></li>
+                    <li><a href="admissions/">Admissions</a>
+                        <ul>
+                            <li><a href="undergraduate-admissions/">Undergraduate Admissions</a></li>
+                        </ul>
+                    </li>
                     <li><a href="economic-development/">Economic Development</a></li>
                     <li><a href="for-students/">For Students</a></li>
                     <li><a href="jobs/">Jobs</a></li>
