746f967aa7529275bdb7b32eba0264a4da7f8926
Josh Palmeri <palmerijosh@gmail.com>
2015-01-22 10:57:36 -0500

    updated for-students content

--- inc/content/second-level/for-students/for-students.php
+++ inc/content/second-level/for-students/for-students.php
@@ -145,7 +145,7 @@
             <div class="for-students-promo clearfix">
                 <div class="video-promo-wrapper">
                     <div class="videoWrapper">
-                        <iframe width="560" height="349" src="http://www.youtube.com/embed/_7mLV-ici9c?controls=2&showinfo=0&rel=0&hd=1" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="560" height="349" src="http://www.youtube.com/embed/Jxe3jG4phPk?controls=2&showinfo=0&rel=0&hd=1" frameborder="0" allowfullscreen></iframe>
                     </div>
                     <div class="video-caption content">
                         <strong>Featured Video:</strong> Happy Holidays from Stony Brook University!
