663422045a37a85a81a4bfe0f2524abef8b77654
Josh Palmeri <palmerijosh@gmail.com>
2015-01-23 16:02:17 -0500

    updated jobs links

--- inc/content/second-level/jobs/jobs.php
+++ inc/content/second-level/jobs/jobs.php
@@ -9,7 +9,7 @@
             <div class="carousel-wrapper">
                 <div id="the-carousel-1" class="owl-carousel carousel-style-01">
                     <div class="item">
-                        <a href="#">
+                        <a href="http://stonybrook.edu/hr/prospective-employees.shtml#career-opportunities">
                             <div class="carousel-image" data-image="01">
                                 <div class="carousel-text top-left">
                                     <div class="carousel-title">Work for Stony Brook and Discover Your Career Potential</div>
@@ -19,7 +19,7 @@
                         </a>
                     </div>
                     <div class="item">
-                        <a href="#">
+                        <a href="http://stonybrookmedicine.edu/careers">
                             <div class="carousel-image" data-image="02">
                                 <div class="carousel-text top-left">
                                     <div class="carousel-title">Make A Difference In Our Patient&#8217;s Lives</div>
@@ -29,7 +29,7 @@
                         </a>
                     </div>
                     <div class="item">
-                        <a href="#">
+                        <a href="http://www.listateveteranshome.org/employment_opportunities.html">
                             <div class="carousel-image" data-image="03">
                                 <div class="carousel-text top-left">
                                     <div class="carousel-title">Work for Stony Brook and Discover Your Career Potential</div>
@@ -50,7 +50,7 @@
             <div class="dual-tone-cards-1 clearfix">
                 <a href="http://stonybrook.edu/hr/prospective-employees.shtml#career-opportunities" class="card"><span>Jobs @ Stony Brook <br />University</span></a>
                 <a href="http://stonybrookmedicine.edu/careers" class="card"><span>Jobs @ Stony Brook <br />Medicine</span></a>
-                <a href="http://www.listateveteranshome.org/" class="card"><span>Jobs @ Long Island State <br />Veterans Home</span></a>
+                <a href="http://www.listateveteranshome.org/employment_opportunities.html" class="card"><span>Jobs @ Long Island State <br />Veterans Home</span></a>
                 <a href="http://stonybrook.edu/facultyjobs" class="link"><span>Faculty Jobs</span></a>
                 <a href="https://stonybrooku.taleo.net/careersection/2/jobsearch.ftl?lang=en" class="link"><span>All Jobs</span></a>
                 <a href="http://stonybrook.edu/employeejobs" class="link"><span>Current Employees</span></a>
