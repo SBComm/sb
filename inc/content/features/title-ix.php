7ada3b2c75b5f77257d09907325b25e14ba203d0
Josh Palmeri <palmerijosh@gmail.com>
2015-02-04 12:16:48 -0500

    updated title-ix content

--- inc/content/features/title-ix.php
+++ inc/content/features/title-ix.php
@@ -5,11 +5,11 @@
 <article>
     <section class="clearfix inside-content main-inside-content two-col white-background">
         <div class="border-box wrapper clearfix pad-top">
-            <h3>Stony Brook Makes Title IX Strides with Initiatives, Training and Resources</h3>
+            <h3>Stony Brook Makes Title IX Strides with Initiatives, <span class="nowrap">Training and Resources</span></h3>
             <div class="content col-2 col-2-left">
                 <p>When it comes to Title IX, Stony Brook University staff is dedicated to protecting and assisting individuals on campus while developing initiatives and offering resources to keep our campus safe from sexual discrimination and misconduct.</p>
                 <h4>What Exactly is Title IX?</h4>
-                <p><a href="http://www2.ed.gov/about/offices/list/ocr/docs/tix_dis.html" target="_blank">Title IX</a> is a comprehensive federal law passed in 1972 that prohibits sexual discrimination in every educational program or activity that is federally funded.</p>
+                <p><a href="http://www2.ed.gov/about/offices/list/ocr/docs/tix_dis.html" target="_blank" class="external-link">Title IX</a> is a comprehensive federal law passed in 1972 that prohibits sexual discrimination in every educational program or activity that is federally funded.</p>
                 <p>At Stony Brook, the law protects all students, faculty and staff from sexual discrimination, including all forms of sexual misconduct, harassment and violence.</p>
             </div>
             <div class="content col-2 col-2-right">
@@ -17,39 +17,39 @@
             </div>
             <div class="content">
                 <h4>Stony Brook’s Coordinated Staff Effort</h4>
-                <p>With direction from the <a href="http://www.stonybrook.edu/diversity" target="_blank">Office of Diversity and Affirmative Action</a>, key staff from the <a href="http://www.stonybrook.edu/lrweb/" target="_blank">Employee and Labor Relations Department</a>, the <a href="http://studentaffairs.stonybrook.edu/ucs/" target="_blank">Office of University Community Standards</a>, and the <a href="http://studentaffairs.stonybrook.edu/stu/" target="_blank">Office of Student Affairs</a>, which encompasses the <a href="_blank">Center for Prevention and Outreach</a>, are driving a collaborative and ongoing effort to better campus safety and eliminate sexual discrimination at Stony Brook University.</p>
-                <p>Stony Brook offers a <a href="http://www.stonybrook.edu/diversity/titleix/" target="_blank">variety of resources</a> on campus and online for students, faculty and staff to seek support regarding Title IX issues, which range from reporting to recovering from an incident.</p>
+                <p>With direction from the <a href="http://www.stonybrook.edu/diversity" target="_blank" class="external-link">Office of Diversity and Affirmative Action</a>, key staff from the <a href="http://www.stonybrook.edu/lrweb/" target="_blank" class="external-link">Employee and Labor Relations Department</a>, the <a href="http://studentaffairs.stonybrook.edu/ucs/" target="_blank" class="external-link">Office of University Community Standards</a>, and the <a href="http://studentaffairs.stonybrook.edu/stu/" target="_blank" class="external-link">Office of Student Affairs</a>, which encompasses the <a href="http://studentaffairs.stonybrook.edu/cpo/index.shtml" target="_blank" class="external-link">Center for Prevention and Outreach</a>, are driving a collaborative and ongoing effort to better campus safety and eliminate sexual discrimination at Stony Brook University.</p>
+                <p>Stony Brook offers a <a href="http://www.stonybrook.edu/diversity/titleix/" target="_blank" class="external-link">variety of resources</a> on campus and online for students, faculty and staff to seek support regarding Title IX issues, which range from reporting to recovering from an incident.</p>
                 <p>“Stony Brook University has a long-standing commitment to creating and fostering a safe living and learning campus environment for all of its students, faculty and staff,” said Title IX Coordinator Marjolie Leonard. “We have been proactive in implementing and continuously reassessing the modes in which we address Title IX-related allegations and incidents to meet the needs of our ever-evolving campus community. Over the past few years, we have expanded our efforts in ways that not only remain compliant with mandated regulations, but also better serve our community.”</p>
                 <h4>Visit Our Title IX Resource Website</h4>
                 <p>For more detailed information about Title IX or to report an incident involving sexual discrimination, harassment, misconduct or violence, please visit <a href="http://www.stonybrook.edu/diversity/titleix/">Stony Brook University’s Title IX Resource website</a>, presented by the Office of Diversity and Affirmative Action.</p>
             </div>
             <div class="content milestones">
                 <h3>Key Milestones</h3>
-                <p><span>September 2012</span><a href="http://sb.cc.stonybrook.edu/news/medical/ViolenceAgainstWomenPreventionGrant.php" target="_blank">SBU received a $270,000 grant</a> from the Department of Justice: Office on Violence Against Women for campus programs that focus on the reduction of domestic violence, dating violence, sexual assault and stalking.</p>
+                <p><span>September 2012</span><a href="http://sb.cc.stonybrook.edu/news/medical/ViolenceAgainstWomenPreventionGrant.php" target="_blank" class="external-link">SBU received a $270,000 grant</a> from the Department of Justice: Office on Violence Against Women for campus programs that focus on the reduction of domestic violence, dating violence, sexual assault and stalking.</p>
 
-                <p><span>June 2013</span>Title IX Committee formed; key administrators complete <a href="https://atixa.org/" target="_blank">Association of Title IX Administrators (ATIXA)</a> investigation training.</p>
+                <p><span>June 2013</span>Title IX Committee formed; key administrators complete <a href="https://atixa.org/" target="_blank" class="external-link">Association of Title IX Administrators (ATIXA)</a> investigation training.</p>
 
                 <p><span>December 2013</span>Phase 1 of “Hear It, See It, Report It” campaign launched to improve SBU’s Title IX policies and procedures by calling for input from individuals who have reported or experienced sexual misconduct on campus.</p>
 
-                <p><span>February 2014</span>Phase 2 of “Hear It, See It, Report It” campaign distributed a <a href="http://www.stonybrook.edu/diversity/documents/resources.pdf" target="_blank">poster</a> listing Title IX resources and sexual misconduct reporting procedures.</p>
+                <p><span>February 2014</span>Phase 2 of “Hear It, See It, Report It” campaign distributed a <a href="http://www.stonybrook.edu/diversity/documents/resources.pdf" target="_blank" class="external-link">poster</a> listing Title IX resources and sexual misconduct reporting procedures.</p>
 
-                <p><span>March 2014</span><a href="http://studentaffairs.stonybrook.edu/ucs/docs/universitystudentconductcode.pdf" target="_blank">University Student Conduct Code</a> was updated, reflecting finalized complainant procedure that defined sexual harassment, sexual assault and sexual violence.</p>
+                <p><span>March 2014</span><a href="http://studentaffairs.stonybrook.edu/ucs/docs/universitystudentconductcode.pdf" target="_blank" class="external-link">University Student Conduct Code</a> was updated, reflecting finalized complainant procedure that defined sexual harassment, sexual assault and sexual violence.</p>
 
                 <p><span>April 2014</span>SBU sent its fifth annual campus climate survey, Polaris, to all students, gathering input that may relate to Title IX issues.</p>
 
-                <p><span>April 2014</span><a href="http://sb.cc.stonybrook.edu/happenings/oncampus/sbu-programs-recognized-by-department-of-justice-for-dedication-to-sexual-assault-prevention/?=marquee2" target="_blank">The Department of Justice chose SBU</a> to serve as one of the locations for its national campus tour, coinciding with the White House Task Force to Protect Students from Sexual Assault’s release of the report titled “Not Alone.”</p>
+                <p><span>April 2014</span><a href="http://sb.cc.stonybrook.edu/happenings/oncampus/sbu-programs-recognized-by-department-of-justice-for-dedication-to-sexual-assault-prevention/?=marquee2" target="_blank" class="external-link">The Department of Justice chose SBU</a> to serve as one of the locations for its national campus tour, coinciding with the White House Task Force to Protect Students from Sexual Assault’s release of the report titled “Not Alone.”</p>
 
                 <p><span>August 2014</span>More than 700 faculty, staff and students received in-person Title IX training, including all Residence Hall Directors and Resident Assistants from on-campus housing.</p>
 
                 <p><span>September 2014</span>Mandatory online Title IX/Clery Act training for all students launched.</p>
 
-                <p><span>October 2014</span>Stony Brook Council approved <a href="http://studentaffairs.stonybrook.edu/ucs/docs/universitystudentconductcode.pdf" target="_blank">University Student Conduct Code</a> revisions after the SUNY Board of Trustees voted to establish systemwide uniform policies and practices on sexual misconduct.</p>
+                <p><span>October 2014</span>Stony Brook Council approved <a href="http://studentaffairs.stonybrook.edu/ucs/docs/universitystudentconductcode.pdf" target="_blank" class="external-link">University Student Conduct Code</a> revisions after the SUNY Board of Trustees voted to establish systemwide uniform policies and practices on sexual misconduct.</p>
 
-                <p><span>December 2014</span><a href="http://www.stonybrook.edu/diversity/titleix/deputies.html" target="_blank">Twenty-five Deputy Title IX Coordinators</a> were identified across the University to improve access to qualified points of contact when reporting Title IX complaints.</p>
+                <p><span>December 2014</span><a href="http://www.stonybrook.edu/diversity/titleix/deputies.html" target="_blank" class="external-link">Twenty-five Deputy Title IX Coordinators</a> were identified across the University to improve access to qualified points of contact when reporting Title IX complaints.</p>
 
-                <p><span>January 2015</span>ATIXA Executive Director <a href="https://www.ncherm.org/consultants/brett-sokolow/" target="_blank">Brett Sokolow</a> trained all <a href="http://www.stonybrook.edu/diversity/titleix/deputies.html" target="_blank">Deputy Title IX Coordinators</a>.</p>
+                <p><span>January 2015</span>ATIXA Executive Director <a href="https://www.ncherm.org/consultants/brett-sokolow/" target="_blank" class="external-link">Brett Sokolow</a> trained all <a href="http://www.stonybrook.edu/diversity/titleix/deputies.html" target="_blank" class="external-link">Deputy Title IX Coordinators</a>.</p>
 
-                <p><span>January 2015</span>Key administrators and faculty received four-day <a href="http://sb.cc.stonybrook.edu/happenings/oncampus/green-dot-violence-prevention-training-at-sbu-january-20-23/" target="_blank">Green Dot Training</a> for bystander intervention tactics.</p>
+                <p><span>January 2015</span>Key administrators and faculty received four-day <a href="http://sb.cc.stonybrook.edu/happenings/oncampus/green-dot-violence-prevention-training-at-sbu-january-20-23/" target="_blank" class="external-link">Green Dot Training</a> for bystander intervention tactics.</p>
             </div>
             <div class="content">
                 <p><a class="sbu-cta white sbu-cta-2 sbu-cta-1a" href="http://www.stonybrook.edu/diversity/titleix/">View Title IX Website</a>
