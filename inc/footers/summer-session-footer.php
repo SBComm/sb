3c4f9744fd329d0cc43af12ac8a1836884b14436
Josh Palmeri <palmerijosh@gmail.com>
2015-02-23 16:20:35 -0500

    added summer footer

---
+++ inc/footers/summer-session-footer.php
@@ -0,0 +1,31 @@
+                <footer class="site-footer clearfix">
+                    <nav class="wrapper">
+                    	<div class="col col-1 equal-col">
+                    		<a href="http://it.stonybrook.edu/services/solar/" title="Log in to SOLAR">SOLAR</a>
+                    		<a href="http://www.stonybrook.edu/mycloud/" title="Log in to Google Apps for Education">Gmail</a>
+                    		<a href="https://blackboard.stonybrook.edu/" target="blank" title="Log in to Blackboard">Blackboard</a>
+                    		<a href="http://sb.cc.stonybrook.edu/bulletin/current/" title="View the Undergraduate Bulletin">Undergraduate Bulletin</a>
+                    		<a href="http://www.stonybrook.edu/gradbulletin" title="View the Graduate Bulletin">Graduate Bulletin</a>
+                    	</div>
+                    	<div class="col col-2 equal-col">
+                    		<span class="address">
+                    			<span class="serif-italic">Stony Brook University<br />
+                                   Registrar's Office</span>
+								<span class="nowrap">276 Administration Bldg.</span><br />
+                                        <span class="nowrap">Stony Brook, NY 11794-1101</span><br />
+								<a class="address-link small-button" href="https://www.google.com/maps/preview?q=276+Administration+Stony+Brook+University+Stony+Brook,+NY+11794-3364" target="_blank" title="View map of Stony Brook University"><em class="fa fa-map-marker icon"></em>View map</a>
+								<span class="phone nowrap">(631) 632-6175 (Option 1)</span>
+                    		</span>
+                    	</div>
+                    	<div class="col col-3 social">
+                    		<h5>We’re Social</h5>
+                    		<div class="social-links">
+                    			<a class="facebook-blue-bkg" href="https://www.facebook.com/stonybrooku" title="Stony Brook Facebook" target="_blank"><em class="fa fa-facebook"></em></a>
+                    			<a class="youtube-red-bkg" href="https://www.youtube.com/user/sbcomm" title="Stony Brook YouTube" target="_blank"><em class="fa fa-youtube-play"></em></a>
+                    			<a class="twitter-blue-bkg" href="https://twitter.com/stonybrooku" title="Stony Brook Twitter" target="_blank"><em class="fa fa-twitter"></em></a>
+                                   <a class="instagram-blue-bkg" href="https://instagram.com/stonybrooku" title="Stony Brook Instagram" target="_blank"><em class="fa fa-instagram"></em></a>
+                    		</div>
+                    		<a class="small-button" href="http://stonybrook.edu/social" title="Social Media Feed">View Social Media Wall</a>
+                    	</div>
+                    </nav>
+                </footer>
