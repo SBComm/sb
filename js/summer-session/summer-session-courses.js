49ef445cab0be7c56242c42496e3521415d3823e
Josh Palmeri <palmerijosh@gmail.com>
2015-02-26 16:48:07 -0500

    updated summer course js for populating current search params

--- js/summer-session/summer-session-courses.js
+++ js/summer-session/summer-session-courses.js
@@ -28,6 +28,7 @@ $(window).load(function(){
 
 	$(".sticky").sticky({ topSpacing: 0 });
 });
+
 $(window).resize(function(){
 	if(hasFlexbox() || !hasNoFlexbox()) {
 		var ww = $(window).width();
@@ -39,7 +40,15 @@ $(window).resize(function(){
 	}
 });
 
+$('#live-filter-search').keypress(function(event) {
+	if (event.keyCode == 13) {
+		$('body').scrollTo( $('#live-filter-list'), 0, {axis: 'y', offset: -200} );
+	}
+});
 
+$('.clear-filter').click(function() {
+	$('body').scrollTo( $('#live-filter-list'), 0, {axis: 'y', offset: -200} );
+});
 
 $(document).ready(function() {
 	if(hasFlexbox() || !hasNoFlexbox()) {
@@ -55,7 +64,7 @@ $(document).ready(function() {
 
 	$('.nivo-lightbox-summer-form').nivoLightbox({
 		afterShowLightbox: function(lightbox) {
-			var searchParams = ['keywords','department','course_level','location','SBC','DEC'];
+			var searchParams = ['keywords','department','course_level','location','session_code','SBC','DEC'];
 			var searchValues = [];
 			for(var i=0; i<searchParams.length; i++) {
 				if($.urlParam(searchParams[i])!=0 && $.urlParam(searchParams[i])!='' && $.urlParam(searchParams[i])!=null) {
@@ -70,8 +79,15 @@ $(document).ready(function() {
 			$('.course-search-form select[name="department"]').val(searchValues.department);
 			$('.course-search-form select[name="course_level"]').val(searchValues.course_level);
 			$('.course-search-form select[name="location"]').val(searchValues.location);
+			$('.course-search-form select[name="session_code"]').val(searchValues.session_code);
 			$('.course-search-form select[name="SBC"]').val(searchValues.SBC);
 			$('.course-search-form select[name="DEC"]').val(searchValues.DEC);
+
+			$('.nivo-lightbox-overlay .course-search-form select, .nivo-lightbox-overlay .course-search-form input').each(function() {
+				if($(this).val()!='') {
+					$(this).addClass('active-data-filter');
+				}
+			});
 		}
 	});
 
