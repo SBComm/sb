f0cd3f9be34a12bff7cc02ba9c5e96149c7a93ad
Josh Palmeri <palmerijosh@gmail.com>
2015-03-16 12:28:49 -0400

    added summer pre reg forms

---
+++ inc/content/second-level/summer-session/forms/transfer-pre-registration.php
@@ -0,0 +1,10 @@
+<article>
+
+    <section class="clearfix inside-content main-inside-content alt-headers two-col white-background">
+        <div class="border-box wrapper clearfix">
+            <h3>Summer Session Pre-Registration</h3>
+            <iframe class="pre-reg-form" src="https://docs.google.com/forms/d/1XvkTA7KciEtnlNpSdg9AWwvcMmaaAGofGBIhiVSZhuw/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+        </div>
+    </section>
+
+</article>
