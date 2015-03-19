6a975a2d7b0415d0b5af7645f84d650cbf04ab7e
Josh Palmeri <palmerijosh@gmail.com>
2015-03-19 13:27:13 -0400

    added summer links to contact page

--- inc/content/second-level/summer-session/contact/contact.php
+++ inc/content/second-level/summer-session/contact/contact.php
@@ -27,88 +27,88 @@
             <div class="content">
                 <div class="one-half">
                     <p>
-                        <strong>Africana Studies</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/afs/" target="_blank">Africana Studies</a></strong><br />
                         S245 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7470
                     </p>
                     <p>
-                        <strong>Anthropology</strong><br />
+                        <strong><a href="http://www.sunysb.edu/anthro/" target="_blank">Anthropology</a></strong><br />
                         S501 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7620
                     </p>
                     <p>
-                        <strong>Applied Mathematics and Statistics</strong><br />
+                        <strong><a href="http://www.ams.sunysb.edu/" target="_blank">Applied Mathematics and Statistics</a></strong><br />
                         P139B Mathematics Building<br />
                         (631) 632-8370
                     </p>
                     <p>
-                        <strong>Art</strong><br />
+                        <strong><a href="http://www.art.sunysb.edu/" target="_blank">Art</a></strong><br />
                         2225 Staller Center for the Arts<br />
                         (631) 632-7250
                     </p>
                     <p>
-                        <strong>Asian and Asian American Studies</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/aaas/" target="_blank">Asian and Asian American Studies</a></strong><br />
                         N507 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7690
                     </p>
                     <p>
-                        <strong>Astronomy, Department of Physics and Astronomy</strong><br />
+                        <strong><a href="http://www.physics.sunysb.edu/Physics/" target="_blank">Astronomy, Department of Physics and Astronomy</a></strong><br />
                         110 Physics Building<br />
                         (631) 632-8100
                     </p>
                     <p>
-                        <strong>Biology</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/ugbiology/" target="_blank">Biology</a></strong><br />
                         Biology Learning Laboratories Building<br />
                         (631) 632-8530
                     </p>
                     <p>
-                        <strong>Business Management</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/sb/departments/business.shtml" target="_blank">Business Management</a></strong><br />
                         Harriman School for Management<br />
                         102 Harriman Hall<br />
                         (631 632-7180
                     </p>
                     <p>
-                        <strong>Chemistry</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/chemistry/" target="_blank">Chemistry</a></strong><br />
                         104 Chemistry Building<br />
                         (631) 632-7884
                     </p>
                     <p>
-                        <strong>Cinema and Cultural Studies</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/commcms/cat/index.html" target="_blank">Cinema and Cultural Studies</a></strong><br />
                         2048 Humanities Building<br />
                         (631) 632-7460
                     </p>
                     <p>
-                        <strong>Classics</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/commcms/cat/index.html" target="_blank">Classics</a></strong><br />
                         2048 Humanities Building<br />
                         (631) 632-7460
                     </p>
                     <p>
-                        <strong>Computer Science</strong><br />
+                        <strong><a href="http://www.cs.sunysb.edu/" target="_blank">Computer Science</a></strong><br />
                         1440 Computer Science Building<br />
                         (631) 632-8470
                     </p>
                     <p>
-                        <strong>Comparative Literature</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/commcms/cat/index.html" target="_blank">Comparative Literature</a></strong><br />
                         2048 Humanities Building<br />
                         (631) 632-7460
                     </p>
                     <p>
-                        <strong>Economics</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/economics/" target="_blank">Economics</a></strong><br />
                         S601 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7540
                     </p>
                     <p>
-                        <strong>Electrical Engineering</strong><br />
+                        <strong><a href="http://www.ee.sunysb.edu/" target="_blank">Electrical Engineering</a></strong><br />
                         273 Light Engineering Lab Building<br />
                         (631) 632-8400
                     </p>
                     <p>
-                        <strong>English</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/english/index.html" target="_blank">English</a></strong><br />
                         Humanities Bldg<br />
                         (631) 632-7400
                     </p>
                     <p>
-                        <strong>Environmental Studies</strong><br />
+                        <strong><a href="http://www.somas.stonybrook.edu/education/undergrad_ba_ens.html" target="_blank">Environmental Studies</a></strong><br />
                         Marine Sciences Research Center<br />
                         (631) 632-8700
                     </p>
@@ -116,73 +116,73 @@
                         <strong>Foreign Languages &amp; Cultures</strong><br />
                         Arabic: 631/632-7777<br />
                         Chinese 631/632-9013<br />
-                        French: (631) 632-7440<br />
-                        German: (631) 632-7440<br />
-                        Italian: (631) 632-7440<br />
-                        Japanese: 631) 632-7690<br />
-                        Spanish: (631) 632-6935
+                        <a href="http://www.stonybrook.edu/eurolangs/" target="_blank">French</a>: (631) 632-7440<br />
+                        <a href="http://www.stonybrook.edu/eurolangs/" target="_blank">German</a>: (631) 632-7440<br />
+                        <a href="http://www.stonybrook.edu/eurolangs/" target="_blank">Italian</a>: (631) 632-7440<br />
+                        <a href="http://www.stonybrook.edu/commcms/asian/contact.shtml" target="_blank">Japanese</a>: 631) 632-7690<br />
+                        <a href="http://www.stonybrook.edu/hispanic/" target="_blank">Spanish</a>: (631) 632-6935
                     </p>
                 </div>
                 <div class="one-half">
                     <p>
-                        <strong>History</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/history/" target="_blank">History</a></strong><br />
                         S301 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7500
                     </p>
                     <p>
-                        <strong>Humanities</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/commcms/cat/index.html" target="_blank">Humanities</a></strong><br />
                         2048 Humanities Building<br />
                         (631) 632-7460
                     </p>
                     <p>
-                        <strong>Linguistics</strong><br />
+                        <strong><a href="http://semlab1.sbs.sunysb.edu/" target="_blank">Linguistics</a></strong><br />
                         S201 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7777
                     </p>
                     <p>
-                        <strong>Marine Sciences</strong><br />
+                        <strong><a href="http://www.somas.stonybrook.edu/" target="_blank">Marine Sciences</a></strong><br />
                         Endeavor Hall<br />
                         (631) 632-8700
                     </p>
                     <p>
-                        <strong>Mathematics</strong><br />
+                        <strong><a href="http://www.math.stonybrook.edu/html/index.shtml" target="_blank">Mathematics</a></strong><br />
                         P143 Mathematics Building<br />
                         (631) 632-8250
                     </p>
                     <p>
-                        <strong>Music</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/commcms/music/" target="_blank">Music</a></strong><br />
                         3304 Staller Center for the Arts<br />
                         (631) 632-7330
                     </p>
                     <p>
-                        <strong>Nursing </strong><br />
+                        <strong><a href="http://www.stonybrook.edu/sb/departments/nursing.shtml" target="_blank">Nursing </a></strong><br />
                         The School of Nursing<br />
                         Level 2<br />
                         235 Health Sciences Center<br />
                         (631) 444-3200
                     </p>
                     <p>
-                        <strong>Philosophy</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/philosophy/index.html" target="_blank">Philosophy</a></strong><br />
                         213 Harriman Hall<br />
                         (631) 632-7570
                     </p>
                     <p>
-                        <strong>Physical Education</strong><br />
+                        <strong><a href="http://www.hsc.stonybrook.edu/shtm/at_index.cfm" target="_blank">Physical Education</a></strong><br />
                         Sports Complex<br />
                         (631) 632-7200
                     </p>
                     <p>
-                        <strong>Physics &amp; Astronomy</strong><br />
+                        <strong><a href="http://www.physics.sunysb.edu/Physics/" target="_blank">Physics &amp; Astronomy</a></strong><br />
                         110 Physics Building<br />
                         (631) 632-8100
                     </p>
                     <p>
-                        <strong>Political Science</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/polsci/" target="_blank">Political Science</a></strong><br />
                         S701 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7632
                     </p>
                     <p>
-                        <strong>Psychology</strong><br />
+                        <strong><a href="http://www.psychology.stonybrook.edu/psychology/" target="_blank">Psychology</a></strong><br />
                         Psychology B Building, Room 120<br />
                         (631) 632-7800
                     </p>
@@ -192,7 +192,7 @@
                         (631) 632-7460
                     </p>
                     <p>
-                        <strong>School of Professional Development</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/spd/index.html" target="_blank">School of Professional Development</a></strong><br />
                         N201 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7050
                     </p>
@@ -203,27 +203,27 @@
                         (631) 632-7777
                     </p>
                     <p>
-                        <strong>Sociology</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/sociology/" target="_blank">Sociology</a></strong><br />
                         S401 Ward Melville Social and Behavioral Sciences Building<br />
                         (631) 632-7700
                     </p>
                     <p>
-                        <strong>Technology &amp; Society</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/est/" target="_blank">Technology &amp; Society</a></strong><br />
                         347 A Harriman Hall<br />
                         (631) 632-8765
                     </p>
                     <p>
-                        <strong>Theatre Arts</strong><br />
+                        <strong><a href="http://ws.cc.stonybrook.edu/theatrearts/" target="_blank">Theatre Arts</a></strong><br />
                         3046 Stall Center for the Arts<br />
                         (631) 632-7300
                     </p>
                     <p>
-                        <strong>Women’s Studies</strong><br />
+                        <strong><a href="http://www.stonybrook.edu/commcms/cat/index.html" target="_blank">Women’s Studies</a></strong><br />
                         2048 Humanities Building<br />
                         (631) 632-7460
                     </p>
                     <p>
-                        <strong>Writing Program </strong><br />
+                        <strong><a href="http://www.stonybrook.edu/writrhet/" target="_blank">Writing Program </a></strong><br />
                         2005 Humanities Building<br />
                         (631) 632-7405
                     </p>
