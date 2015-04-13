3e848e92dd4997e3b96d1aeb553c86437ce3b6c4
Josh Palmeri <palmerijosh@gmail.com>
2015-04-13 11:09:49 -0400

    removed ap bio link

--- inc/content/second-level/summer-session/audiences/high-school-students.php
+++ inc/content/second-level/summer-session/audiences/high-school-students.php
@@ -72,7 +72,6 @@
                                 <li><a href="http://greenegazette.com/" target="_blank">Robert W. Greene Summer Institute for High School Journalists</a></li>
                                 <li><a href="http://www.stonybrook.edu/cesame/students/MathCamp/mathcamphs.shtml" target="_blank">Mathematics Summer Camp</a></li>
                                 <li><a href="http://www.stonybrook.edu/cesame/students/PhysicsCamp/physummercamp.html" target="_blank">Physics Summer Camp</a></li>
-                                <li><a href="http://www.stonybrook.edu/cesame/students/APBioSummerProgram/apbioprepprogram.html" target="_blank">College Biology/AP Biology Laboratory Preparation</a></li>
                                 <li><a href="http://www.stonybrook.edu/cesame/teachers/Competitions/ProteinModelingChallengeHome.html" target="_blank">Protein Modeling Challenge</a></li>
                                 <li><a href="http://anesthesia.stonybrook.edu/SARAS" target="_blank">SARAS: Science And Research Awareness Series</a></li>
                                 <li><a href="https://llrc.stonybrook.edu/node/1424" target="_blank">StarTalk Program</a></li>
