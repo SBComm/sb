957eb1de4066a588eb58684d06da43b4480a15ba
Josh Palmeri <palmerijosh@gmail.com>
2015-05-04 09:02:58 -0400

    removed hotjar js

--- inc/header.php
+++ inc/header.php
@@ -1,21 +1,4 @@
     <head>
-        <?php if($is_production_environment) { ?>
-            <!--Hotjar-->
-            <script>
-                (function(f,b,g){
-                    var xo=g.prototype.open,xs=g.prototype.send,c;
-                    f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
-                    f._hjSettings={hjid:7729, hjsv:2};
-                    function ls(){f.hj.documentHtml=b.documentElement.outerHTML;c=b.createElement("script");c.async=1;c.src="//static.hotjar.com/c/hotjar-7729.js?sv=2";b.getElementsByTagName("head")[0].appendChild(c);}
-                    if(b.readyState==="interactive"||b.readyState==="complete"||b.readyState==="loaded"){ls();}else{if(b.addEventListener){b.addEventListener("DOMContentLoaded",ls,false);}}
-                    if(!f._hjPlayback && b.addEventListener){
-                        g.prototype.open=function(l,j,m,h,k){this._u=j;xo.call(this,l,j,m,h,k)};
-                        g.prototype.send=function(e){var j=this;function h(){if(j.readyState===4){f.hj("_xhr",j._u,j.status,j.response)}}this.addEventListener("readystatechange",h,false);xs.call(this,e)};
-                    }
-                })(window,document,window.XMLHttpRequest);
-            </script>
-        <?php } ?>
-
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
