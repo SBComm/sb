6effde4771e8df5046de9509a83bc9cda168e5f1
Josh Palmeri <palmerijosh@gmail.com>
2015-06-26 14:25:40 -0400

    updated application message

--- inc/content/second-level/undergraduate-admissions/international/international-application-procedure.php
+++ inc/content/second-level/undergraduate-admissions/international/international-application-procedure.php
@@ -24,9 +24,9 @@
     <section class="clearfix inside-content main-inside-content white-background">
         <div class="border-box wrapper clearfix">
             <h3>How to Apply as an International Applicant</h3>
-            <!--<div class="content application-details alert-message red clearfix">
-                <p><strong>Please note:</strong> Stony Brook is no longer accepting applications for the spring 2015 semester.</p>
-            </div>-->
+            <div class="content application-details alert-message red clearfix">
+                <p><strong>Please note:</strong> Applications are no longer being accepted for the fall 2015 semester. Spring and fall 2016 applications will become available August 1.</p>
+            </div>
             <div class="content application-details clearfix">
                 <p>Follow these steps to apply as an International Applicant:</p>
             </div>
