2cceb0afd19f9dfb3ec28b13a17cc6a850bff358
Josh Palmeri <palmerijosh@gmail.com>
2015-07-01 14:00:22 -0400

    updated dates

--- inc/content/second-level/undergraduate-admissions/deadlines/freshman-deadlines.php
+++ inc/content/second-level/undergraduate-admissions/deadlines/freshman-deadlines.php
@@ -6,13 +6,13 @@
         <th>Notification Schedule</th>
     </tr>
     <tr>
-        <td><strong>Fall 2015</strong></td>
+        <td><strong>Fall 2016</strong></td>
         <td>January 15</td>
         <td>February 1</td>
         <td>By April 1*</td>
     </tr>
     <tr>
-        <td><strong>Spring 2015</strong></td>
+        <td><strong>Spring 2016</strong></td>
         <td>November 1</td>
         <td>November 1</td>
         <td>Beginning Mid-November</td>
