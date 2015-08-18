dd49cdb6ae48610de28852ccac81729c4807e3f7
Josh Palmeri <palmerijosh@gmail.com>
2015-08-18 12:12:08 -0400

    changed to protocol-relative URLs

--- inc/footer-scripts.php
+++ inc/footer-scripts.php
@@ -1,4 +1,4 @@
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
 		<?php includeAsset('js','js/cd-dropdown/jquery.dropdown.js'); ?>
@@ -264,7 +264,7 @@
         <?php } ?>
 
         <?php if($page_type=='second-level' && $second_level=='analytics') { ?>
-            <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+            <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 			<?php includeAsset('js','plugins/spinner/spin.min.js'); ?>
 			<?php includeAsset('js','plugins/analytics/main.js'); ?>
         <? } ?>
