489d2296d90349aa4132b82f06620cdc3bc0a184
Josh Palmeri <palmerijosh@gmail.com>
2015-10-19 15:24:05 -0400

    updated json get

---
+++ shared/get-json.php
@@ -0,0 +1,12 @@
+<?php
+
+	header('content-type: application/json; charset=utf-8');
+
+	$f_url = $_GET["url"];
+
+	$_source = file_get_contents($f_url);
+	$_source = preg_replace("@[\\r|\\n|\\t]+@", "", $_source);
+
+	echo $_GET['callback'] . '('.json_encode($_source).')';
+
+?>
