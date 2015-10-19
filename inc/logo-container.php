dc291ae58321b88b5693d773f11aa2091792dd7a
Josh Palmeri <palmerijosh@gmail.com>
2015-10-19 16:54:42 -0400

    absolute src url for logo

--- inc/logo-container.php
+++ inc/logo-container.php
@@ -2,7 +2,7 @@
 		            <div class="logo-container page-cat-main clearfix border-box">
 		            	<header class="no-pad-wrapper">
 				            <div class="logo-wrapper">
-				            	<div class="logo"><a href="http://www.stonybrook.edu/"><img src="img/stony-brook-university-logo.jpg" /></a></div>
+				            	<div class="logo"><a href="http://www.stonybrook.edu/"><img src="//mobile.cc.stonybrook.edu/sb/img/stony-brook-university-logo.jpg" /></a></div>
 				            	<!--<div class="site-tagline">The State University of <span>New York</span></div>-->
 				            </div>
 			            </header>
@@ -11,7 +11,7 @@
 					<div class="logo-container page-cat-sub clearfix border-box">
 		            	<header class="no-pad-wrapper">
 				            <div class="logo-wrapper">
-				            	<div class="logo"><a href="http://www.stonybrook.edu/"><img src="https://mobile.cc.stonybrook.edu/sb/img/stony-brook-university-logo.jpg" /></a></div>
+				            	<div class="logo"><a href="http://www.stonybrook.edu/"><img src="//mobile.cc.stonybrook.edu/sb/img/stony-brook-university-logo.jpg" /></a></div>
 				            	<h1 class="site-name serif"><a href="<?php echo $og_url; ?>"><?php echo $page_title_sub; ?></a></h1>
 				            </div>
 			            </header>
@@ -20,7 +20,7 @@
 					<div class="logo-container page-cat-sub clearfix border-box">
 		            	<header class="no-pad-wrapper">
 				            <div class="logo-wrapper">
-				            	<div class="logo"><a href="http://www.stonybrook.edu/"><img src="img/stony-brook-university-logo.jpg" /></a></div>
+				            	<div class="logo"><a href="http://www.stonybrook.edu/"><img src="//mobile.cc.stonybrook.edu/sb/img/stony-brook-university-logo.jpg" /></a></div>
 				            	<h1 class="site-name serif"><a href="http://stonybrook.edu/newsroom/"><?php echo $page_title_sub; ?></a></h1>
 				            </div>
 			            </header>
@@ -29,7 +29,7 @@
 					<div class="logo-container page-cat-sub clearfix border-box">
 		            	<header class="no-pad-wrapper">
 				            <div class="logo-wrapper">
-				            	<div class="logo"><a href="http://www.stonybrook.edu/"><img src="img/stony-brook-university-logo.jpg" /></a></div>
+				            	<div class="logo"><a href="http://www.stonybrook.edu/"><img src="//mobile.cc.stonybrook.edu/sb/img/stony-brook-university-logo.jpg" /></a></div>
 				            	<h1 class="site-name serif"><a href="<?php echo $og_url; ?>"><?php echo $page_title_sub; ?></a></h1>
 				            </div>
 			            </header>
