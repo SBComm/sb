b77c2d9b31b7b1ec1e288b795fccef8d0612416f
Josh Palmeri <palmerijosh@gmail.com>
2015-10-19 15:59:27 -0400

    moved scripts to footer file

--- shared/sbu-footer.php
+++ shared/sbu-footer.php
@@ -44,10 +44,65 @@
     </div><!-- .sbu-wrapper -->
 
     <!-- <scripts> -->
-		<?php 
-			$file = "footer-scripts.php";
-			include($path . $file);
+		<?php includeAsset('js','js/cd-dropdown/jquery.dropdown.js'); ?>
+		<script type="text/javascript">
+			$( function() {
+				$( '#cd-dropdown' ).dropdown( {
+					gutter : 0
+				} );
+			});
+		</script>
+
+		<?php
+			includeAsset('js','js/vendor/jquery.easing.1.3.js');
+			includeAsset('js','js/vendor/jquery.easing.compatibility.js');
+			includeAsset('js','js/vendor/jquery.scrollTo.min.js');
+			includeAsset('js','js/vendor/jquery.localScroll.min.js');
+			includeAsset('js','js/vendor/jquery.ba-bbq.js');
+			includeAsset('js','js/vendor/jquery.motio.js');
+			includeAsset('js','js/vendor/jquery.tooltipster.min.js');
+			includeAsset('js','js/plugins.js');
+			includeAsset('js','js/main.js');
+			includeAsset('js','js/vendor/fastclick.js');
 		?>
+
+        <script type="text/javascript">
+		    //<![CDATA[
+		        $(function() {
+				    FastClick.attach(document.body);
+				});
+		    //]]>
+		</script>
+
+		<script type="text/javascript">
+			$(document).ready(function() {
+			    //<![CDATA[
+					$(".site-name").fitText({ minFontSize: '20px', maxFontSize: '27px' });
+					$(".page-title h1").fitText(1.2, { minFontSize: '20px', maxFontSize: '46px' });
+					/*$(".site-carousel h1").fitText(1, { minFontSize: '30px', maxFontSize: '100px' });*/
+			    //]]>
+
+			    $('#totop').on('click',function(event) {
+					<?php if($page_to_top_loc=='default') { ?>
+						var scrollToClass = '0px';
+					<?php } else if($page_to_top_loc=='main-nav') { ?>
+						var scrollToClass = '.main-nav-container';
+					<?php } else { ?>
+						var scrollToClass = '.<?php echo $page_to_top_loc; ?>';
+					<?php } ?>
+
+					scrollToTop(scrollToClass);
+				});
+
+			    /* Initialize Tooltips */
+				$('.tooltip').tooltipster({
+					theme: 'tooltipster-light'
+				});
+			});
+		</script>
+		<?php includeAsset('js','js/vendor/selectivizr-min.js'); ?>
+		<?php includeAsset('js','js/vendor/iframeResizer.js'); /* using non-min version; min produces error on */ ?> 
+
 	<!-- </scripts> -->
 
 	<!-- <googleanalytics> -->
