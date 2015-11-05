014682f553662e4f107e484a12424faa3a057fa4
Josh Palmeri <palmerijosh@gmail.com>
2015-11-05 14:03:40 -0500

    content update

--- inc/content/second-level/undergraduate-admissions/contact-us/chat.php
+++ inc/content/second-level/undergraduate-admissions/contact-us/chat.php
@@ -33,7 +33,8 @@
             <div class="content col-2">
                 <h4>Chat Archives</h4>
                 <ul class="default-bullets">
-                    <li><a class="external-link" href="http://www.stonybrook.edu/ugadmissions/blogs/chats/ceas-10-21-15.htm" target="_blank">Should be titled Engineering and Applied Sciences (10/21/2015)</a></li>
+                    <li><a class="external-link" href="http://www.stonybrook.edu/ugadmissions/blogs/chats/honors-11-4-15.htm" target="_blank">Honors Programs Chat (11/4/15)</a></li>
+                    <li><a class="external-link" href="http://www.stonybrook.edu/ugadmissions/blogs/chats/ceas-10-21-15.htm" target="_blank">Engineering and Applied Sciences (10/21/2015)</a></li>
                     <li><a class="external-link" href="http://www.stonybrook.edu/ugadmissions/blogs/chats/oos-12-11-14.html" target="_blank">Out of State Students (12/9/2014)</a></li>
                     <li><a class="external-link" href="http://www.stonybrook.edu/ugadmissions/blogs/chats/scholars14.htm" target="_blank">University Scholars (3/26/2014)</a></li>
                     <li><a class="external-link" href="http://www.stonybrook.edu/ugadmissions/blogs/chats/ceas-3-12-14.htm" target="_blank">Engineering and Applied Sciences (3/12/2014)</a></li>
--- inc/content/second-level/undergraduate-admissions/visit/campus-tours.php
+++ inc/content/second-level/undergraduate-admissions/visit/campus-tours.php
@@ -39,7 +39,8 @@
                     </a>
                 </div>
                 <h4>Group Tour Requests</h4>
-                <p>Schools and/or organizations planning to bring 20 or more students to campus must <a href="undergraduate-admissions/visit/campus-tours/request-a-guided-tour/"><span class="hide-accessible"> click here to</span> complete our Group Visit Request Form</a>. Organizations wishing to bring less than 20 students should contact Lyle Wind, Director of Campus Tours, at Lyle.Wind@stonybrook.edu.</p>
+                <p>Thank you for your interest in Stony Brook. All group tour dates for the fall semester have been filled and we are no longer accepting requests for the fall.</p>
+                <p>We will begin scheduling group visits for the spring in late January. Schools and/or organizations interested in planning a group visit for the spring should e-mail <a class="email-link" href="mailto:grouptours@stonybrook.edu">grouptours@stonybrook.edu</a> with your request at that time. Please note that we are able to accommodate a maximum of 50 students for group tours.</p>
                 <h4>Interviews</h4>
                 <ul class="default-bullets">
                     <li><strong>On-Campus:</strong> Interviews are not required for applicants. On-campus interviews with admissions counselors are offered on a very limited basis to qualified students with questions about our undergraduate program. Such interviews are for informational purposes only and will have no bearing on the review of an application for admission. All students requesting an interview must call (631) 632-6868 and be pre-screened by an admissions counselor. Due to limited availability during our application reading season, we may be unable to accommodate all requests for interviews.</li>
