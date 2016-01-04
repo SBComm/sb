65f8d428dcbd206a2f6e0bfe55d7b191f7c38a00
Josh Palmeri <palmerijosh@gmail.com>
2016-01-04 13:22:50 -0500

    testing medicine

--- alumni/test.php
+++ alumni/test.php
@@ -112,7 +112,7 @@
 
 		                <!-- <undergrad-admissions> -->
 							<?php 
-								$file = "alumni-test/get-connected/chapters/school-of-medicine.php";
+								$file = "alumni-test/get-connected/chapters/school-of-dental-medicine.php";
 								include($path . $content . $secLv . $file);
 							?>
 						<!-- </undergrad-admissions> -->
