754b334126d69ecba23b9bd14eebdcc8a4aa04a3
Josh Palmeri <palmerijosh@gmail.com>
2016-01-14 14:10:12 -0500

    added proofing and local messages to top of page

--- inc/global.php
+++ inc/global.php
@@ -138,14 +138,14 @@ if($is_proofing_environment) {
 <![endif]-->
 <?php
     if($is_proofing_environment) { ?>
-        <!--<div id="proofing-message">
-            <div class="text content"><i class="fa fa-crosshairs fa-spin"></i></i> <strong>Please Note:</strong> You are viewing the <em>Proofing Site</em> for Stony Brook University. This page is not accessible to the general public. <em>DO NOT share this link externally.</em><span class="close-proofing-message"><i class="fa fa-times"></i>Close</span></div>
-        </div>-->
+        <div id="proofing-message">
+            <div class="text content"><i class="fa fa-info-circle"></i></i> You are viewing the <strong>Proofing</strong> Environment. <em>Do not share this link externally.</em><span class="close-proofing-message"><i class="fa fa-times"></i>Close</span></div>
+        </div>
     <?php }
     if($is_local_environment) { ?>
-        <!--<div id="proofing-message">
-            <div class="text content"><i class="fa fa-crosshairs fa-spin"></i></i> <strong>Please Note:</strong> You are viewing the <em>Local Site</em> for Stony Brook University. This page is not accessible to the general public. <em>DO NOT share this link externally.</em><span class="close-proofing-message"><i class="fa fa-times"></i>Close</span></div>
-        </div>-->
+        <div id="proofing-message">
+            <div class="text content"><i class="fa fa-info-circle"></i></i> You are viewing the <strong>Local</strong> Environment. <em>Do not share this link externally.</em><span class="close-proofing-message"><i class="fa fa-times"></i>Close</span></div>
+        </div>
     <?php }
     if(!$is_local_environment) {
         include ($_SERVER['DOCUMENT_ROOT'] . '/emergency/message.html');
