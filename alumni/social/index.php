423112926776f6b1d64633d93bc8e19ed5cecd89
Josh Palmeri <palmerijosh@gmail.com>
2016-01-25 12:05:55 -0500

    using content/ path for alumni social page

--- alumni/social/index.php
+++ alumni/social/index.php
@@ -114,7 +114,7 @@
 
 		                <?php 
 							$file = "social-alumni.php";
-							include($path . $content . $social . $file);
+							echo($path . $content . $social . $file);
 						?>
 
 		            </div> <!-- .main -->
