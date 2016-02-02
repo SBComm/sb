8c15ce019df47c0d6a76936c581d9f871d5bec47
Josh Palmeri <palmerijosh@gmail.com>
2016-02-02 11:23:00 -0500

    added google analytics

--- test/top-stories-2015/index.php
+++ test/top-stories-2015/index.php
@@ -276,5 +276,14 @@ that most cancers could be attributed to "bad luck."</p>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 		<script src="js/main.js"></script>
 		<script src="js/vendor/fastclick.js"></script>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-30058444-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 	</body>
 </html>
