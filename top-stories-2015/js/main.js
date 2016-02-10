1c4ad1bc8f4269d7cee4fbde4cd7f46cc0e9c251
Josh Palmeri <palmerijosh@gmail.com>
2016-02-10 16:04:28 -0500

    added startsWith polyfill

--- top-stories-2015/js/main.js
+++ top-stories-2015/js/main.js
@@ -14,6 +14,13 @@ $.urlParam = function(name){
     }
 };
 
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
+
 
 var siteURL = '/sb/top-stories-2015/';
 var totalNumStories = 15;
