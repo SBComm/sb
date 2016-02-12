2e398b40400a8b4fc3e93c70d71fdf5a922e6014
Josh Palmeri <palmerijosh@gmail.com>
2016-02-12 11:47:28 -0500

    updated logo placement

--- inc/widgets/alumni/alumni-partners.php
+++ inc/widgets/alumni/alumni-partners.php
@@ -1,12 +1,12 @@
-<section class="white-background">
+<section class="white-background logo-container">
     <div class="wrapper border-box pad-top pad-bottom">
-        <div class="one-third clearfix">
+        <div class="one-half clearfix">
             <a href="https://www.islandfcu.org/Island-at-Stony-Brook.aspx" title="Visit Island Federal at Stony Brook"><img class="icon grayscale-to-color-hover" src="img/logo/partners/logo-island-federal-550.png" alt="Logo of Island Federal Credit Union" /></a>
         </div>
-        <div class="one-third clearfix">
+        <?php /*<div class="one-third clearfix">
             <a href="http://www.petro.com/articles/stony-brook-alumni-special-offers" title="Get special offers from Petro Home Services for Stony Brook Alumni"><img class="icon grayscale-to-color-hover" src="img/logo/partners/logo-petro-mutual-550.png" alt="Logo of Petro Home Services" /></a>
-        </div>
-        <div class="one-third clearfix">
+        </div> */ ?>
+        <div class="one-half clearfix">
             <a href="https://www.libertymutual.com/quotescholarship?MM_webID=0000006515&amp;cmpgncde=2397&amp;keyCode=AIQSMP00&amp;src=aff_6qs_m_0002126_stonybrook" title="Get an insurance quote from Liberty Mutual"><img class="icon grayscale-to-color-hover" src="img/logo/partners/logo-liberty-mutual-550-new.png" alt="Logo of Liberty Mutual" /></a>
         </div>
     </div>
