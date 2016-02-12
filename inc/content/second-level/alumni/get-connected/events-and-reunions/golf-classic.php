33f5b540bd4f46b55ef15a561ca511213c6b8648
Josh Palmeri <palmerijosh@gmail.com>
2016-02-12 16:35:46 -0500

    golf content updates

--- inc/content/second-level/alumni/get-connected/events-and-reunions/golf-classic.php
+++ inc/content/second-level/alumni/get-connected/events-and-reunions/golf-classic.php
@@ -14,20 +14,21 @@
                     </div>
                     <div class="two-third">
                         <h4>Annual Scholarship Golf Classic</h4>
-                        <p>Each June, the Alumni Association hosts its annual Scholarship Golf Classic at one of Long Island's finest courses. Proceeds go to scholarships for deserving Stony Brook University students who need your continued support now more than ever.</p>
-                        <p>Thanks to our supporters, we raised more than $62,000 for student scholarships and awards at the 14th Annual Scholarship Golf Classic on June 22, 2015. View our sponsors, donors and participants in the 2015 Alumni Association Annual Report.</p>
-                        <a class="sbu-outline-button sbu-outline-button--red arrow-after" href="alumni/annual-report-2015" target="_blank">View Annual Report</a>
+                        <h4>Presented By</h4>
+                        <p><a href="http://www.myvrm.com/" target="_blank"><img src="img/alumni/sponsors/myVRM-logo.jpg" alt="MyVRM logo" /></a></p>
+                        <p><br /></p>
+                        
                     </div>
+                </div>
+                <div class="border-box whole clearfix ">
+                    <p>Each June, the Alumni Association hosts its annual Scholarship Golf Classic at one of Long Island's finest courses. Proceeds go to scholarships for deserving Stony Brook University students who need your continued support now more than ever.</p>
+                    <p>Thanks to our supporters, we have distributed more than $347,000 in student scholarships and awards to more than 220 deserving students since the Golf Classic’s inception 15 years ago.</p>
                 </div>
                 <div class="border-box whole clearfix pad-top section-border-top">
                     <div class="two-third left-col-content">
                         <h4>Save the Date!</h4>
-                        <p>The 15th Annual Scholarship Golf Classic will take place <strong>Monday, June 27, 2016</strong>.</p>
+                        <p>Mark your calendars for a great day at our Annual Scholarship Golf Classic on Monday, June 27, 2016.</p>
                         <p>Your participation makes a big impact in contribution to the <a href="http://studentaffairs.stonybrook.edu/alumniawards/index.shtml" target="_blank">Alumni Association Annual Student Awards</a>. Thank you!</p>
-                        <p><br /></p>
-
-                        <h4>Title Sponsor</h4>
-                        <p><img src="img/alumni/sponsors/myVRM-logo.jpg" alt="MyVRM logo" /></p>
                         <p><br /></p>
 
                         <h4>Where &amp; When</h4>
@@ -36,25 +37,48 @@
                         21 Breeze Hill Road, Northport, NY<br />
                         10:30 AM - Registration and Breakfast<br />
                         12 PM - Shotgun </p>
-                        <p><br /></p>
-
-                        <h4>Thank You to Our Sponsors!</h4>
-                        <p><img src="img/alumni/sponsors/CBRE.jpg" alt="MyVRM logo" style="display: inline; float: left; margin-right: 20px; width: 100%; max-width: 400px;" />Cocktail Hour Sponsor<br />
-                        David C. Leviton '84<br />
-                        CBRE</p>
-                        <p><br /></p>
-
-                        <h4>More Information</h4>
-                        <p>For more information about the upcoming Scholarship Golf Classic, please call (631) 632-4466.</p>
                     </div>
                     <div class="one-third">
                         <img class="pad-bottom" src="img/alumni/misc/golf.jpg" alt="Four golfers posing for a shot" />
                     </div>
                 </div>
                 <div class="border-box whole clearfix pad-top section-border-top">
+                    <div class="one-half left-col-content">
+                        <h4>Thank You to Our Sponsors!</h4>
+                        <p><strong>Title Sponsor</strong></p>
+                        <p><a href="http://www.myvrm.com" target="_blank"><img src="img/alumni/sponsors/myVRM-logo.jpg" alt="MyVRM logo" style="display: inline; float: left; margin-right: 20px; width: 100%; max-width: 200px; margin-bottom: 30px;" /></a></p>
+                        <p class="clearfix">&nbsp;<br /></p>
+                        <p class="clearfix">&nbsp;<br /></p>
+                        <p><strong>Gift Sponsor</strong></p>
+                        <p><a href="https://www.islandfcu.org/" target="_blank"><img src="img/logo/partners/logo-island-federal-550.png" alt="Island Federal Credit Union Logo" style="display: inline; float: left; margin-right: 20px; width: 100%; max-width: 200px; margin-bottom: 30px;" /></a></p>
+                        <p class="clearfix">&nbsp;<br /></p>
+                        <p class="clearfix">&nbsp;<br /></p>
+                        <p><strong>Cocktail Hour Sponsor</strong><br />
+                        David C. Leviton '84</p>
+                        <p><a href="http://www.cbre.us/o/longisland/Pages/home.aspx" target="_blank"><img src="img/alumni/sponsors/CBRE.jpg" alt="CBRE logo" style="display: inline; float: left; margin-right: 20px; width: 100%; max-width: 200px; margin-bottom: 30px;" /></a></p>
+                    </div>
+                    <div class="one-half">
+                        <p class="clearfix">&nbsp;<br /></p>
+                        <p class="clearfix">&nbsp;<br /></p>
+                        <p><strong>Fore Sponsor</strong><br />
+                        <h4>Lithos LLC Construction Company</h4></p>
+                        <p class="clearfix">&nbsp;<br /></p>
+                        <p class="clearfix">&nbsp;<br /></p>
+                        <p><strong>Photography Sponsor</strong></p>
+                        <p><a href="https://www.libertymutual.com/web-banner-alumni?MM_webID=0000006515&amp;src=aff_1sw_m_0002126_stonybrook" target="_blank"><img src="img/logo/partners/logo-liberty-mutual-550-new.png" alt="Liberty Mutual logo" style="display: inline; float: left; margin-right: 20px; width: 100%; max-width: 200px; margin-bottom: 30px;" /></a></p>
+                    </div>
+                </div>
+                <div class="border-box whole clearfix pad-top section-border-top">
+                    <h4>More Information</h4>
+                    <p>For more information about the upcoming Scholarship Golf Classic, please call (631) 632-4466.</p>
+                    <p>View our 2015 sponsors, donors and participants in the 2015 Alumni Association Annual Report.</p>
+                    <a class="sbu-outline-button sbu-outline-button--red arrow-after" href="alumni/annual-report-2015" target="_blank">View Annual Report</a>
+                    <p><br /></p>
+
                     <h4>Volunteer</h4>
                     <p>We need your help to make next year’s event a success! <a href="alumni/form/volunteer/">Volunteer today</a> for the 2016 Golf Committee.</p>
                     <a class="sbu-outline-button sbu-outline-button--red arrow-after" href="alumni/form/volunteer">Volunteer</a>
+                    <p><br /></p>
                 </div>
             </div>
         </div>
