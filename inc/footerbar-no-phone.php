60e5e18c69258a1fdcda32b6a8334638c8da3644
Josh Palmeri <palmerijosh@gmail.com>
2016-03-01 16:55:42 -0500

    added drectedit class to copyright

--- inc/footerbar-no-phone.php
+++ inc/footerbar-no-phone.php
@@ -1,7 +1,7 @@
 		        <div class="footer-container clearfix">
                     <footer class="footer-bar">
                         <nav class="wrapper">
-                            <span>&copy; <?php echo date("Y") ?> Stony Brook University</span><span>Stony Brook, NY 11794</span>
+                            <span><span id="directedit" class="copyright">&copy;</span> <?php echo date("Y") ?> Stony Brook University</span><span>Stony Brook, NY 11794</span>
                         </nav>
                     </footer>
                 </div>
--- inc/footerbar.php
+++ inc/footerbar.php
@@ -1,7 +1,7 @@
 		        <div class="footer-container clearfix">
                     <footer class="footer-bar">
                         <nav class="wrapper">
-                            <span><span class="copyright">&copy;</span> <?php echo date("Y") ?> Stony Brook University</span><span>Stony Brook, NY 11794</span><span>(631) 632-6000</span>
+                            <span><span id="directedit" class="copyright">&copy;</span> <?php echo date("Y") ?> Stony Brook University</span><span>Stony Brook, NY 11794</span><span>(631) 632-6000</span>
                         </nav>
                     </footer>
                 </div>
