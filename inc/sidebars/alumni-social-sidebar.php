474700caf201b3d1fc60fb234df8d51bd2d23d3a
Josh Palmeri <palmerijosh@gmail.com>
2016-03-18 15:04:49 -0400

    added flickr

--- inc/sidebars/alumni-social-sidebar.php
+++ inc/sidebars/alumni-social-sidebar.php
@@ -2,6 +2,7 @@
  	<div class="sidebar-wrapper clearfix">
  		<a class="facebook-blue-bkg facebook-blue-hover circle" href="https://www.facebook.com/stonybrookalumni" title="Stony Brook Alumni Association on Facebook!" target="_blank"><span class="label">Facebook</span><em class="fa fa-facebook"></em></a>
  		<a class="twitter-blue-bkg twitter-blue-hover circle" href="https://twitter.com/stonybrookalum" title="Check out Stony Brook Alumni Association tweets!" target="_blank"><span class="label">Twitter</span><em class="fa fa-twitter"></em></a>
+ 		<a class="flickr-pink-bkg flickr-pink-hover circle" href="https://www.flickr.com/photos/sbuadvancement/" title="View Stony Brook Advancement Photos!" target="_blank"><span class="label">Flickr</span><em class="fa fa-flickr"></em></a>
  		<a class="linkedin-blue-bkg linkedin-blue-hover circle" href="https://www.linkedin.com/grps?gid=108294&amp;trk=hb_side_g" title="Join Stony Brook Alumni Association on LinkedIn!" target="_blank"><span class="label">Linked In</span><em class="fa fa-linkedin"></em></a>
  		<a class="white-bkg pinterest-red-hover circle print" href="javascript:window.print()" title="Print this page" target="_blank"><span class="label">print</span><em class="fa fa-print"></em></a>
  	</div>
