bbb88c51659a91f39e59ed2355c5273979802662
Josh Palmeri <palmerijosh@gmail.com>
2016-04-04 12:05:28 -0400

    added mobile interim page but blocking crawlers

---
+++ mobile/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<title>Top 15 Stories of 2015 | Stony Brook University</title>
+		<meta name="description" content="The top 15 stories and news headlines of 2015 from Stony Brook University have made an impact on Long Island, New York and the world." />
+		<meta name="keywords" content="top stories, 2015, Stony Brook, university, news, " />
+		<meta name="author" content="Stony Brook University" />
+
+		<!--See http://davidwalsh.name/facebook-meta-tags-->
+        <meta property="og:title" content="Top 15 Stories of 2015 | Stony Brook University" />
+        <meta property="og:description" content="The top 15 stories and news headlines of 2015 from Stony Brook University have made an impact on Long Island, New York and the world.">
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="http://stonybrook.edu/top15/" />
+        <meta property="og:image" content="http://mobile.cc.stonybrook.edu/sb/top-stories-2015/img/header/stony-brook-top-15-stories-of-2015-masthead-1600.jpg" />
+        <meta property="fb:app_id" content="" />
+    	<!--END Open Graph Meta Tags-->
+
+		 <!--BEGIN Cross-device Favicon code-->
+        <link rel="apple-touch-icon" href="apple-touch-icon-144x144-precomposed.png">
+        <link rel="icon" href="apple-touch-icon.png">
+        <!--[if IE]>
+            <link rel="shortcut icon" href="/favicon.ico">
+        <![endif]-->
+        <meta name="msapplication-TileColor" content="#000000">
+        <meta name="msapplication-TileImage" content="apple-touch-icon-144x144-precomposed.png">
+    	<!--END Cross-device Favicon code-->
+
+        <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/ee71ef3e-a7f1-47dd-8ae1-4ae6bec02b30.css"/>
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/set1.css" />
+		<link rel="stylesheet" type="text/css" href="css/overlay/style5.css" />
+  		<!--<script src="js/modernizr.custom.js"></script>-->
+  		<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<!--[if IE]>
+  			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<script>
+		  window.fbAsyncInit = function() {
+		    FB.init({
+		      appId      : '1673861639552912',
+		      xfbml      : true,
+		      version    : 'v2.5'
+		    });
+		  };
+
+		  (function(d, s, id){
+		     var js, fjs = d.getElementsByTagName(s)[0];
+		     if (d.getElementById(id)) {return;}
+		     js = d.createElement(s); js.id = id;
+		     js.src = "//connect.facebook.net/en_US/sdk.js";
+		     fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+		</script>
+		<div class="container">
+			<header class="site-logo">
+				<a href="http://www.stonybrook.edu/"><img src="//mobile.cc.stonybrook.edu/sb/img/stony-brook-university-logo.jpg" alt="Stony Brook University logo" /></a>
+			</header>
+			<div class="content clearfix">
+				<div class="grid">
+					<a href="http://m.stonybrook.edu/emergency/" class="figure-link effect-sadie">
+						<img src="img/landing/cancer/cancer-1.jpg" alt="Smoke stacks"/>
+						<figcaption>
+							<h2>Campus Alerts</h2>
+						</figcaption>			
+					</a>
+					<a href="http://www.stonybrook.edu/safety" class="figure-link effect-sadie">
+						<img src="//mobile.cc.stonybrook.edu/sb/img/wide-landing/sb-frey-800x245.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Campus Safety</h2>
+						</figcaption>			
+					</a>
+					<a href="http://stonybrook.edu/calendar" class="figure-link effect-sadie">
+						<img src="img/landing/leakey/leakey-1.jpg" alt="img04"/>
+						<figcaption>
+							<h2>Events</h2>
+						</figcaption>			
+					</a>
+					<a href="http://m.stonybrook.edu/map/" class="figure-link effect-sadie">
+						<img src="img/landing/campaign/campaign-1.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Maps</h2>
+						</figcaption>			
+					</a>
+					<a href="http://www.stonybrook.edu/transportation/campusbus/" class="figure-link effect-sadie">
+						<img src="img/landing/baby/baby-2.jpg" alt="img04"/>
+						<figcaption>
+							<h2>Bus Schedules</h2>
+						</figcaption>			
+					</a>
+					<a href="https://blackboard.stonybrook.edu/" class="figure-link effect-sadie">
+						<img src="img/landing/masculinity/masculinity-3.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Blackboard</h2>
+						</figcaption>			
+					</a>
+					<a href="https://psns.cc.stonybrook.edu/psp/he90prods/?cmd=login&amp;languageCd=ENG" class="figure-link effect-sadie">
+						<img src="img/landing/sleep/sleep-3.jpg" alt="img04"/>
+						<figcaption>
+							<h2>Solar</h2>
+						</figcaption>			
+					</a>
+					<a href="http://mobile.cc.stonybrook.edu/sb/admissions/" class="figure-link effect-sadie">
+						<img src="img/landing/computer-science/computer-science-1.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Admissions</h2>
+						</figcaption>			
+					</a>
+					<a href="http://www.goseawolves.org/m/" class="figure-link effect-sadie">
+						<img src="img/landing/he-for-she/he-for-she-1.jpg" alt="img04"/>
+						<figcaption>
+							<h2>Athletics</h2>
+						</figcaption>			
+					</a>
+					<a href="http://stonybrook.edu/happenings/" class="figure-link effect-sadie">
+						<img src="img/stock/16.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Happenings</h2>
+						</figcaption>			
+					</a>
+					<a href="http://stonybrook.edu/newsroom/" class="figure-link effect-sadie">
+						<img src="img/stock/13.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Newsroom</h2>
+						</figcaption>			
+					</a>
+					<a href="http://stonybrook.edu/alumni/" class="figure-link effect-sadie">
+						<img src="img/landing/dinosaur-mammal/dinosaur-mammal.jpg" alt="img04"/>
+						<figcaption>
+							<h2>Alumni</h2>
+						</figcaption>			
+					</a>
+					<a href="http://library.stonybrook.edu/" class="figure-link effect-sadie">
+						<img src="img/landing/billy-joel/billy-joel-1.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Libraries</h2>
+						</figcaption>			
+					</a>
+					<a href="http://www.stonybrook.edu/commcms/campusdining/" class="figure-link effect-sadie">
+						<img src="img/landing/stone-tools/stone-tools-2.jpg" alt="img04"/>
+						<figcaption>
+							<h2>Campus Dining</h2>
+						</figcaption>			
+					</a>
+					<a href="http://stonybrook.edu/search/people/" class="figure-link effect-sadie">
+						<img src="img/landing/jameel-warney/jameel-warney-2.jpg" alt="img05"/>
+						<figcaption>
+							<h2>People Search</h2>
+						</figcaption>			
+					</a>
+					<a href="#" class="figure-link effect-sadie">
+						<img src="img/landing/face-reconstruction/face-reconstruction.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Site Search</h2>
+						</figcaption>			
+					</a>
+					<a href="#" class="figure-link effect-sadie">
+						<img src="img/branding/rays/red-rays-full.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Give to SBU</h2>
+						</figcaption>
+					</a>
+					<a href="#" class="figure-link effect-sadie">
+						<img src="img/branding/rays/red-rays-full.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Campaign</h2>
+						</figcaption>
+					</a>
+					<a href="#" class="figure-link effect-sadie">
+						<img src="img/branding/rays/red-rays-full.jpg" alt="img05"/>
+						<figcaption>
+							<h2>40 Under Forty</h2>
+						</figcaption>
+					</a>
+					<a href="#" class="figure-link effect-sadie">
+						<img src="img/branding/rays/red-rays-full.jpg" alt="img05"/>
+						<figcaption>
+							<h2>SB Experts</h2>
+						</figcaption>
+					</a>
+					<a href="#" class="figure-link effect-sadie">
+						<img src="img/branding/rays/red-rays-full.jpg" alt="img05"/>
+						<figcaption>
+							<h2>Bulletin</h2>
+						</figcaption>
+					</a>
+				</div>
+			</div>
+			<div class="copyright-line">
+				<span class="copyright">&copy;</span> <?php echo date("Y") ?> <a href="http://stonybrook.edu" target="_blank">Stony Brook University</a>
+			</div>
+		</div>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+		<script src="js/vendor/fastclick.js"></script>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-30058444-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+	</body>
+</html>
---
+++ mobile/js/main.js
@@ -0,0 +1,521 @@
+String.prototype.trimToLength = function(m) {
+  return (this.length > m) 
+    ? jQuery.trim(this).substring(0, m).split(" ").slice(0, -1).join(" ") + "..."
+    : this;
+};
+
+$.urlParam = function(name){
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 0;
+    }
+};
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
+
+
+var siteURL = '/sb/top-stories-2015/';
+var totalNumStories = 15;
+
+
+
+function calcHeight(item,header) {
+	var newHeight = (item.height() * 2) - 0.5; //shave off a teeny bit for pixel glitch
+	header.height(newHeight);
+	header.css('max-height',newHeight);
+}
+
+function getItem(windowWidth) {
+	var item;
+	if(windowWidth>800) {
+		item = $('.calc-height');
+	} else {
+		item = $('.calc-height > img');
+	}
+	return item;
+}
+
+function getOpenOverlay() {
+	return $('.overlay-wrapper').not(':hidden');
+}
+
+function getCurrrentStoryId() {
+	var $overlay = getOpenOverlay();
+	return $overlay.attr('data-story-id');
+}
+
+function getStoryOverlayBySlug(slug) {
+	return $('.overlay-wrapper[data-story-slug="'+slug+'"]');
+}
+
+function getOverlayWrapperByID(storyID) {
+	return $('.overlay-wrapper[data-story-id="'+storyID+'"]');
+}
+
+function getStoryIdBySlug(slug) {
+	return getStoryOverlayBySlug(slug).attr('data-story-id');
+}
+
+function getStorySlugByID(storyID) {
+	return getOverlayWrapperByID(storyID).attr('data-story-slug');
+}
+
+function getStoryURLByID(storyID) {
+	return getOverlayWrapperByID(storyID).attr('data-story-url');
+}
+
+function getStoryTriggerByID(storyID) {
+	return $('.trigger-overlay[data-story-id="'+storyID+'"]');
+}
+
+function getTitleByStoryID(storyID) {
+	return $('.overlay-wrapper[data-story-id="'+storyID+'"]').attr('data-story-title');
+}
+
+function getStorySlug() {
+	return $('body').attr('data-story-slug');
+}
+
+function getCurrentStorySlug() {
+	var currentURL = $(location).attr('href');
+	var n = currentURL.lastIndexOf('/');
+	var storySlug = currentURL.substring(n + 1);
+	return storySlug;
+}
+
+function setStorySlug(slug) {
+	return $('body').attr('data-story-slug',slug);
+}
+
+function setContentHeight(ww,$openOverlay) {
+	var $imgBox = $openOverlay.find('.img-box'),
+	    $contentBox = $openOverlay.find('.content-box'),
+	    $buttonBox = $openOverlay.find('.story-footer');
+
+	calcContentHeight(ww,$imgBox,$contentBox,$buttonBox);
+}
+
+function calcContentHeight(ww,$imgBox,$contentBox,$buttonBox) {
+
+	var imgBoxHeightDefault = 120;
+
+	//reset heights
+	$imgBox.height(imgBoxHeightDefault);
+	$contentBox.height('auto');
+	$buttonBox.height('auto');
+
+	//get natural heights
+	var imgBoxHeight = $imgBox.outerHeight();
+	var contentBoxHeight = $contentBox.outerHeight();
+	var buttonBoxHeight = $buttonBox.outerHeight();
+
+	//calculate height of 3 sections combined
+	var totalHeight = imgBoxHeight + contentBoxHeight + buttonBoxHeight;
+	var windowHeight = $(window).height();
+
+	//console.log('totalHeight: '+totalHeight);
+	//console.log('windowHeight: '+windowHeight);
+	//console.log('imgBoxHeight: '+imgBoxHeight);
+	//console.log('contentBoxHeight: '+contentBoxHeight);
+	//console.log('buttonBoxHeight: '+buttonBoxHeight);
+
+	var newContentBoxHeight, difference;
+
+	if(totalHeight<windowHeight && ww<1200) {
+		//do resize
+		difference = windowHeight - totalHeight;
+		newContentBoxHeight = contentBoxHeight + difference;
+		$imgBox.height(imgBoxHeightDefault);
+		$contentBox.height(newContentBoxHeight);
+	} else if (ww>=1200) {
+		
+		totalHeight = contentBoxHeight + buttonBoxHeight;
+		
+		if(totalHeight<windowHeight) {
+			newContentBoxHeight = windowHeight - buttonBoxHeight;
+			$contentBox.height(newContentBoxHeight);
+			$imgBox.height(newContentBoxHeight);
+		} else {
+			$contentBox.height('auto');
+			$imgBox.height($contentBox.outerHeight());
+		}
+		
+	}
+}
+
+function getStoryData($overlayContent) {
+
+	var storyData = {
+		storyTitle : 	$overlayContent.attr('data-story-title'),
+		storyURL : 		$overlayContent.attr('data-story-url'),
+		storyImageURL : $overlayContent.attr('data-story-img-url'),
+		storySlug : 	$overlayContent.attr('data-story-slug'),
+		storyDesc : 	$overlayContent.find('.story').text().trimToLength(150)
+	};
+
+	return storyData;
+}
+
+function setMetaTags(storyData,isDefault) {
+	var newTitle, newDesc, newURL, newImageURL;
+	if(isDefault) {
+		newTitle = 'Top 15 Stories of 2015 | Stony Brook University';
+		newDesc = 'The top 15 stories and news headlines of 2015 from Stony Brook University have made an impact on Long Island, New York and the world.';
+		newURL = 'http://stonybrook.edu/top15/';
+		newImageURL = 'http://mobile.cc.stonybrook.edu/sb/top-stories-2015/img/header/stony-brook-top-15-stories-of-2015-masthead-1600.jpg';
+	} else {
+		newTitle = storyData.storyTitle + ' | Stony Brook University';
+		newDesc = storyData.storyDesc;
+		newURL = storyData.storyURL;
+		newImageURL = storyData.storyImageURL;
+	}
+	$("title").text(newTitle);
+	$("meta[name='description']").attr("content", newDesc);
+
+	$("meta[property='og\\:title']").attr("content", newTitle);
+	$("meta[property='og\\:description']").attr("content", newDesc);
+	$("meta[property='og\\:url']").attr("content", newURL);
+	$("meta[property='og\\:image']").attr("content", newImageURL);
+}
+
+function setTwitterURL($overlayContent) {
+	var tweetHref = 'http://twitter.com/share?text=Check this out! "'+getStoryData($overlayContent).storyTitle+'"&amp;url='+getStoryData($overlayContent).storyURL;
+	$overlayContent.find('.twitter-share-trigger').attr('href',tweetHref);
+}
+
+function pushHistoryState(newTitle,linkHref) {
+	if(Modernizr.history) {
+		history.pushState(null, newTitle, linkHref);
+	}
+}
+
+function popHistoryState() {
+	var currentURL = $(location).attr('href');
+	var currentStorySlug = getCurrentStorySlug();
+	var storyID = getStoryIdBySlug(currentStorySlug);
+	var storyTrigger = getStoryTriggerByID(storyID);
+		//console.log(currentURL);
+		//console.log(currentStorySlug);
+		//console.log(getCurrentStorySlug());
+		//console.log('langing');
+	if(currentURL.match(/top-stories-2015\/$/) || currentURL.match(/top-stories-2015$/)) {
+		//if the overlay is open, close it
+		if($('.overlay').hasClass('open')) {
+			//storyTrigger.trigger('click');
+			//console.log($(this));
+			var $overlayWrapper = getOverlayWrapperByID(getStoryIdBySlug(getStorySlug()));
+			$overlayWrapper.attr('data-push-state','false');
+			//console.log(getStoryIdBySlug(getStorySlug()));
+			//console.log($overlayWrapper);
+			//console.log($overlayWrapper.attr('data-push-state'));
+			getOpenOverlay().find('.button.overlay-close').trigger('click');
+			//updateOverlayContent(storyID);
+		}
+	} else {
+		getStoryOverlayBySlug(currentStorySlug).attr('data-push-state','false');
+		if($('.overlay').hasClass('open')) {
+			$('.overlay-mask').fadeIn(200, function() {
+				updateOverlayContent(storyID);
+				$('.overlay-mask').fadeOut(200);
+			});
+		} else {
+			updateOverlayContent(storyID);
+		}
+	}
+}
+
+function updateOverlayContent(storyID) {
+	//move current overlay content to top
+	getOpenOverlay().scrollTop(0);
+
+	//reset the open overlay-wrapper to be hidden
+	$('.overlay-wrapper').hide();
+
+	$overlayContent = $('.overlay-wrapper[data-story-id="'+storyID+'"]');
+	$overlayContent.show();
+	$('body').addClass('disable-scroll');
+	$('.overlay').addClass('open');
+
+	var ww = $(window).width();
+
+	setContentHeight(ww,getOpenOverlay());
+	storyData = getStoryData($overlayContent);
+	setMetaTags(storyData,false);
+	setTwitterURL($overlayContent);
+
+	//console.log(storyData.storySlug);
+
+	var linkHref = siteURL + storyData.storySlug;
+	if($overlayContent.attr('data-push-state')!='false') {
+		pushHistoryState(storyData.storyTitle,linkHref);
+	} else {
+		$overlayContent.attr('data-push-state','true');
+	}
+
+	$('body').attr('data-story-slug',storyData.storySlug);
+
+	var gaSlug = '/'+storyData.storySlug;
+
+	//send GA pageview
+	ga('set', {
+		page: gaSlug,
+		title: storyData.storyTitle
+	});
+	ga('send', 'pageview');
+
+	//build next/prev text
+	var prevID = parseInt(storyID) - 1;
+	var nextID = parseInt(storyID) + 1;
+
+	if(prevID < 1) {
+		prevID = totalNumStories;
+	}
+	if(nextID > totalNumStories) {
+		nextID = 1;
+	}
+
+	$('.previous-story-trigger').attr('data-story-id',prevID);
+	$('.next-story-trigger').attr('data-story-id',nextID);
+
+	var prevTitle = getTitleByStoryID(prevID);
+	var nextTitle = getTitleByStoryID(nextID);
+
+	$('.previous-story-trigger').attr('data-story-title',prevTitle);
+	$('.next-story-trigger').attr('data-story-title',nextTitle);
+	/*
+	$('.prev-story-preview h4').text(prevTitle);
+	$('.next-story-preview h4').text(nextTitle);
+	*/
+
+	$('.overlay-wrapper').removeClass('preview');
+	$('.story-preview').removeClass('show');
+}
+
+if(Modernizr.history) {
+	window.addEventListener("popstate", function(e) {
+		$(document).ready(function(){
+			popHistoryState();
+		});
+	});
+}
+
+$(document).ready(function(){
+
+	// attach fastclick
+	FastClick.attach(document.body);
+	
+	var $triggerBttn = $('#trigger-overlay-btn'),
+		$triggerBox = $('.trigger-overlay'),
+		$overlay = $('.overlay'),
+		$closeBttn = $('button.overlay-close');
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+	function toggleOverlay(storyID, nextStoryID, pushState) {
+
+		if(typeof getStorySlugByID(storyID) == 'undefined') {
+
+			console.log('Undefined: story summary not found');
+
+		} else {
+
+			var storyData;
+			var $overlayWrapper = getOverlayWrapperByID(storyID);
+
+			if($overlay.hasClass('open')) {
+				$overlay.removeClass('open');
+				$overlay.addClass('close');
+
+				var onEndTransitionFn = function( ev ) {
+					if(support.transitions) {
+						//if( ev.propertyName !== 'visibility' ) return;
+						$(this).unbind( transEndEventName, onEndTransitionFn );
+					}
+
+					$('body').removeClass('disable-scroll');
+					$overlay.removeClass('close');
+				};
+
+				if(support.transitions) {
+					$overlay.on(transEndEventName, onEndTransitionFn);
+				} else {
+					onEndTransitionFn();
+				}
+
+				//console.log(storyID);
+				//console.log($overlayWrapper);
+				//console.log($overlayWrapper.attr('data-push-state'));
+
+				if($overlayWrapper.attr('data-push-state')=='false') {
+					pushState = false;
+				}
+				//console.log(pushState);
+				//console.log();
+
+				//console.log(getCurrentStorySlug());
+				if(pushState===true) {
+					history.pushState(null, null, siteURL);
+				} else {
+					$overlayWrapper.attr('data-push-state','');
+				}
+				setMetaTags(storyData,true);
+
+				if(nextStoryID>0) {
+					toggleOverlay(nextStoryID);
+				}
+			}
+			else if( !$overlay.hasClass('close') ) {
+				updateOverlayContent(storyID);
+			}
+
+		}
+	}
+
+	var transitionEndComplete = false;
+
+	$('.overlay').on('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd', 
+    function() {
+    	if(!transitionEndComplete) {
+			setContentHeight(ww,getOpenOverlay());
+	    	transitionEndComplete = true;
+    	}
+    });
+
+	$triggerBttn.on('click',function() {
+		var storyID = '1';
+		toggleOverlay(storyID, false);
+	});
+
+	$triggerBox.on('click',function() {
+		var storyID = $(this).attr('data-story-id');
+		toggleOverlay(storyID, false);
+	});
+
+
+	$closeBttn.on('click',function() {
+		var storyID = $(this).closest('.overlay-wrapper').attr('data-story-id');
+		//console.log($(this).closest('.overlay-wrapper'));
+		//console.log(storyID);
+		toggleOverlay(storyID, false, true);
+	});
+
+	/* calc header height */
+
+	var header = $('.header-image-wraper');
+	var ww;
+
+	$(window).load(function(){
+		ww = $(window).width();
+		//calcHeight(getItem(ww),header);
+	});
+	$(window).resize(function(){
+		ww = $(window).width();
+		//calcHeight(getItem(ww),header);
+		var $openOverlay = getOpenOverlay();
+		setContentHeight(ww,$openOverlay);
+	});
+
+	/* default story URL */
+
+	var defaultStorySlug = getCurrentStorySlug();
+	var defaultStoryID, defaultStoryTrigger;
+	setStorySlug(defaultStorySlug);
+	//console.log(defaultStorySlug);
+	if( defaultStorySlug && (!defaultStorySlug.startsWith('index.')) && (!defaultStorySlug.startsWith('content?')) && defaultStorySlug!=null && defaultStorySlug!='undefined' ) {
+		defaultStoryID = getStoryIdBySlug(decodeURIComponent(defaultStorySlug));
+		//console.log(defaultStoryID);
+		//toggleOverlay(defaultStoryID, false);
+		updateOverlayContent(defaultStoryID);
+	}
+
+	/* social share */
+
+	$('.facebook-share-trigger').on('click',function() {
+		var storyURL = getStoryData(getOpenOverlay()).storyURL;
+		//console.log(storyURL);
+
+		FB.ui({
+			method: 'share',
+			href: storyURL,
+		}, function(response){
+			var $fbTrigger = getOpenOverlay().find('.facebook-share-trigger');
+			$fbTrigger.addClass('submitted').attr('disabled','disabled');
+		});
+	});
+
+	$('.twitter-share-trigger').on('click',function(e) {
+		window.open(this.href, "Share on Twitter", "width=600, height=600");
+		var $tweetTrigger = getOpenOverlay().find('.twitter-share-trigger');
+		$tweetTrigger.addClass('submitted');
+    	return false;
+	});
+
+	$('.story-read-trigger').on('click',function(e) {
+		if($(window).width()>1800) {
+			window.open(this.href, "Read on SBU", "width=1260, height=1000");
+    		return false;
+		} else if($(window).width()>1200) {
+			window.open(this.href, "Read on SBU", "width=1024, height=850");
+    		return false;
+		} else if($(window).width()>960) {
+			window.open(this.href, "Read on SBU", "width=960, height=750");
+    		return false;
+		} else {
+			return;
+		}
+	});
+
+	$('.story-nav-button').hover(
+		function() {
+			if($('.overlay').attr('disable-preview-hover')!='true') {
+				$('.overlay-wrapper').addClass('preview');
+			}
+		}, function() {
+			$('.overlay-wrapper').removeClass('preview');
+		}
+	);
+	
+	$('.content-box, .img-box').hover(
+		function() {
+			if($('.overlay').attr('disable-preview-hover')=='true') {
+				$('.overlay').attr('disable-preview-hover','false');
+			}
+		}
+	);
+
+	$('.next-story-trigger, .previous-story-trigger').on('click',function() {
+		var storyID = $(this).attr('data-story-id');
+		$('.overlay-mask').fadeIn(200, function() {
+			updateOverlayContent(storyID);
+			$('.overlay-wrapper').removeClass('preview');
+			$('.story-preview').removeClass('show');
+			$('.overlay').attr('disable-preview-hover','true');
+			$('.overlay-mask').fadeOut(200);
+		});
+	});
+
+	$('.img-box, .content-box').on('click',function(e) {
+		if($(this).closest('.overlay-wrapper').hasClass('preview')) {
+			e.preventDefault();
+			$('.overlay-wrapper').removeClass('preview');
+			$('.story-preview').removeClass('show');
+		}
+	});
+
+
+});
