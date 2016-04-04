c7a98066bcc710c2540eca0b9814d70cb02ce653
Josh Palmeri <palmerijosh@gmail.com>
2016-04-04 12:03:33 -0400

    added dev index but blocking crawlers

--- top-stories-2015/index-dev.php
+++ top-stories-2015/index-dev.php
@@ -274,7 +274,7 @@ that most cancers could be attributed to "bad luck."</p>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
-		<script src="js/main.js"></script>
+		<script src="js/main1.js"></script>
 		<script src="js/vendor/fastclick.js"></script>
 
 		<script>
