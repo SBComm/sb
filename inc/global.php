5ff862b4753394d0409b8a121ed9da29c909834d
Josh Palmeri <palmerijosh@gmail.com>
2016-04-08 10:06:53 -0400

    added Google Tag Manager

--- inc/global.php
+++ inc/global.php
@@ -125,13 +125,21 @@ if($is_proofing_environment) {
     }
 
 ?>
-<!--<div class="contentpath_information" style="display: none; visibility: hidden; opacity: 0; font-size: 0;">
+<?php /* <div class="contentpath_information" style="display: none; visibility: hidden; opacity: 0; font-size: 0;">
     Site Status: <?php echo $site_status[$second_level]; ?><br />     
     Content Folder: <?php echo $content; ?><br />     
     Path for Content: <?php echo $pathForContent; ?><br />     
-</div>-->
+</div> */ ?>
 
-
+<?php if($is_production_environment) {
+    /* As per acob Levich <jacob.levich@stonybrook.edu> via Sara Wallace <swallace@butlertill.com>
+    Added 04-07-16 by Josh Palmeri */
+?>
+    <!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-P43R9T"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P43R9T');</script>
+    <!-- End Google Tag Manager -->
+<?php } ?>
 
 
 
