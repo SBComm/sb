06f5d5c2d3ba68000e14c9d08858258ae90c86a4
Josh Palmeri <palmerijosh@gmail.com>
2016-04-14 17:09:22 -0400

    added home and WP integrated news spotlights

--- inc/content/home/index.php
+++ inc/content/home/index.php
@@ -1,19 +1,13 @@
-<section class="hero-sb clearfix">
+<section class="hero-sb animated-preload_background clearfix">
 	<img src="img/home-apr15/Jonathan-Conyers.png" alt="Bronx native Jonathan Conyers" />     
 	<div class="hero-sb_text" >
 		<div>
-			<h1 class="clearfix">Far Beyond New&nbsp;York</h1>
+			<h1 class="clearfix">Far Beyond Maximum Impact</h1>
 			<p class="clearfix">At Stony Brook, Bronx native Jonathan Conyers is forging a future he didn&rsquo;t believe was in reach.</p> 
 			<a class="clearfix video-link nivo-lightbox" href="http://www.youtube.com/watch?v=NBCsNNSOlZ0" data-options="autoplay=1&amp;modestbranding=1&amp;rel=0">Watch the video&nbsp;<i class="fa fa-play-circle"></i></a>
 		</div>  
 	</div>
 </section>
-
-
-
-
-
-
 <div class="optionalCallout clearfix">
 	<p class="clearfix">
 		<a class="clearfix" href="#">
@@ -27,32 +21,41 @@
 		<h3 class="clearfix"><span>Want to change the great big world?</span> <span><strong>So&nbsp;do we.</strong> Join our community of explorers.</span></h3>
 		<ul class="clearfix">
 			<li class="btn"><a href="">Visit us</a></li>
-			<li class="btn"><a href="">Take a virtual tour</a></li>
-			<li class="btn"><a class="apply-form-trigger" href="http://stonybrook.edu/undergraduate-admissions/apply/">Apply</a></li>
+			<li class="btn"><a class="nivo-lightbox" href="https://www.youvisit.com/tour/59968/?pl=v&amp;hover=0" data-lightbox-type="iframe">Take a virtual tour</a></li>
+			<li class="btn"><a href="http://mobile.cc.stonybrook.edu/sb/admissions/">Apply</a></li>
 		</ul>
-		<div class="apply-form apply-form-1 clearfix hide-accessible">
-			<h3 class="clearfix"><span>Take the next step to greatness.</span></h3>
-				<h4 class="clearfix"><span>Which best describes you?</span></h4>
+		<?php
+			/* Apply flow, saving for home 2.0, if that ever comes. ran into issues with how the schools are devided, namely grad vs spd and hsc as its own entity. for now simply linking to the admissions page */
+			/*
 			<ul class="clearfix">
-				<li class="btn"><a href="">Freshman</a></li>
-				<li class="btn"><a href="">Transfer</a></li>
-				<li class="btn"><a href="">International</a></li>
-				<li class="btn"><a href="">Second Degree</a></li>
-				<li class="btn"><a href="">Readmission</a></li>
+				<li class="btn"><a href="">Visit us</a></li>
+				<li class="btn"><a href="">Take a virtual tour</a></li>
+				<li class="btn"><a class="apply-form-trigger" href="http://mobile.cc.stonybrook.edu/sb/admissions/">Apply</a></li>
 			</ul>
-			<!--
-			<div class="apply-form clearfix">
-				<h4 class="clearfix"><span>What level?</span></h4>
+			<div class="apply-form apply-form-1 clearfix hide-accessible">
+				<h3 class="clearfix"><span>Take the next step to greatness.</span></h3>
+					<h4 class="clearfix"><span>Which best describes you?</span></h4>
 				<ul class="clearfix">
-					<li class="btn"><a href="">Freshman</a></li>
-					<li class="btn"><a href="">Sophomore</a></li>
-					<li class="btn"><a href="">Junior</a></li>
-					<li class="btn"><a href="">Senior</a></li>
-					<li class="btn"><a href="">Masters</a></li>
+					<li class="btn"><a href="">Undergraduate</a></li>
+					<li class="btn"><a href="">Graduate</a></li>
+					<li class="btn"><a href="">Health Sciences</a></li>
+					<li class="btn"><a href="">School of Professional Development</a></li>
 				</ul>
+				<!--
+				<div class="apply-form clearfix">
+					<h4 class="clearfix"><span>What level?</span></h4>
+					<ul class="clearfix">
+					<li class="btn"><a href="">Freshman</a></li>
+					<li class="btn"><a href="">Transfer</a></li>
+					<li class="btn"><a href="">International</a></li>
+					<li class="btn"><a href="">Second Degree</a></li>
+					<li class="btn"><a href="">Readmission</a></li>
+					</ul>
+				</div>
+				-->
 			</div>
-			-->
-		</div>
+			*/ 
+		?>
 	</div><!-- /.wpb_wrapper -->	
 </div><!-- /.seawolfStrip -->	
 <div class="boldMoves">
@@ -60,60 +63,24 @@
 		<h2>We&rsquo;re Making Bold Moves</h2>
 		<p class="jumpAwayLink"><a href="">See All Stories&nbsp;<i class="fa fa-long-arrow-right"></i></a></p>
 
-		<div class="boldMovesBox">
-			<a href="javascript:void(0);" class="boldMovesBox-title">
-				<h3>Research Headline Goes Here Lorum Ipsum Sit Research Headline Goes Here Lorum Ipsum Sit</h3>
-			</a>  
-			<div class="boldMovesBox-story">
-				<a href="" class="boldMovesBox-fullStory">Read Story</a>
-			</div>
-			<a href="" class="boldMovesBox-link"><span>All </span>Athletics<span> News <i class="fa fa-long-arrow-right"></i></span></a>
-		</div> 
-		<div class="boldMovesBox">
-			<a href="javascript:void(0);" class="boldMovesBox-title">
-				<h3>Research Headline Goes</h3>
-			</a>  
-			<div class="boldMovesBox-story">
-				<a href="" class="boldMovesBox-fullStory">Read Story</a>
-			</div>
-			<a href="" class="boldMovesBox-link"><span>All </span>Athletics<span> News <i class="fa fa-long-arrow-right"></i></span></a>
-		</div> 
-		<div class="boldMovesBox">
-			<a href="javascript:void(0);" class="boldMovesBox-title">
-				<h3>Research Headline Goes Here Lorum Ipsum Sit</h3>
-			</a>  
-			<div class="boldMovesBox-story">
-				<a href="" class="boldMovesBox-fullStory">Read Story</a>
-			</div>
-			<a href="" class="boldMovesBox-link"><span>All </span>Athletics<span> News <i class="fa fa-long-arrow-right"></i></span></a>
-		</div> 
-		<div class="boldMovesBox">
-			<a href="javascript:void(0);" class="boldMovesBox-title">
-				<h3>Research Headline Goes Here Lorum Ipsum Sit Lorum Ipsum Sit</h3>
-			</a>  
-			<div class="boldMovesBox-story">
-				<a href="" class="boldMovesBox-fullStory">Read Story</a>
-			</div>
-			<a href="" class="boldMovesBox-link"><span>All </span>Athletics<span> News <i class="fa fa-long-arrow-right"></i></span></a>
-		</div> 
-		<div class="boldMovesBox">
-			<a href="javascript:void(0);" class="boldMovesBox-title">
-				<h3>Research Headline Goes</h3>
-			</a>  
-			<div class="boldMovesBox-story">
-				<a href="" class="boldMovesBox-fullStory">Read Story</a>
-			</div>
-			<a href="" class="boldMovesBox-link"><span>All </span>Athletics<span> News <i class="fa fa-long-arrow-right"></i></span></a>
-		</div> 
-		<div class="boldMovesBox">
-			<a href="javascript:void(0);" class="boldMovesBox-title">
-				<h3>Research Headline</h3>
-			</a>  
-			<div class="boldMovesBox-story">
-				<a href="" class="boldMovesBox-fullStory">Read Story</a>
-			</div>
-			<a href="" class="boldMovesBox-link"><span>All </span>Athletics<span> News <i class="fa fa-long-arrow-right"></i></span></a>
-		</div> 
+		<?php
+			/* Outputs the spotlights from Happenings */
+			/* --- TEMPLATE --- */
+			/*
+				<div class="boldMovesBox">
+					<a href="#;" class="boldMovesBox-title">
+						<h3>Research Headline Goes Here Lorum Ipsum Sit Research Headline Goes Here Lorum Ipsum Sit</h3>
+					</a>  
+					<div class="boldMovesBox-story">
+						<a href="" class="boldMovesBox-fullStory">Read Story</a>
+					</div>
+					<a href="#" class="boldMovesBox-link"><span>All </span>Athletics<span> News <i class="fa fa-long-arrow-right"></i></span></a>
+				</div> 
+			 */
+            $file = "scripts/get-homepage-spotlights.php";
+            include($path . $file);
+        ?>
+
 	</div><!-- /.wpb_wrapper -->	      
 </div><!-- /.boldMoves -->
 <div class="whatsComingUp">
@@ -145,13 +112,13 @@
 				</a>
 			</li>  
 		</ul>
-		<p class="jumpAwayLink"><a href="">See All Events&nbsp;<i class="fa fa-long-arrow-right"></i></a></p>
+		<p class="jumpAwayLink"><a href="http://calendar.activedatax.com/stonybrook/">See All Events&nbsp;<i class="fa fa-long-arrow-right"></i></a></p>
 	</div><!-- /.wpb_wrapper -->	
 </div><!-- /.whatsComingUp -->
 <div class="changingWorld clearfix">
 	<div class="wpb_wrapper clearfix"> 
 		<img src="img/home-apr15/Campaign_LockUp_white.png" alt="The Campaign for Stony Brook" />  
 		<div class="pitch"><h3>Through research and discovery, we are changing the world.</h3>
-			<a href="">Join Us</a></div>
+			<a href="http://stonybrook.edu/campaign">Join Us</a></div>
 	</div><!-- /.wpb_wrapper -->	  
 </div><!-- /.changingWorld -->
