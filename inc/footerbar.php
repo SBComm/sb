d733fecde4c51397baa4be05140ef31196f28bff
Josh Palmeri <palmerijosh@gmail.com>
2016-04-18 11:11:39 -0400

    added policy links in footerbar

--- inc/footerbar.php
+++ inc/footerbar.php
@@ -1,7 +1,11 @@
 		        <div class="footer-container clearfix">
                     <footer class="footer-bar">
                         <nav class="wrapper">
-                            <span><span id="directedit" class="copyright">&copy;</span> <?php echo date("Y") ?> Stony Brook University</span><span>Stony Brook, NY 11794</span><span>(631) 632-6000</span>
+                            <span><span id="directedit" class="copyright">&copy;</span> <?php echo date("Y") ?> Stony Brook University</span><span>Stony Brook, NY 11794</span><span class="last">(631) 632-6000</span>
+                            <div class="clearfix policy-links">
+                                <a class="item" href="http://www.stonybrook.edu/sb/access">Accessibility</a>
+                                <a class="item" href="http://www.stonybrook.edu/sb/privacy">Privacy Policy</a>
+                            </div>
                         </nav>
                     </footer>
                 </div>
