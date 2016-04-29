40ec683df4c45288c4910dc5f6fae63afd9f7630
Patrick Hughes <patrick96hughes@gmail.com>
2016-04-29 11:31:38 -0400

    Added accessibility and privacy pages

---
+++ inc/content/second-level/privacy/privacy.php
@@ -0,0 +1,81 @@
+<article class="clearfix second-level-header">
+    <section class="header-title-image-flowers page-title">
+        <div class="wrapper border-box">
+            <h1><a href="about/">About Stony Brook</a></h1>
+        </div>
+    </section>
+    <section class="clearfix inside-content main-inside-content two-col white-background">
+        <div class="border-box wrapper clearfix pad-top">
+
+            <div class="content pad-top">
+                
+                <h3>Commitment to Privacy</h3>
+                <p>At the  Stony Brook University, we are committed to protecting your privacy and  making it easier and more efficient for individuals and businesses to  interact with the University. We recognize that it is critical for  individuals and businesses to be confident that their privacy is  protected when they visit this Web site. You can travel through most of  this Web site without giving us any information about yourself.  Sometimes we do need information to provide services that you request,  and this commitment of privacy explains our online information  practices. </p>
+                <p>Stony Brook University does not collect any <em>personal information </em>about  you unless you provide that information voluntarily by sending an  e-mail, completing an Information Request Form, or completing the  online application. </p>
+                <p>This policy is consistent with the  provisions of the Internet Security and Privacy Act, the Freedom of  Information Law, and the Personal Privacy Protection Law. </p>
+                <h4>Information Collected Automatically When You Visit this Web Site</h4>
+                <p>When you visit this Web site, the University automatically collects and  stores the following information about your visit: </p>
+                <ul class="star-bullets star-bullets-red">
+                      <li>The Internet Protocol address of the computer that accessed this Web site </li>
+                      <li>The type of browser, its version, and the operating system on which that   browser is running </li>
+                      <li>The Web page from which the<em> user</em> accessed the current Web page </li>
+                      <li>The date and time of the <em>user's</em> request </li>
+                      <li>The pages that were visited and the amount of time spent at each page </li>
+                </ul>
+                <p>None of the above-mentioned information is deemed to constitute <em>personal   information</em> by the <a href="http://public.leginfo.state.ny.us/LAWSSEAF.cgi?QUERYTYPE=LAWS+&amp;QUERYDATA=@PLSTT0A2+&amp;LIST=SEA5+&amp;BROWSER=43090560+&amp;TOKEN=50339927+&amp;TARGET=VIEW" target="_blank">Internet Privacy and Security Act</a>.  The information that is collected automatically is used to improve the  content of this Web site and to help Stony Brook University understand  how <em>users</em> are interacting with its Web site. This information  is collected for statistical analysis and to determine what information  is of most and least interest to our<em> users</em>. The information is  not collected for commercial marketing purposes and Stony Brook  University is not authorized to sell or otherwise disclose the  information collected from this Web site for commercial marketing  purposes. </p>
+                <p>The Website may employ, either directly or through trusted third parties, tracking pixels. Tracking pixels are tiny, transparent graphics with a unique identifier, similar in function to cookies, and are used to provide analytical information concerning the user experience as well as to support custom marketing activities for users of the Website. In contrast to cookies, which are stored on a user's computer hard drive, tracking pixels are embedded invisibly on web pages. The Website may use tracking pixels to help us better manage content, such as by improving the user interface or improving our marketing programs. We also may use non-personally identifiable information to create aggregate tracking information reports regarding user demographics, traffic patterns and purchases and may link tracking information with personal information provided by users.</p>
+                <h4>Information Collected When You E-Mail the University or Complete a Transaction </h4>
+                <p>During your visit to this Web site you may send an e-mail to the  University. Your e-mail address and the contents of your message will  be collected. The information collected is not limited to text  characters and may include audio, video, and graphic information  formats included in the message. Your e-mail address and the  information included in your message will be used to respond to you, to  address issues you identify, to improve this Web site, or to forward  your message to the appropriate campus office for response. Your e-mail  address is not collected for commercial purposes and Stony Brook  University is not authorized to sell or otherwise disclose your e-mail  address for commercial purposes.</p>
+                <p>During  your visit to this Web site you may complete a transaction such as an  online application or an Information Request Form. The information  collected by the University, including <em>personal information</em> volunteered by you in completing the transaction, is used by Stony  Brook University and may be disclosed to other parties for those  purposes that may be reasonably ascertained from the nature and terms  of the transaction in which the information was submitted.</p>
+                <p>Stony Brook University does not knowingly collect <em>personal information</em> from children under the age of 13 or create profiles of children under the age   of 13. <em>Users</em> are cautioned, however, that the collection of <em>personal   information</em> submitted in an e-mail will be treated as though it were submitted by  an adult, and may, unless exempted from access by federal or State law,  be subject to public access. </p>
+
+                <h4>Disclosure of Information Collected Through this Web Site </h4>
+                <p>The collection of information through this Web site and the disclosure  of that information are subject to the provisions of the Internet  Security and Privacy Act. Stony Brook University will only collect <em>personal   information</em> through this Web site or disclose <em>personal information</em> collected through this Web site if the <em>user</em> has consented to the   collection or disclosure of such <em>personal information</em>. The voluntary   disclosure of <em>personal information</em> to Stony Brook University by the <em>user</em>,  constitutes consent to the collection and disclosure of the information  by Stony Brook University for the purposes for which the <em>user</em> disclosed the information to Stony Brook University. </p>
+                <p>However, Stony Brook University may collect or disclose <em>personal   information</em> without consent if the collection or disclosure is: (1) necessary to  perform the statutory duties of Stony Brook University, or necessary  for Stony Brook University to operate a program authorized by law, or  authorized by state or federal statute or regulation; (2) made pursuant  to a court order or by law; (3) for the purpose of validating the  identity of the <em>user</em>; or (4) of information to be used solely  for statistical purposes that is in a form that cannot be used to  identify any particular person. </p>
+                <p>Further, the disclosure of information, including <em>personal   information</em>,  collected through this Web site is subject to the provisions of the  Freedom of Information Law and the Personal Privacy Protection Law. Stony   Brook University may disclose <em>personal information</em> to federal or state law enforcement authorities to enforce its rights  against unauthorized access or attempted unauthorized access to Stony  Brook University’s information technology assets. </p>
+                <h4>Retention of Information Collected Through this Web Site</h4>
+                <p>The information collected through this Web site is retained by Stony  Brook University in accordance with the records retention and  disposition requirements of the New York State Arts &amp; Cultural  Affairs Law. 
+                <a class="arrow-after" href="http://www.archives.nysed.gov/records/mr_laws_acal5705.shtml">Learn More</a></p>
+                <p>In general, the Internet services logs of Stony Brook University,  comprising electronic files or automated logs created to monitor access  and use of Agency services provided through this Web site, are retained  for 21 business days and then destroyed. Information concerning these  records retention and disposition schedules may be obtained through the  Internet privacy policy contact listed in this policy.</p>
+                
+                <h4>Access to and Correction of <em>Personal Information</em> Collected Through this Web Site</h4>
+                <p>Any <em>user</em> may submit a request   to Stony Brook University's privacy compliance officer to determine whether <em>personal information</em> pertaining to that <em>user</em> has been collected through Stony Brook University. Any such request  shall be made in writing and must be accompanied by reasonable proof of  identity of the <em>user</em>. Reasonable proof of identity may  include verification of a signature, inclusion of an identifier  generally known only to the<em> user</em>, or similar appropriate   identification. The address of the privacy compliance officer is: </p>
+                <blockquote><p>Office of University Counsel<br>
+                State University of New York at Stony   Brook<br>
+                Administration Room 328<br>
+                Stony Brook, New York 11794</p>
+                </blockquote>
+                <p>The privacy compliance officer shall, within five (5) business days of the   receipt of a proper request, provide access to the <em>personal information</em>;  deny access in writing, explaining the reasons therefor; or acknowledge  the receipt of the request in writing, stating the approximate date  when the request will be granted or denied, which date shall not be  more than thirty (30) days from the date of the acknowledgment.</p>
+                <p>In the event that Stony Brook University has collected <em>personal   information</em> pertaining to a <em>user</em> through this Web site and that   information is to be provided to the <em>user</em> pursuant to the <em>user</em>’s   request, the privacy compliance officer shall inform the <em>user</em> of his or   her right to request that the <em>personal information</em> be amended or   corrected under the procedures set forth in <a href="http://www.tenant.net/Other_Laws/Pubofc/pubofc08.html" target="_blank">section 95 of the Public Officers Law</a>. </p>
+
+                <h4>Confidentiality and Integrity of <em>Personal   Information</em> Collected Through this Web Site</h4>
+                     <p>Stony Brook University   is strongly committed to protecting <em>personal information</em> collected through this Web site against unauthorized access, use, or  disclosure. Consequently, Stony Brook University limits employee access  to <em>personal   information</em> collected through this Web site to  only those employees who need access to the information in the  performance of their official duties. Employees who have access to this  information follow appropriate procedures in connection with any  disclosures of <em>personal information</em>.</p>
+                     <p>In addition,  Stony Brook University has implemented procedures to safeguard the  integrity of its information technology assets, including, but not  limited to, authentication, authorization, monitoring, auditing, and  encryption. These security procedures have been integrated into the  design, implementation, and day-to-day operations of this Web site as  part of our continuing commitment to the security of electronic content  as well as the electronic transmission of information. </p>
+                     <p>For Web site security purposes and to maintain the availability of this Web   site for all <em>users</em>,  Stony Brook University employs software to monitor traffic to identify  unauthorized attempts to upload or change information or otherwise  damage this Web site.</p>
+                     <h4>Cookies</h4>
+                     <p>Cookies are small pieces of information that are stored by the user's browser on the hard drive of the user's computer. This Web site uses cookies for some of its applications.</p>
+                     <h4>Disclaimer</h4>
+                     <p>The information provided in this privacy policy should not be construed as giving business, legal, or other advice, or warranting as failproof, the security of information provided through this Web site.</p>
+                     <p>Contact Information <br>
+                    For questions regarding this Internet privacy policy, please contact our privacy officer via e-mail at <a class="email-link" href="mailto:privacy@stonybrook.edu">privacy@stonybrook.edu</a> or by regular mail at:</p>
+                    <blockquote>
+                        <p>Office of University Counsel<br>
+                         State University of New York at Stony   Brook<br>
+                        Administration Room 328<br>
+                        Stony Brook, New York 11794 </p>
+                    </blockquote>
+                <h4>Definition</h4>
+                <p>The following definitions apply to, and appear in italics, in this policy:</p>
+                <p><em>Personal information:</em> For purposes of this policy, “<em>personal   information</em>”  means any information concerning a natural person which, because of  name, number, symbol, mark, or other identifier, can be used to  identify that natural person. </p>
+                <p><em>User</em> shall have the meaning set forth in subdivision 8 of section 202   of the State Technology Law. </p>
+            </div>
+        </div>
+    </section>
+
+    <?php
+        $campaignColor = "black";
+        $file = "widgets/campaign-banner.php";
+        include($path . $file);
+    ?>
+</article>
+
