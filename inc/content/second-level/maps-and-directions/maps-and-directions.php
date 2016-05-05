c7e5cb7baa03fe64be42bb7a6f5ebf7ae7f1f8ef
Patrick Hughes <patrick96hughes@gmail.com>
2016-05-05 13:45:10 -0400

    Updated the styling for Campus Maps and cleaned up content for map discription.

--- inc/content/second-level/maps-and-directions/maps-and-directions.php
+++ inc/content/second-level/maps-and-directions/maps-and-directions.php
@@ -1,90 +1,120 @@
 <article class="clearfix second-level-header">
     <section class="header-title-image-flowers page-title">
         <div class="wrapper border-box">
-            <h1><a href="provacy/">Accessibility</a></h1>
+            <h1><a href="maps-and-directions/">Maps &amp; Directions</a></h1>
         </div>
     </section>
     <section class="clearfix inside-content main-inside-content two-col white-background">
         <div class="border-box wrapper clearfix pad-top">
             <div class="content pad-top">
-              <div class="maps">  
-
-                <p><h3>Campus Maps</h3>
-                <p><img class="map-image" src="img/maps/googlemap2.gif" alt="Map of Stony Brook University through google maps" /><a href="http://www.stonybrook.edu/sb/googlemap.shtml"></a>
-                <a href="http://www.stonybrook.edu/sb/googlemap.shtml">Google Maps</a> <br>
-                <a href="googlemap.shtml">View map</a> and get directions to campus.
-                <br>
-                <strong>Parking Map</strong> is available at <a href="http://www.stonybrook.edu/parking/parkingmap.shtml">transportation and parking</a></p>
-                
                
-                <br>
 
-
-                
-                <p><img class="map-image" src="img/maps/interactivemap.gif" alt="Interactive Map of Stony Brook Univeersity"><strong><a href="http://www.youvisit.com/tour/map/stonybrook/80175?pl=v" target="_blank">Interactive map</a> </strong> <a href="http://www.youvisit.com/tour/map/stonybrook/80175?pl=v" target="_blank">clickable views</a> of buildings on campus.
-                <a href="http://www.youvisit.com/tour/map/stonybrook/80175?pl=v"></a></p>
-                <br>
-                
-                
-                <br>
-
-
-                <p>
-                <a href="http://www.stonybrook.edu/sb/map/ada/index.html"><img src="img/maps/dexclip.gif" alt="Index with Disaility Access Information"></a>
-                
-                <br>
-                <a href="http://www.stonybrook.edu/sb/map/ada/index.html">Disability Access Information</a>
-                <br>
-                Usable by itself or in conjunction with the printable map, this <a href="http://www.stonybrook.edu/sb/map/ada/index.html">index</a> supplies disability access information for certain campus buildings. This is a work in progress; more information will be added as it becomes available. For more information, contact <a href="http://stonybrook.edu/dss">Disability Support Services.</a>
+                <p class="clearfix"><h3>Campus Maps</h3>
+                <p class="clearfix">
+                    <h4>Google Maps</h4>
+                    <a class="inline-image-link" href="http://www.stonybrook.edu/sb/googlemap.shtml"><img class="map-image" src="img/maps/googlemap2.gif" alt="Map of Stony Brook University through google maps" /></a>
+                    <br />
+                    <a href="googlemap.shtml">View map</a> and get directions to campus.
+                    <br />
+                    
                 </p>
-                <br></p>
+                
+                <br />
+                <p class="clearfix">
+                    <h4>Parking Map</h4>
 
-                </div>
+                    <a class="inline-image-link" href="http://www.stonybrook.edu/parking/parkingmap.shtml"><img class="map-image" src="img/maps/googlemap2.gif" alt="Map of Stony Brook University through google maps" /></a>
+                    <br />
+                    View the parking map available <a href="http://www.stonybrook.edu/parking/parkingmap.shtml">here.</a>
+
+                </p>
+                
+                <p class="clearfix">
+                    <h4>Interactive Map</h4>
+                    <a class="inline-image-link" href="http://www.youvisit.com/tour/map/stonybrook/80175?pl=v"><img class="map-image" src="img/maps/interactivemap.gif" alt="Interactive Map of Stony Brook Univeersity"></a>
+                    <br />
+                    <a href="http://www.youvisit.com/tour/map/stonybrook/80175?pl=v" target="_blank">Click here</a> to view buildings on campus.
+                    <a href="http://www.youvisit.com/tour/map/stonybrook/80175?pl=v"></a>
+                </p>
+                <br />
+
+                <p class="clearfix">
+                    <h4>Disability Access Information</h4>
+                    <a class="inline-image-link"  href="http://www.stonybrook.edu/sb/map/ada/index.html"><img src="img/maps/dexclip.gif" alt="Index with Disaility Access Information"></a>
+                    
+                    <br />
+                    Usable by itself or in conjunction with the printable map, this <a href="http://www.stonybrook.edu/sb/map/ada/index.html">index</a> supplies disability access information for certain campus buildings. This is a work in progress; more information will be added as it becomes available. For more information, contact <a href="http://stonybrook.edu/dss">Disability Support Services.</a>
+                </p>
+                
+                </p>
+                <br />
+
+                <p class="clearfix">
+                <h4>Printable Map</h4>
+                    <a class="inline-image-link" href="http://www.stonybrook.edu/sb/map/newmap.pdf"><img src="img/maps/thumbofmap.jpg" alt="Printable version of Stony Brook University Map"></a>
+                    <br />
+                    Print a <a href="http://www.stonybrook.edu/sb/map/newmap.pdf">PDF version.</a> Map index supplies disability access information.
+
+                    [Download free <a href="http://www.adobe.com/products/acrobat/readstep2.html">Adobe Reader</a> to view PDF.]
+                </p>
+                <br>
+
+
+                <p class="clearfix">
+                    <h4>Stony Brook Southampton Map </h4>
+                    <a class="inline-image-link" href="http://www.stonybrook.edu/sb/map/images/sbshmap.pdf"><img src="img/maps/sbshmapthumb.gif"></a>
+                    <br />
+                    Viewable online in PDF format; printable on 8 1/2 x 11 paper.
+                    [Download free <a href="http://www.adobe.com/products/acrobat/readstep2.html">Adobe Reader</a> to view PDF.]
+                </p>
+                <br />
+                <br />
+                <br />
 
                 
                 <h3>Directions to the University</h3>
                 <h4>Main Campus</h4>
-                <p>Stony Brook University is situated on an 1,039 acre site (see campus maps) on the north shore of Long Island in southeastern New York. We are approximately 60 miles east of New York City.</p>
+                <p class="clearfix">Stony Brook University is situated on an 1,039 acre site (see campus maps) on the north shore of Long Island in southeastern New York. We are approximately 60 miles east of New York City.</p>
 
                 <strong>By Car</strong>
-                <p>From New York City, take the Long Island Expressway (LIE, I-495) eastbound from the Queens Midtown Tunnel in Manhattan or the Throgs Neck Bridge or Whitestone Bridge in Queens to exit 62, and follow Nicolls Road (Route 97) north for 9 miles. The main entrance to the University is on the left. To find the campus via GPS, enter the address "100 Nicolls Road, Stony Brook, NY 11790."</p>
-                <p><a href="https://www.e-zpassny.com/en/about/about.shtml"><img src="img/maps/ezpass.jpg" alt="EZ Pass Logo"> Save Time and Money on NYS Thruway Travel with E-ZPass</a></p>
+                <p class="clearfix">From New York City, take the Long Island Expressway (LIE, I-495) eastbound from the Queens Midtown Tunnel in Manhattan or the Throgs Neck Bridge or Whitestone Bridge in Queens to exit 62, and follow Nicolls Road (Route 97) north for 9 miles. The main entrance to the University is on the left. To find the campus via GPS, enter the address "100 Nicolls Road, Stony Brook, NY 11790."</p>
+                <p class="clearfix"><a href="https://www.e-zpassny.com/en/about/about.shtml"><img src="img/maps/ezpass.jpg" alt="EZ Pass Logo"> Save Time and Money on NYS Thruway Travel with E-ZPass</a></p>
                                <strong>By Train</strong>
-                <p>Take the <a href="http://mta.info/lirr/">Long Island Railroad's</a> Port Jefferson line from Penn Station in Manhattan to Stony Brook. The LIRR station is at the north end of the campus; bus service to the central campus is provided. Trains to and from Penn generally require transfers at Jamaica or Huntington. Hicksville is also a transfer point on some lines. <a href="http://lirr42.mta.info/index.php?error=noRoute">Find schedule and fare information here.</a></p>
+                <p class="clearfix">Take the <a href="http://mta.info/lirr/">Long Island Railroad's</a> Port Jefferson line from Penn Station in Manhattan to Stony Brook. The LIRR station is at the north end of the campus; bus service to the central campus is provided. Trains to and from Penn generally require transfers at Jamaica or Huntington. Hicksville is also a transfer point on some lines. <a href="http://lirr42.mta.info/index.php?error=noRoute">Find schedule and fare information here.</a></p>
 
-                <br>
+                <br />
                 <strong>By Bus</strong>
-                <p>
-                7Bus provides round-trip, seven-days-a-week transport between the Stony Brook campus and the Lower East Side of Manhattan and Rego Park, Queens. For more information, visit the <a href="http://7bus.com/">7Bus website.</a>
+                <p class="clearfix">
+                    7Bus provides round-trip, seven-days-a-week transport between the Stony Brook campus and the Lower East Side of Manhattan and Rego Park, Queens. For more information, visit the <a href="http://7bus.com/">7Bus website.</a>
                 </p>
 
-                <br>
+                <br />
                 <strong>By Ferry</strong>
-                <p>Car ferries cross Long Island Sound at <a href="http://www.88844ferry.com/">Bridgeport, Connecticut to Port Jefferson, Long Island.</a> Take Route 25A west to Nicolls Road, and turn left on Nicolls. The University entrances are the first three intersections you come to.</p>
-                <p>From <a href="https://www.longislandferry.com/">New London, Connecticut to Orient Point, Long Island,</a> take Route 25A west to Nicolls Road and proceed as above.</p>
+                <p class="clearfix">Car ferries cross Long Island Sound at <a href="http://www.88844ferry.com/">Bridgeport,         Connecticut to Port Jefferson, Long Island.</a> Take Route 25A west to Nicolls Road, and turn left on Nicolls. The University entrances are the first three intersections you come to.</p>
+                <p class="clearfix">From <a href="https://www.longislandferry.com/">New London, Connecticut to Orient Point, Long Island,</a> take Route 25A west to Nicolls Road and proceed as above.</p>
 
-                <br>
+                
                 <strong>By Plane</strong>
-                <p><a href="http://www.macarthurairport.com/">Long Island's Islip-MacArthur Airport</a> is 16 miles from the campus and is serviced by direct flights by major airlines and commuter lines.</p>
-                <p><a href="http://a.cms.omniupdate.com/10/">New York City Area Airports</a>(JFK, LaGuardia and Newark)are 50 miles to the west. From Kennedy Airport, you may take the <a href="http://web.mta.info/mta/airtrain.htm">AirTrain</a> to Jamaica Station and transfer to the LIRR for the train to Stony Brook (Port Jefferson line).</p>
-                <p>Driving directions to Stony Brook University from Islip-MacArthur Airport: Exit the airport and turn left onto Veterans Memorial Highway (Route 454 West). Merge onto Sunrise Highway (Route 27) and take the second exit onto Nicolls Road. Drive north for 12 miles. The main entrance to the University is on the left.</p>
-                <br>
+                <p class="clearfix"><a href="http://www.macarthurairport.com/">Long Island's Islip-MacArthur Airport</a> is 16 miles from the campus and is serviced by direct flights by major airlines and commuter lines.</p>
+                <p class="clearfix"><a href="http://a.cms.omniupdate.com/10/">New York City Area Airports</a>(JFK, LaGuardia and Newark)are 50 miles to the west. From Kennedy Airport, you may take the <a href="http://web.mta.info/mta/airtrain.htm">AirTrain</a> to Jamaica Station and transfer to the LIRR for the train to Stony Brook (Port Jefferson line).</p>
+                <p class="clearfix">Driving directions to Stony Brook University from Islip-MacArthur Airport: Exit the airport and turn left onto Veterans Memorial Highway (Route 454 West). Merge onto Sunrise Highway (Route 27) and take the second exit onto Nicolls Road. Drive north for 12 miles. The main entrance to the University is on the left.</p>
+                <br />
 
                 <h4>Southhampton</h4>
-                <p>Stony Brook Southampton is located at 239 Montauk Highway, Southampton, NY 11968.</p>
-                <br>
+                <p class="clearfix">Stony Brook Southampton is located at 239 Montauk Highway, Southampton, NY 11968.</p>
+                
                 <strong>By Car</strong>
-                <p>From New York City and other points west of Southampton, take I-495 (Long Island Expressway) east to Exit 70, then turn right on Route 111 (follow the signs for Route 27 East/Montauk - Sunrise Highway). Take Route 27 (east) and proceed 19 miles to Southampton Campus. Make right on Tuckahoe Road. Go past first entrance on Tuckahoe Road and past the empty field. Turn right at next entrance where you will see a guard booth on your right. On the left, a parking lot is available 
+                <p class="clearfix">From New York City and other points west of Southampton, take I-495 (Long Island Expressway) east to Exit 70, then turn right on Route 111 (follow the signs for Route 27 East/Montauk - Sunrise Highway). Take Route 27 (east) and proceed 19 miles to Southampton Campus. Make right on Tuckahoe Road. Go past first entrance on Tuckahoe Road and past the empty field. Turn right at next entrance where you will see a guard booth on your right. On the left, a parking lot is available 
 
-                <br>
-                <p><strong class="redtext">By Train</strong><br>
+                <br />
+                <p class="clearfix"><strong class="redtext">By Train</strong><br />
                 Take the <a href="http://www.mta.nyc.ny.us/lirr/html/ttn/lirrtt.htm" target="_blank">Long   Island Railroad's</a> Montauk Branch from Jamaica Station in New York City to   Southampton. (Service from Manhattan's Penn Station to Jamaica runs frequently.)   The campus is a short taxi ride from the station. Here is the <a href="http://www.mta.nyc.ny.us/lirr/html/ttn/southamp.htm" target="_blank">schedule for Southampton</a>.</p>
 
                 <h4>Stony Brook Manhattan</h4>
-                <p>Stony Brook Manhattan is now located at 387 Park Avenue South, 3rd floor, between 27th and 28th Streets, with the classroom entrance around the corner at 101 East 27th Street, just beyond The Devon Shops and beneath a narrow Stony Brook Manhattan banner.</p>
+                <p class="clearfix">Stony Brook Manhattan is now located at 387 Park Avenue South, 3rd floor, between 27th and 28th Streets, with the classroom entrance around the corner at 101 East 27th Street, just beyond The Devon Shops and beneath a narrow Stony Brook Manhattan banner.</p>
 
                 <strong>By Train</strong>
-                <p>
+                <p class="clearfix">
                     <ul class="default-bullets">
                          <li>#6 (Lexington Local) @ Park Ave S. &amp; 28th St. </li>
                          <li>N, R (Broadway Local) @ Broadway &amp; 28th St. </li>
@@ -98,7 +128,7 @@
                      </ul>
                 </p>
                 <strong>By Bus</strong>
-                <p>
+                <p class="clearfix">
                     <ul class="default-bullets">
                         <li>#34 (34th St. Crosstown)</li>
                         <li>#23 (23rd St. Crosstown)</li>
@@ -107,7 +137,7 @@
                 </p>
 
                 <h4>Map of Long Island Region</h4>
-                <br>
+                <br />
                 <img src="img/maps/limap.gif" alt="Map of Long Island">
             </div>
         </div>
