42c9760b1933e5155a795c7c294fdb6e11c2cd48
Josh Palmeri <palmerijosh@gmail.com>
2016-05-09 18:32:06 -0400

    no cache

--- inc/scripts/get-magazine-json.php
+++ inc/scripts/get-magazine-json.php
@@ -4,6 +4,7 @@
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $wp_posts_url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
 	$wp_post_data = curl_exec($ch);
 	curl_close($ch);
 
