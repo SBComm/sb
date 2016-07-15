bb88ff68a70ed9efc32a310eaa87fa915d1c1e34
Josh Palmeri <palmerijosh@gmail.com>
2016-07-15 15:57:21 -0400

    replaced mobile.cc

--- shared/calendar/test/get-header-footer.php
+++ shared/calendar/test/get-header-footer.php
@@ -253,7 +253,7 @@
                     <div class="logo-container page-cat-sub clearfix border-box">
                         <header class="no-pad-wrapper">
                             <div class="logo-wrapper">
-                                <div class="logo"><a href="http://www.stonybrook.edu/"><img src="//mobile.cc.stonybrook.edu/sb/img/stony-brook-university-logo.jpg"></a></div>
+                                <div class="logo"><a href="http://www.stonybrook.edu/"><img src="//www.stonybrook.edu/sb/img/stony-brook-university-logo.jpg"></a></div>
                                 <h1 class="site-name serif"><a href="">Calendar</a></h1>
                             </div>
                         </header>
