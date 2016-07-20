514c7eeed6e42294487f9152782bb8760303aa19
Josh Palmeri <palmerijosh@gmail.com>
2016-07-20 13:41:53 -0400

    test output of protocol

--- inc/inc.php
+++ inc/inc.php
@@ -4,7 +4,7 @@
     $inc_loc       = 'inc';   //include folder
     $dir           = dirname(__FILE__);
     $http_protocol = ($_SERVER['HTTP_X_FORWARDED_PROTO']!='') ? $_SERVER['HTTP_X_FORWARDED_PROTO'] : 'http';
-
+    echo("<!--".$http_protocol."-->");
     if($_SERVER['SERVER_NAME'] == 'localhost') {
         $root          = $_SERVER['DOCUMENT_ROOT'];
         $site          = 'sb'; //site folder inside the root dir
