4302b5f26b49f127a25a21a699f852cee3bb4e4e
Josh Palmeri <palmerijosh@gmail.com>
2016-09-22 11:05:22 -0400

    added visit link

--- inc/content/second-level/undergraduate-admissions/visit/visit-nav.php
+++ inc/content/second-level/undergraduate-admissions/visit/visit-nav.php
@@ -8,5 +8,6 @@
 	<a <?php if($selected_sub_nav==1) { ?>class="selected" <?php } ?>href="undergraduate-admissions/visit/campus-tours">Campus Tours</a>
 	<a <?php if($selected_sub_nav==2) { ?>class="selected" <?php } ?>href="undergraduate-admissions/visit/directions-and-parking">Directions &amp; Parking</a>
 	<a <?php if($selected_sub_nav==3) { ?>class="selected" <?php } ?>href="undergraduate-admissions/visit/campus-maps">Campus Maps</a>
+	<a class="selected" href="visit#staying-here" target="_blank">Nearby Accommodations</a>
 	<a <?php if($selected_sub_nav==4) { ?>class="selected" <?php } ?>href="undergraduate-admissions/visit/coming-to-you">Coming To You</a>
 </nav>
