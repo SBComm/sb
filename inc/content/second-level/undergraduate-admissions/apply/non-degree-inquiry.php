49815ace3faa43fdc351eabdbaa33db302f6ad8a
Josh Palmeri <palmerijosh@gmail.com>
2016-09-23 15:02:16 -0400

    added form for stefan

---
+++ inc/content/second-level/undergraduate-admissions/apply/non-degree-inquiry.php
@@ -0,0 +1,17 @@
+<article>
+    <section class="clearfix inside-content main-inside-content visit-content two-col white-background">
+        <div class="border-box wrapper clearfix">
+            <div class="content">
+                 <iframe class="admissions-group-form" src="https://docs.google.com/forms/d/e/1FAIpQLSfSxaSJqCOly9pyPdApG-p5g9RDhcsp_YibYM4oH0fzFFIbSQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            </div>
+        </div>
+    </section>
+
+    <!-- <social-sidebar> -->
+        <?php if($social_sidebar!='') {
+            $file = "sidebars/".$social_sidebar."-social-sidebar.php";
+            include($path . $file);
+        } ?>
+    <!-- </social-sidebar> -->
+
+</article>
