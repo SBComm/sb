a81ccca8ac234d322c5620b165cde108e9319120
Josh Palmeri <palmerijosh@gmail.com>
2016-10-04 14:11:55 -0400

    made image paths absolute for template sharing

--- inc/widgets/campaign-banner.php
+++ inc/widgets/campaign-banner.php
@@ -1,8 +1,8 @@
 <section class="campaign-banner campaign-banner--on-<?php echo($campaignColor); ?> fixed-background clearfix">
     <div class="campaign-banner__wrapper border-box clearfix">
         <span class="campaign-banner__logo-text hide-accessible">The Campaign for Stony Brook</span>
-        <img class="campaign-banner__logo-image campaign-banner__logo-image--desktop" src="/img/branding/logo/the-campaign-for-stony-brook--on-<?php echo($campaignColor); ?>--landscape.png" alt="The Campaign for Stony Brook logo" />
-        <img class="campaign-banner__logo-image campaign-banner__logo-image--mobile" src="/img/branding/logo/the-campaign-for-stony-brook--on-<?php echo($campaignColor); ?>--portrait.png" alt="The Campaign for Stony Brook logo" />
+        <img class="campaign-banner__logo-image campaign-banner__logo-image--desktop" src="https://www.stonybrook.edu/far-beyond/img/branding/logo/the-campaign-for-stony-brook--on-<?php echo($campaignColor); ?>--landscape.png" alt="The Campaign for Stony Brook logo" />
+        <img class="campaign-banner__logo-image campaign-banner__logo-image--mobile" src="https://www.stonybrook.edu/far-beyond/img/branding/logo/the-campaign-for-stony-brook--on-<?php echo($campaignColor); ?>--portrait.png" alt="The Campaign for Stony Brook logo" />
         <span class="campaign-banner__tagline">Together we go far beyond.</span>
         <a class="campaign__button campaign-banner__button" href="http://stonybrook.edu/campaign/" target="_blank">Join Us</a>
     </div>
