fd7038a44959700c88290dd8c49cde7cc92bba56
Josh Palmeri <palmerijosh@gmail.com>
2016-10-11 12:08:56 -0400

    fixed nav color to #900

--- inc/footerbar.php
+++ inc/footerbar.php
@@ -1,7 +1,13 @@
+<?php
+    $physical_address = '';
+    if($second_level != 'undergrad-admissions') {
+        $physical_address = '100 Nicolls Road, ';
+    }
+?>
 		        <div class="footer-container clearfix">
                     <footer class="footer-bar">
                         <nav class="wrapper">
-                            <span><span id="directedit" class="copyright">&copy;</span> <?php echo date("Y") ?> Stony Brook University</span><span>Stony Brook, NY 11794</span><span class="last">(631) 632-6000</span>
+                            <span><span id="directedit" class="copyright">&copy;</span> <?php echo date("Y") ?> Stony Brook University</span><span><?php echo $physical_address ?>Stony Brook, NY 11794</span><span class="last">(631) 632-6000</span>
                             <div class="clearfix policy-links">
                                 <a class="item" href="http://www.stonybrook.edu/sb/access">Accessibility</a>
                                 <a class="item" href="http://www.stonybrook.edu/sb/privacy">Privacy Policy</a>
