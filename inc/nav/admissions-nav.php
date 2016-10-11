b83a6738421add535c6038a6cf0a6e26ab26dd53
Josh Palmeri <palmerijosh@gmail.com>
2016-10-11 11:21:35 -0400

    updated maps nav link

--- inc/nav/admissions-nav.php
+++ inc/nav/admissions-nav.php
@@ -166,12 +166,7 @@
   			</li>
 			<li class="<?php if($drop_nav_selected_tab==20) echo('selected'); ?>" data-li-order="03">
 				<a href="undergraduate-admissions/visit/directions-and-parking" class="clearfix">
-					<span class="label clearfix">Directions &amp; Parking</span>
-				</a>
-			</li>
-			<li class="<?php if($drop_nav_selected_tab==21) echo('selected'); ?>" data-li-order="04">
-				<a href="undergraduate-admissions/visit/campus-maps" class="clearfix">
-					<span class="label clearfix">Campus<br />Maps</span>
+					<span class="label clearfix">Maps &amp; Directions</span>
 				</a>
 			</li>
 			<li>
@@ -179,11 +174,11 @@
 					<span class="label clearfix">Nearby<br />Hotels</span>
 				</a>
 			</li>
-			<?php /*<li class="<?php if($drop_nav_selected_tab==22) echo('selected'); ?>" data-li-order="05">
+			<li class="<?php if($drop_nav_selected_tab==22) echo('selected'); ?>" data-li-order="05">
 				<a href="undergraduate-admissions/visit/coming-to-you" class="clearfix">
 					<span class="label clearfix">Coming to<br />You</span>
 				</a>
-			</li>*/ ?>
+			</li>
 			<li class="<?php if($drop_nav_selected_tab==6) echo('selected'); ?>" data-li-order="06" data-last-child="true" data-image="map">
 				<a href="undergraduate-admissions/visit" class="clearfix">
 					<span class="label clearfix">Plan Your Visit</span>
