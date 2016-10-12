7783c3c3f7319614b9b617e256cd3b009089280a
Josh Palmeri <palmerijosh@gmail.com>
2016-10-12 15:09:41 -0400

    added Google Tag Manager

--- 5questions/inc/global.php
+++ 5questions/inc/global.php
@@ -1,3 +1,7 @@
 <!--[if lt IE 8]>
     <p class="chromeframe">Uh-oh! You are using an <strong>outdated</strong> web browser. :-( It's simple to fix! <a href="http://browsehappy.com/">Click here to upgrade your browser</a> for a much better experience.</p>
 <![endif]-->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P43R9T"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
--- 5questions/inc/header.php
+++ 5questions/inc/header.php
@@ -37,6 +37,14 @@
 
         <meta name="specificfeeds-verification-code" content="HkHZYNv62P0vlPLAZVA5"/>
 
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P43R9T');</script>
+        <!-- End Google Tag Manager -->
+
     <!--BEGIN main CSS Includes-->
         <link rel="stylesheet" href="css/vendor/normalize.min.css">
         <?php includeAsset('css','plugins/nivo-lightbox/nivo-lightbox.css','all'); ?>
