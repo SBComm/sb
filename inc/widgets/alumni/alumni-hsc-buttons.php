e3eef225060c19374085aeb16b43e99260f34d62
Josh Palmeri <palmerijosh@gmail.com>
2016-11-15 16:24:00 -0500

    added htm and logic to not show the button for the page you are on

--- inc/widgets/alumni/alumni-hsc-buttons.php
+++ inc/widgets/alumni/alumni-hsc-buttons.php
@@ -1,6 +1,13 @@
 <div class="rounded-image-buttons clearfix">
+    <?php if($selected_sub_nav!="school-of-medicine") { ?>
     <a class="medicine<?php if($selected_sub_nav=="school-of-medicine") { ?> selected<?php } ?>" href="alumni/chapters/school-of-medicine"><div class="label"><span>School of</span> Medicine</div></a>
+    <?php } else if($selected_sub_nav!="school-of-dental-medicine") { ?>
     <a class="dental<?php if($selected_sub_nav=="school-of-dental-medicine") { ?> selected<?php } ?>" href="alumni/chapters/school-of-dental-medicine"><div class="label"><span>School of</span> Dental Medicine</div></a>
+    <?php } else if($selected_sub_nav!="school-of-social-welfare") { ?>
     <a class="social-welfare<?php if($selected_sub_nav=="school-of-social-welfare") { ?> selected<?php } ?>" href="alumni/chapters/school-of-social-welfare"><div class="label"><span>School of</span> Social Welfare</div></a>
+    <?php } else if($selected_sub_nav!="school-of-nursing") { ?>
     <a class="nursing<?php if($selected_sub_nav=="school-of-nursing") { ?> selected<?php } ?>" href="alumni/chapters/school-of-nursing"><div class="label"><span>School of</span> Nursing</div></a>
+    <?php } else if($selected_sub_nav!="school-of-health-technology-management") { ?>
+    <a class="htm<?php if($selected_sub_nav=="school-of-health-technology-management") { ?> selected<?php } ?>" href="alumni/chapters/school-of-health-technology-management"><div class="label"><span>School of</span> Health Technology Management</div></a>
+    <?php } ?>
 </div>
