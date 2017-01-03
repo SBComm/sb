51564070f5bb75d5a36020bd557e0dc983921c01
Josh Palmeri <palmerijosh@gmail.com>
2017-01-03 15:07:17 -0500

    added topStoriesYear var

--- top-stories/js/main1.js
+++ top-stories/js/main1.js
@@ -1,3 +1,5 @@
+console.log(topStoriesYear);
+
 String.prototype.trimToLength = function(m) {
   return (this.length > m) 
     ? jQuery.trim(this).substring(0, m).split(" ").slice(0, -1).join(" ") + "..."
