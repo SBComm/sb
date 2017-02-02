7835624e632fc69cd67ade3296e34d55541bf61e
Communications Office <commoff@dh214-113.adm.sunysb.edu>
2017-02-02 16:37:35 -0500

    updated year

--- inc/widgets/alumni/alumni-flipbook-promo.php
+++ inc/widgets/alumni/alumni-flipbook-promo.php
@@ -2,7 +2,7 @@
     <div class="border-box wrapper clearfix pad-bottom">
         <div class="content pad-top pad-bottom">
 	        <h3>A Year to Be Proud Of</h3>
-	        <p>View the Stony Brook Alumni Association Annual Report for the academic year 2014-15.</p>
+	        <p>View the Stony Brook Alumni Association Annual Report for the academic year 2015-16.</p>
 	        <a class="sbu-outline-button sbu-outline-button--on-white arrow-after" href="alumni/annual-report" target="_blank">View Report</a>
         </div>
     </div>
