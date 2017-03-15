01939b1572f7d7bc7079b37f4b62a909615fb859
Josh Palmeri <joshua.palmeri@stonybrook.edu>
2017-03-15 09:34:06 -0400

    added request-assistance form dev, removed from indexing

---
+++ inc/content/request-assistance/request-assistance.php
@@ -0,0 +1,49 @@
+<style type="text/css">
+	.form-request h4 {
+		color: #fff;
+	}
+</style>
+
+<div id="form" class="seawolfStrip form-request">
+	<div class="wpb_wrapper"> 
+		<h3 class="clearfix"><span>Request Assistance</span></h3>
+		<br />
+		<h4 class="clearfix">Using this Communications form for projects makes it easy for us to help you determine your content priorities. <br /><br />To get started, let's ask you a few questions to understand your needs.</h4>
+		<br />
+
+		<ul class="clearfix">
+			<li class="btn"><a class="show-form-trigger" href="#" data-target-form="get-started">Get Started</a></li>
+		</ul>
+
+		<div id="get-started" class="apply-form clearfix hide-accessible">
+			<h2 class="clearfix">First, what would you like to do?</h2>
+			<ul class="clearfix">
+				<li class="btn"><a class="show-form-trigger" href="#" data-target-form="form-event">Promote an event</a></li>
+				<li class="btn"><a class="show-form-trigger" href="#" data-target-form="form-event">Run a campaign</a></li>
+				<li class="btn"><a class="show-form-trigger" href="#" data-target-form="form-event">Order print materials</a></li>
+				<li class="btn"><a class="show-form-trigger" href="#" data-target-form="form-event">Get website assistance</a></li>
+				<li class="btn"><a class="show-form-trigger" href="#" data-target-form="form-event">Etc...</a></li>
+			</ul>
+		</div>
+
+		<div id="form-event" class="apply-form clearfix hide-accessible">
+			<h4 class="clearfix"><span>OK, Promote an event!</span></h4>
+			<ul class="clearfix">
+				<li class="btn"><a href="">Undergraduate</a></li>
+				<li class="btn"><a href="">Graduate</a></li>
+				<li class="btn"><a href="">Health Sciences</a></li>
+				<li class="btn"><a href="">School of Professional Development</a></li>
+			</ul>
+			<div class="apply-form clearfix">
+				<h4 class="clearfix"><span>What level?</span></h4>
+				<ul class="clearfix">
+				<li class="btn"><a href="">Freshman</a></li>
+				<li class="btn"><a href="">Transfer</a></li>
+				<li class="btn"><a href="">International</a></li>
+				<li class="btn"><a href="">Second Degree</a></li>
+				<li class="btn"><a href="">Readmission</a></li>
+				</ul>
+			</div>
+		</div>
+	</div><!-- /.wpb_wrapper -->	
+</div><!-- /.seawolfStrip -->	
---
+++ request-assistance/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<!-- <inc> -->
+		<?php
+			$root = '/user/far-beyond/www';
+
+			$this_dir = $_SERVER['REQUEST_URI'];
+			$dev_dir  = '/development/sb/';
+			$prod_dir = '/sb/';
+			$is_dev = strpos($this_dir,'/development/');
+			if($_SERVER['SERVER_NAME'] == 'localhost') {
+				$inc = $_SERVER['DOCUMENT_ROOT'] . $prod_dir . "inc/inc.php";
+			} else if($is_dev !== false) {
+				$inc = $root . $dev_dir . "inc/inc.php";
+			} else {
+				$inc = $root . $prod_dir . "inc/inc.php";
+			}
+			include($inc);
+		?>
+	<!-- </inc> -->
+	<!--site variables-->
+		<?php
+			$page_type = 'home';
+		    $page_to_top_link = true;
+		    $page_footerbar = true;
+		    $main_nav_selected_tab = 0;
+		    $audience_nav_selected_tab = 0;
+		    $page_footer = true;
+		    $lightbox = true;
+	        $html_dom_parser = true;
+	        $page_to_top_link = false;
+		    $site_breadcrumbs = false;
+		?>
+
+	<head>
+		<?php 
+			$file  = $header;
+			include($path . $file);
+		?>
+	</head>
+    <body>
+        <!-- <global> -->
+			<?php 
+				$file  = $global;
+				include($path . $file);
+			?>
+		<!-- </global> -->
+
+        <div class="sbu-wrapper clearfix">
+        	<div class="sbu-sub-wrapper">
+
+		        <div class="header-container">
+		        	<div class="nav-elements-container">
+			        	<!-- <quick-nav> -->
+							<?php
+								include($path . $quick_nav);
+							?>
+						<!-- </quick-nav> -->
+			            <!-- <more-nav> -->
+							<?php 
+								include($path . $more_nav);
+							?>
+						<!-- </more-nav> -->
+			            <!-- <audience-nav> -->
+							<?php /*
+								include($path . $audience_nav);
+							*/ ?>
+						<!-- </audience-nav> -->
+					</div>
+					<!-- <logo-container> -->
+						<?php 
+							include($path . $logo_container);
+						?>
+					<!-- </logo-container> -->
+		        </div>
+		        <div class="main-nav-container clearfix">
+		        	<!-- <site-nav> -->
+						<?php 
+							include($path . $site_nav);
+						?>
+					<!-- </site-nav> -->
+		        </div>
+		        <div class="main-container">
+		            <div class="main clearfix">
+
+		                <!-- <main-content> -->
+							<?php
+								$file = "request-assistance/request-assistance.php";
+								include($pathForContent . $content . $file);
+							?>
+						<!-- </main-content> -->
+
+		            </div> <!-- .main -->
+		        </div> <!-- .main-container -->
+		        <!-- <div.footer-container> -->
+		        	<?php if($page_footer) {
+						$file = $footer;
+						include($path . $file);
+					} ?>
+					<?php 
+						if($page_footerbar) {
+							$file = $footerbar;
+							include($path . $file);
+						}
+					?>
+				<!-- </div.footer-container> -->
+<!--
+				<div class="left-fixed-sidebar-container">
+
+				</div>
+
+				<div class="right-fixed-sidebar-container">
+
+				</div>
+-->
+				<!-- <to-top> -->
+					<?php if($page_to_top_link) {
+						$file = "to-top.php";
+						include($path . $file);
+					} ?>
+				<!-- </to-top> -->
+
+	        </div><!-- .sbu-sub-wrapper -->
+	    </div><!-- .sbu-wrapper -->
+
+	    <!-- <scripts> -->
+			<?php 
+				$file = "footer-scripts.php";
+				include($path . $file);
+			?>
+		<!-- </scripts> -->
+
+		<!-- <googleanalytics> -->
+			<?php
+				$file = "site-analytics.php";
+				include($path . $file);
+			?>
+		<!-- </googleanalytics> -->
+    </body>
+</html>
