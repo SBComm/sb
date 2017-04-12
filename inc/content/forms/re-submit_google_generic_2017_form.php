53dc4cdf8c77daae30853329e992412ab2b75a57
Josh Palmeri <joshua.palmeri@stonybrook.edu>
2017-04-12 16:33:41 -0400

    created generic meeting request form: google form

---
+++ inc/content/forms/re-submit_google_generic_2017_form.php
@@ -0,0 +1,21 @@
+<article class="clearfix second-level-header">
+    <section class="header-title-image-sac page-title">
+        <div class="wrapper border-box">
+            <h1>&nbsp;</h1>
+        </div>
+    </section>
+
+    <section class="clearfix page-content white-background">
+        <div class="wrapper clearfix pad-top pad-bottom">
+            <div class="pad-top pad-bottom">
+                <img class="inline-image hide-accessible-mobile" src="img/university/pole-banners-far-beyond.jpg" />
+                <p>We need some more information to process your request.</p>
+                <?php
+                    $file = "widgets/forms/google_generic_2017_form.php";
+                    include($path . $file);
+                ?>
+
+            </div>
+        </div>
+    </section>
+</article>
---
+++ inc/content/forms/submit_google_generic_2017_form.php
@@ -0,0 +1,18 @@
+<article class="clearfix second-level-header">
+    <section class="header-title-image-sac page-title">
+        <div class="wrapper border-box">
+            <h1>Thank You</h1>
+        </div>
+    </section>
+
+    <section class="clearfix page-content white-background">
+        <div class="wrapper clearfix pad-top pad-bottom">
+            <div class="pad-top pad-bottom">
+                <img class="inline-image" src="img/university/pole-banners-far-beyond.jpg" />
+                <p><strong>Hi <?php echo htmlspecialchars($_POST['first-name']); ?></strong>, <br /><br />We recieved your request and will contact you soon.</p>
+                <br />
+                <br />
+            </div>
+        </div>
+    </section>
+</article>
