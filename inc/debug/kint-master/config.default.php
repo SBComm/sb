4290e994a63944de926b5a178764fd7bc6bb8a8e
Josh Palmeri <joshua.palmeri@stonybrook.edu>
2017-05-05 16:48:18 -0400

    adjusted kint levels to be infinite

--- inc/debug/kint-master/config.default.php
+++ inc/debug/kint-master/config.default.php
@@ -86,7 +86,8 @@ $_kintSettings['charEncodings'] = array();
 
 
 /** @var int max array/object levels to go deep, if zero no limits are applied */
-$_kintSettings['maxLevels'] = 5;
+//$_kintSettings['maxLevels'] = 5;
+$_kintSettings['maxLevels'] = 0;
 
 
 /** @var bool whether dumped indexed arrays that are in ideal sequence are displayed */
