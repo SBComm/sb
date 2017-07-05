67fecf26d2168f19903a43fd148163f0a96e6910
Josh Palmeri <joshua.palmeri@stonybrook.edu>
2017-07-05 15:25:58 -0400

    added test pages for second level

---
+++ test/nav-2017/about.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<!-- <inc> -->
+		<?php
+			$root = '/user/far-beyond/www';
+
+			$this_dir = $_SERVER['REQUEST_URI'];
+			$dev_dir  = '/development/sb/';
+			$prod_dir = '/sb/';
+			$is_dev = strpos($this_dir,'/development/');
+			if($_SERVER['SERVER_NAME'] == 'localhost') {
+				$inc = $_SERVER['DOCUMENT_ROOT'] . $prod_dir . "inc/inc.php";
+			} else if($is_dev !== false) {
+				$inc = $root . $dev_dir . "inc/inc.php";
+			} else {
+				$inc = $root . $prod_dir . "inc/inc.php";
+			}
+			include($inc);
+		?>
+	<!-- </inc> -->
+	<!--site variables-->
+		<?php
+			$page_type       = 'second-level';
+			$second_level    = 'site-directory';
+			$page_title_sub  = 'About';
+			$page_title_full = $page_title . (isset($page_title_sub) && $page_title_sub!='' ? ' | ' . $page_title_sub : '');
+
+		    $og_title        = 'Stony Brook ' . $page_title_sub;
+		    $og_description  = 'Stony Brook University is going beyond the expectations of what today’s public universities can accomplish.  This young university has grown to become one of only four University Center campuses in the State of New York (SUNY) system with more than 25,200 students and faculty that have earned prestigious awards, including the Nobel Prize, Pulitzer Prize, Indianapolis Prize for animal conservation, Abel Prize and the inaugural Breakthrough Prize in Mathematics.';
+		    $og_url          = 'http://www.stonybrook.edu/about';
+		    $main_nav_selected_tab = 3;
+
+		    $page_to_top_link = true;
+
+		    $page_footerbar   = true;
+		    $page_footer      = true;
+
+		    $carousel 		  = true;
+
+		    $page_scroll        = true;
+		    $page_scroll_el     = 'logo';     //Options are audience-nav, logo, main-nav, bottom
+		    $page_scroll_time   = 150;      //Time for scroll function in milliseconds
+		    $page_scroll_mobile = true;  //Define whether the scroll-on-load occurs only on mobile. False by default, meaning scroll will happen on desktop and mobile.
+
+		    $site_breadcrumbs = false;
+
+		    $mega_nav = true;
+		    $mega_nav_ou = true;
+		?>
+
+	<head>
+		<?php 
+			$file  = $header;
+			include($path . $file);
+		?>
+	</head>
+    <body>
+        <!-- <global> -->
+			<?php 
+				$file  = $global;
+				include($path . $file);
+			?>
+		<!-- </global> -->
+
+        <div class="sbu-wrapper clearfix">
+        	<div class="sbu-sub-wrapper">
+
+		        <div class="main-nav-container clearfix">
+		        	<!-- <site-nav> -->
+						<?php 
+							//include($path . $site_nav);
+							include($path . "site-nav-2017.php");
+						?>
+					<!-- </site-nav> -->
+		        </div>
+		        <div class="main-container">
+		            <div class="main clearfix">
+
+		                <!-- <content> -->
+							<?php 
+								$file = "{$second_level}/{$second_level}.php";
+								//include($pathForContent . $content . $secLv . $file);
+								include('/user/commcms/www/_second-level/about/index.php');
+							?>
+						<!-- </content> -->
+
+		            </div> <!-- .main -->
+		        </div> <!-- .main-container -->
+		        <!-- <div.footer-container> -->
+		        	<?php if($page_footer) {
+						$file = "footers/sbu-footer.php";
+						include($path . $file);
+					} ?>
+					<?php if($page_footerbar) {
+						$file = "footerbar.php";
+						include($path . $file);
+					} ?>
+				<!-- </div.footer-container> -->
+<!--
+				<div class="left-fixed-sidebar-container">
+
+				</div>
+
+				<div class="right-fixed-sidebar-container">
+
+				</div>
+-->
+				<!-- <to-top> -->
+					<?php if($page_to_top_link) {
+						$file = "to-top.php";
+						include($path . $file);
+					} ?>
+				<!-- </to-top> -->
+
+	        </div><!-- .sbu-sub-wrapper -->
+	    </div><!-- .sbu-wrapper -->
+
+	    <!-- <scripts> -->
+			<?php 
+				$file = "footer-scripts-ou-2017.php";
+				include($path . $file);
+			?>
+		<!-- </scripts> -->
+
+		<!-- <googleanalytics> -->
+			<?php 
+				$file = "site-analytics.php";
+				include($path . $file); 
+			?>
+		<!-- </googleanalytics> -->
+    </body>
+</html>
