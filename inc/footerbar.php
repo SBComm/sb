2d43ab1c8f99a4b2bfe955d975a385cdb4dee686
Josh Palmeri <joshua.palmeri@stonybrook.edu>
2017-07-20 17:28:54 -0400

    Add Sustainability and Diversity to footer via https://app.asana.com/0/68697120462982/383924928596404/f

--- inc/footerbar.php
+++ inc/footerbar.php
@@ -9,8 +9,10 @@
                         <nav class="wrapper">
                             <span><span id="directedit" class="copyright">&copy;</span> <?php echo date("Y") ?> Stony Brook University</span><span><?php echo $physical_address ?>Stony Brook, NY 11794</span><span class="last">(631) 632-6000</span>
                             <div class="clearfix policy-links">
-                                <a class="item" href="http://www.stonybrook.edu/sb/access">Accessibility</a>
-                                <a class="item" href="http://www.stonybrook.edu/sb/privacy">Privacy Policy</a>
+                                <a class="item" href="http://www.stonybrook.edu/accessibility/">Accessibility</a>
+                                <a class="item" href="http://www.stonybrook.edu/privacy/">Privacy Policy</a>
+                                <a class="item" href="http://www.stonybrook.edu/sustainability/">Sustainability</a>
+                                <a class="item" href="http://www.stonybrook.edu/diversity/">Diversity</a>
                             </div>
                         </nav>
                     </footer>
