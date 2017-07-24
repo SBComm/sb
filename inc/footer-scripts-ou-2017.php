2ef3361efdeff4b58557dc91e54e211e44237351
Josh Palmeri <joshua.palmeri@stonybrook.edu>
2017-07-24 16:50:54 -0400

    added scripts and libs

--- inc/footer-scripts-ou-2017.php
+++ inc/footer-scripts-ou-2017.php
@@ -38,6 +38,169 @@
 	new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ) );
 </script>
 
+<script type="text/javascript">
+	$(document).ready(function() {
+		$(".site-name").fitText({ minFontSize: '20px', maxFontSize: '27px' });
+		$(".page-title h1").fitText(1.2, { minFontSize: '20px', maxFontSize: '52px' });
+		//$("h1.second-level-head-region_heading").fitText(1.2, { minFontSize: '30px', maxFontSize: '52px' });
+		/*$(".site-carousel h1").fitText(1, { minFontSize: '30px', maxFontSize: '100px' });*/
+	});
+</script>
+
+<script> 
+	$(window).load(function(){
+		doBreadcrumbs();
+		$('.breadcrumbs').scrollTo('100%', 400);
+		if($(window).width()>=1022) {
+			if($('.background-video').length) {
+				$('.background-video').get(0).play();
+			}
+		}
+	});
+	$(window).resize(function(){
+		doBreadcrumbs();
+		if($(window).width()>=1022) {
+			if ($('.sticky-wrapper').length == 0) { 
+				if($('.background-video').length) {
+					$('.background-video').get(0).play();
+				}
+			}
+			configureSearchPosition();
+		} else {
+			if(configuredSearch && !$('html').hasClass('ie9')) {
+				resetSearchPosition();
+			}
+		}
+	});
+</script>
+
+<?php if($carousel) { ?>
+	<?php includeAsset('js','js/elements/owl-carousel/owl.carousel.js'); ?>
+    <script>
+        $(document).ready(function() {
+
+            var owl = $("#the-carousel-1");
+            var owlSlides = owl.children('.item').length;
+            if(owlSlides==1) {
+            	autoPlayValue = false;
+            	 $('.owl-prev-1').remove();
+            	 $('.owl-next-1').remove();
+            } else {
+            	autoPlayValue = 8000;
+                $('.owl-prev-1').on('click', function() {
+                    owl.trigger('owl.prev');
+                });
+                $('.owl-next-1').on('click', function() {
+                    owl.trigger('owl.next');
+                });
+            }
+
+            owl.owlCarousel({
+                autoPlay: autoPlayValue,
+                navigation : false, // Show default next and prev buttons
+                slideSpeed : 300,
+                paginationSpeed : 400,
+                singleItem: true,
+                transitionStyle : "fade"
+            });
+
+			 	<?php if($carousel_2) { ?>
+        		var owl2 = $("#the-carousel-2");
+        		var owlSlides2 = owl2.children('.item').length;
+                if(owlSlides2==1) {
+                	 $('.owl-prev-2').remove();
+                	 $('.owl-next-2').remove();
+                } else {
+	                $('.owl-prev-2').on('click', function() {
+	                    owl2.trigger('owl.prev');
+	                });
+	                $('.owl-next-2').on('click', function() {
+	                    owl2.trigger('owl.next');
+	                });
+                }
+                owl2.owlCarousel({
+                	itemsCustom: [[0,1],[800,2]],
+                    autoPlay: false,
+                    navigation : false, // Show default next and prev buttons
+                    slideSpeed : 1000,
+                    paginationSpeed : 1000
+                });
+            <?php } ?>
+            
+        	<?php if($carousel_3) { ?>
+        		var owl3 = $("#the-carousel-3");
+        		var owlSlides3 = owl3.children('.item').length;
+                if(owlSlides3==1) {
+                	 $('.owl-prev-3').remove();
+                	 $('.owl-next-3').remove();
+                } else {
+	                $('.owl-prev-3').on('click', function() {
+	                    owl3.trigger('owl.prev');
+	                });
+	                $('.owl-next-3').on('click', function() {
+	                    owl3.trigger('owl.next');
+	                });
+                }
+                owl3.owlCarousel({
+                	itemsCustom: [[0,1],[480,2],[620,3],[760,4],[960,5],[1022,6]],
+                    autoPlay: 320000,
+                    navigation : true, // Show next and prev buttons
+                    slideSpeed : 800,
+                    paginationSpeed : 400
+                });
+            <?php } ?>
+
+        	<?php if($carousel_4) { ?>
+        		var owl4 = $("#the-carousel-4");
+        		var owlSlides4 = owl4.children('.item').length;
+                if(owlSlides4==1) {
+                	 $('.owl-prev-4').remove();
+                	 $('.owl-next-4').remove();
+                } else {
+	                $('.owl-prev-4').on('click', function() {
+	                    owl4.trigger('owl.prev');
+	                });
+	                $('.owl-next-4').on('click', function() {
+	                    owl4.trigger('owl.next');
+	                });
+                }
+                owl4.owlCarousel({
+                    autoPlay: 60000,
+                    navigation : false, // Show default next and prev buttons
+                    slideSpeed : 1200,
+                    paginationSpeed : 2000,
+                    singleItem: true,
+                	transitionStyle : "fade"
+                });
+            <?php } ?>
+			
+        });
+    </script>
+<?php } ?>
+
+<?php if($carousel) { ?>
+	<?php includeAsset('js','plugins/slick-1.5.7/slick/slick.min.js'); ?>
+    <script>
+	$(document).ready(function(){
+		if($('.slick-slider').length) {
+			$('.slick-slider').slick();
+		}
+	});
+	</script>
+<?php } ?>
+
+<?php if($motio==true) { ?>
+   	<script type="text/javascript">
+	   	$(document).ready(function() {
+			var panning = new Motio(document.getElementById('panning'), {
+			    fps: 30, // Frames per second. More fps = higher CPU load.
+			    speedX: -15 // Negative horizontal speed = panning to left.
+			});
+			panning.play(); // Start playing animation
+		});
+	</script>
+<?php } ?>
+
 <?php 
 /*
 echo $social_feed_channel["twitter"];
