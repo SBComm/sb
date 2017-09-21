fd1c5d0900b7938e2cff57147fd1e2c204430337
Josh Palmeri <joshua.palmeri@stonybrook.edu>
2017-09-21 09:19:57 -0400

    added aria-label to date

--- inc/feed/active-calendar_2016_v2.php
+++ inc/feed/active-calendar_2016_v2.php
@@ -66,6 +66,7 @@
 
             $eventMonth = $eventDateTime->format('M');
             $eventMonthNum = $eventDateTime->format('n');
+            $eventMonthFull = $eventDate->format('F');
             $eventDay   = $eventDateTime->format('j');
             $eventYear  = $eventDateTime->format('Y');
 
@@ -96,7 +97,7 @@
 
                 $html .= '<li>';
                 $html .= '<a class="clearfix" href="'.$rss_url.'" title="'.$rss_title.'">';
-                $html .= '<div class="eventDate"><span class="eventDate_day">'.$eventDay.'</span><span class="eventDate_month">'.$eventMonth.'</span></div>';
+                $html .= '<div class="eventDate" aria-label="'.$eventMonthFull.' '.$eventDay.', '.$eventYear.'"><span class="eventDate_day">'.$eventDay.'</span><span class="eventDate_month">'.$eventMonth.'</span></div>';
                 $html .= '<span class="event-title '.$calendar_title_color.'"><span class="item">'.$rss_title.'</span></span>';
                 if($showTime) {
                     $html .= '<span class="event-time">'.$eventWeekday.', '.$eventMonthNum.'/'.$eventDay.' at '.$eventHour.':'.$eventMinute.' '.$eventAMPM.'</span>';
