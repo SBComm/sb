62db74141d48b2fd0dd26b19f95dd63bb87672b3
Josh Palmeri <palmerijosh@gmail.com>
2017-10-24 13:01:02 -0400

    added form label

--- 5questions/inc/email-subscribe.php
+++ 5questions/inc/email-subscribe.php
@@ -2,7 +2,8 @@
 	<div class="form-overlay"> </div>
 	<form action="http://www.specificfeeds.com/widgets/subscribeWidget/dXBoREVrSFpONTJTWVVLK2wrcmRyd0JhOGZVTFRwZWpXcTJMa1F2NnBuU292Szhpd21xNWlmekpDc2lrV25Ucy8yTXdOOG5XQWhXQlltVG82U1BpcnZ4eEl1WHdDT1B4YXFOcWdTcm1uM21OVHM2QnpWa0h5YnJjKzIyZ3BLTEJ8ZmxUakkwR3JMSDJnbzNnNjJWVHdza1dlV2dIbzcrZU5XNEpxd01mWHRqWT0=/8/" method="post" target="popupwindow">
 		<h3>Don't miss a new interview!</h3>
-		<input name="data[Widget][email]" type="email" value="" placeholder="Enter your email" required="required" />
+		<label for="email-subscribe-input" class="hide-accessible">Your Email Address</label>
+		<input id="email-subscribe-input" name="data[Widget][email]" type="email" value="" placeholder="Enter your email" required="required" />
 		<input name="data[Widget][feed_id]" type="hidden" value="dXBoREVrSFpONTJTWVVLK2wrcmRyd0JhOGZVTFRwZWpXcTJMa1F2NnBuU292Szhpd21xNWlmekpDc2lrV25Ucy8yTXdOOG5XQWhXQlltVG82U1BpcnZ4eEl1WHdDT1B4YXFOcWdTcm1uM21OVHM2QnpWa0h5YnJjKzIyZ3BLTEJ8ZmxUakkwR3JMSDJnbzNnNjJWVHdza1dlV2dIbzcrZU5XNEpxd01mWHRqWT0=" />
 		<input name="data[Widget][feedtype]" type="hidden" value="8" />
 		<input class="sub_inp_subt brand-button brand-button--red" name="subscribe" type="submit" value="Subscribe" />
