45a10e2b6cc7926bd1e9d7b3f142cc6d432146a2
Josh Palmeri <palmerijosh@gmail.com>
2017-10-24 13:07:00 -0400

    added form labels

--- 5questions/inc/email-subscribe.php
+++ 5questions/inc/email-subscribe.php
@@ -4,8 +4,11 @@
 		<h3>Don't miss a new interview!</h3>
 		<label for="email-subscribe-input" class="hide-accessible">Your Email Address</label>
 		<input id="email-subscribe-input" name="data[Widget][email]" type="email" value="" placeholder="Enter your email" required="required" />
-		<input name="data[Widget][feed_id]" type="hidden" value="dXBoREVrSFpONTJTWVVLK2wrcmRyd0JhOGZVTFRwZWpXcTJMa1F2NnBuU292Szhpd21xNWlmekpDc2lrV25Ucy8yTXdOOG5XQWhXQlltVG82U1BpcnZ4eEl1WHdDT1B4YXFOcWdTcm1uM21OVHM2QnpWa0h5YnJjKzIyZ3BLTEJ8ZmxUakkwR3JMSDJnbzNnNjJWVHdza1dlV2dIbzcrZU5XNEpxd01mWHRqWT0=" />
-		<input name="data[Widget][feedtype]" type="hidden" value="8" />
-		<input class="sub_inp_subt brand-button brand-button--red" name="subscribe" type="submit" value="Subscribe" />
+		<label for="email-subscribe-wid-id" class="hide-accessible">ID</label>
+		<input id="email-subscribe-wid-id" name="data[Widget][feed_id]" type="hidden" value="dXBoREVrSFpONTJTWVVLK2wrcmRyd0JhOGZVTFRwZWpXcTJMa1F2NnBuU292Szhpd21xNWlmekpDc2lrV25Ucy8yTXdOOG5XQWhXQlltVG82U1BpcnZ4eEl1WHdDT1B4YXFOcWdTcm1uM21OVHM2QnpWa0h5YnJjKzIyZ3BLTEJ8ZmxUakkwR3JMSDJnbzNnNjJWVHdza1dlV2dIbzcrZU5XNEpxd01mWHRqWT0=" />
+		<label for="email-subscribe-type" class="hide-accessible">Type</label>
+		<input id="email-subscribe-type" name="data[Widget][feedtype]" type="hidden" value="8" />
+		<label for="email-subscribe-submit" class="hide-accessible">Subscribe</label>
+		<input id="email-subscribe-submit" class="sub_inp_subt brand-button brand-button--red" name="subscribe" type="submit" value="Subscribe" />
 	</form>
 </div>
