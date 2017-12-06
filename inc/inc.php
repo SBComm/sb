958673d1103fcdd40fca739d817e5827c81a5a36
Josh Palmeri <palmerijosh@gmail.com>
2017-12-06 17:06:07 -0500

    Updated open graph image

--- inc/inc.php
+++ inc/inc.php
@@ -84,7 +84,7 @@
     $og_description    = $page_description;
     $og_type           = 'website';
     $og_url            = $http_protocol.'://www.stonybrook.edu/';
-    $og_image          = $http_protocol.'://www.stonybrook.edu/sb/images/newlogohomepage.gif';
+    $og_image          = 'https://www.stonybrook.edu/commcms/_images/page-images/campus-images-2017/fj-gaylor/171011_SBU_0464-min.jpg';
 
     $meta = true;
 
