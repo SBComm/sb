fc06828eaf665f61777058bbe9c5f2e0fcff5c78
Josh Palmeri <palmerijosh@gmail.com>
2018-04-23 12:32:53 -0400

    Updated action to https

--- 5questions/inc/email-subscribe.php
+++ 5questions/inc/email-subscribe.php
@@ -1,6 +1,6 @@
 <div class="email-subscribe clearfix">
 	<div class="form-overlay"> </div>
-	<form action="http://www.specificfeeds.com/widgets/subscribeWidget/dXBoREVrSFpONTJTWVVLK2wrcmRyd0JhOGZVTFRwZWpXcTJMa1F2NnBuU292Szhpd21xNWlmekpDc2lrV25Ucy8yTXdOOG5XQWhXQlltVG82U1BpcnZ4eEl1WHdDT1B4YXFOcWdTcm1uM21OVHM2QnpWa0h5YnJjKzIyZ3BLTEJ8ZmxUakkwR3JMSDJnbzNnNjJWVHdza1dlV2dIbzcrZU5XNEpxd01mWHRqWT0=/8/" method="post" target="popupwindow">
+	<form action="https://www.specificfeeds.com/widgets/subscribeWidget/dXBoREVrSFpONTJTWVVLK2wrcmRyd0JhOGZVTFRwZWpXcTJMa1F2NnBuU292Szhpd21xNWlmekpDc2lrV25Ucy8yTXdOOG5XQWhXQlltVG82U1BpcnZ4eEl1WHdDT1B4YXFOcWdTcm1uM21OVHM2QnpWa0h5YnJjKzIyZ3BLTEJ8ZmxUakkwR3JMSDJnbzNnNjJWVHdza1dlV2dIbzcrZU5XNEpxd01mWHRqWT0=/8/" method="post" target="popupwindow">
 		<h3>Don't miss a new interview!</h3>
 		<label for="email-subscribe-input" class="hide-accessible">Your Email Address</label>
 		<input id="email-subscribe-input" name="data[Widget][email]" type="email" value="" placeholder="Enter your email" required="required" />
