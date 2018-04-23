ca734b544f2a0ee26abd5de66d111fb6115e04af
Josh Palmeri <palmerijosh@gmail.com>
2018-04-23 10:36:51 -0400

    Disabling verification of ssl certificate
    Showing issues on http (magazine and homepage spotlights)

--- inc/scripts/get-homepage-spotlights.php
+++ inc/scripts/get-homepage-spotlights.php
@@ -13,9 +13,10 @@ function printBooks($path, $pathForContent, $content, $sbu_site_url) {
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $wp_posts_url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-	curl_setopt($ch, CURLOPT_CAINFO, "/usr/local/ssl/certs/cacert.pem");
+	//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+	//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+	//curl_setopt($ch, CURLOPT_CAINFO, "/usr/local/ssl/certs/cacert.pem");
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	$wp_post_data = curl_exec($ch);
 	curl_close($ch);
 
--- inc/scripts/get-magazine-json.php
+++ inc/scripts/get-magazine-json.php
@@ -5,9 +5,10 @@
 	curl_setopt($ch, CURLOPT_URL, $wp_posts_url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-	curl_setopt($ch, CURLOPT_CAINFO, "/usr/local/ssl/certs/cacert.pem");
+	//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+	//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+	//curl_setopt($ch, CURLOPT_CAINFO, "/usr/local/ssl/certs/cacert.pem");
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	$wp_post_data = curl_exec($ch);
 	curl_close($ch);
 
