20de95c835ac6fd859ac546df5be777e941a34f8
Josh Palmeri <palmerijosh@gmail.com>
2018-04-23 12:34:05 -0400

    Updated jquery to https

--- 5questions/inc/footer-scripts.php
+++ 5questions/inc/footer-scripts.php
@@ -1,4 +1,4 @@
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
 		<script type="text/javascript">
