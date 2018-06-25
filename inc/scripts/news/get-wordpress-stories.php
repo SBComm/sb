2463ab879ce4ef9bcf1f8da87a61029cb22cfa89
Josh Palmeri <palmerijosh@gmail.com>
2018-06-25 15:54:09 -0400

    Tons of improvements/enhancements

--- inc/scripts/news/get-wordpress-stories.php
+++ inc/scripts/news/get-wordpress-stories.php
@@ -1,21 +1,28 @@
 <?php 
 
-    include('inc/estimated-read-time.php');
+    include('inc/news-inc.php');
 
 	// Get passed params
 	$current_page 		= $_GET["page"]=='' ? '1' : $_GET["page"];
-	$per_page 	= $_GET["per_page"]=='' ? '25' : $_GET["per_page"];
+	$per_page 	= $_GET["per_page"]=='' ? '30' : $_GET["per_page"];
 	$orderby 	= $_GET["orderby"]=='' ? 'date' : $_GET["orderby"];
 	$order 		= $_GET["order"]=='' ? 'desc' : $_GET["order"];
 	$categories	= $_GET["categories"]=='' ? '' : $_GET["categories"];
 	$tags		= $_GET["tags"]=='' ? '' : $_GET["tags"];
+
+	if($per_page > 100) {
+		$per_page = 100;
+	}
+
+	$magazine_category_id = jp_get_magazine_category_id();
+	//var_dump($magazine_category_id);
 
 	/*
 	 * GET CATEGORY ID(S) FROM CAT SLUG(S)
 	 */
 
 	// Get category slug
-    $api_url = "https://www.stonybrook.edu/happenings/wp-json/wp/v2/categories?slug=".$categories;
+    $api_url = "http://localhost/news/wp-json/wp/v2/categories?slug=".$categories;
 
 	//  Initiate curl
 	$ch = curl_init();
@@ -25,22 +32,24 @@
 	$result = curl_exec($ch);
 	curl_close($ch);
 
-	$cats = json_decode($result, false);
-	//echo json_encode($cats, JSON_PRETTY_PRINT);
+	$cats_arr = json_decode($result, false);
+	//echo json_encode($cats_arr, JSON_PRETTY_PRINT);
 
 	$category_ids = '';
 	$category_filter = '';
+	$api_query_cat_label = '';
 
-	$numCats = count($cats);
+	$numCats = count($cats_arr);
 	if($numCats>0) {
 		$i = 0;
-	    foreach ($cats as $cat) {
+	    foreach ($cats_arr as $cat) {
 	    	$category_ids .= $cat->{'id'};
 	    	if(++$i !== $numCats) {
 				$category_ids .= ',';
 			}
 	    }
-	    $category_filter = "categories=".$category_ids;
+	    $category_filter 		= "categories=".$category_ids;
+		$api_query_cat_label 	= "categories=".$categories;
 	}
 
 	/*
@@ -48,7 +57,7 @@
 	 */
 
 	// Get category slug
-    $api_url = "https://www.stonybrook.edu/happenings/wp-json/wp/v2/tags?slug=".$tags;
+    $api_url = "http://localhost/news/wp-json/wp/v2/tags?slug=".$tags;
 
 	//  Initiate curl
 	$ch = curl_init();
@@ -58,47 +67,60 @@
 	$result = curl_exec($ch);
 	curl_close($ch);
 
-	$tags = json_decode($result, false);
-	//echo json_encode($stories, JSON_PRETTY_PRINT);
+	$tags_arr = json_decode($result, false);
+	//echo json_encode($tags_arr, JSON_PRETTY_PRINT);
 
 	$tag_ids = '';
 	$tag_filter = '';
+	$api_query_tag_label = '';
 
-	$numTags = count($tags);
+	$numTags = count($tags_arr);
 	if($numTags>0) {
 		$i = 0;
-	    foreach ($tags as $tag) {
+	    foreach ($tags_arr as $tag) {
 	    	$tag_ids .= $tag->{'id'};
 	    	if(++$i !== $numTags) {
 				$tag_ids .= ',';
 			}
 	    }
-	    $tag_filter = "tags=".$tag_ids;
+	    $tag_filter 			= "tags=".$tag_ids;
+		$api_query_tag_label 	= "tags=".$tags;
 	}
 
     /*
 	 * GET STORIES
 	 */
 
+    $status_filter = "status=publish,private,pending";
+    $exclude_cat_filter = "categories_exclude=".$magazine_category_id;
+
 	$api_query_per_page 	= "per_page=".$per_page;
 	$api_query_page 		= "page=".$current_page;
 	$api_query_orderby 		= "orderby=".$orderby;
 	$api_query_order 		= "order=".$order;
-	$api_query_cat_label 	= (count($categories)>0 ? "categories=".$categories : "");
-	$api_query_tag_label 	= (count($tags)>0 ? "tags=".$tags : "");
-    $api_url = "https://www.stonybrook.edu/happenings/wp-json/wp/v2/posts?".$api_query_page.'&'.$api_query_per_page."&".$api_query_orderby."&".$api_query_order."&".$category_filter."&".$tag_filter;
-
-    var_dump($api_url);
 
 	// Get header and pagination
+    $api_url = "http://localhost/news/wp-json/wp/v2/posts?".$api_query_page.'&'.$api_query_per_page."&".$api_query_orderby."&".$api_query_order."&".$category_filter."&".$tag_filter."&".$status_filter."&".$exclude_cat_filter;
+
+    $api_access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL25ld3MiLCJpYXQiOjE1Mjk0MTgxOTgsIm5iZiI6MTUyOTQxODE5OCwiZXhwIjoxNTMwMDIyOTk4LCJkYXRhIjp7InVzZXIiOnsiaWQiOiIxIn19fQ.m8dm1vr_Xh1bOM37vVZhd6aul80R_3wMEo_DpPJ5uDg";
+
+    $api_headers = array(
+    	'Content-length: 0',
+		'Content-Type: application/json',
+		sprintf('Authorization: Bearer %s', $api_access_token)
+	);
+
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_VERBOSE, 1);
 	curl_setopt($ch, CURLOPT_HEADER, 1);
 	curl_setopt($ch, CURLOPT_URL,$api_url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $api_headers);
 	$result = curl_exec($ch);
+
 	curl_close($ch);
+	
 	$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
 	$header = substr($result, 0, $header_size);
 	$body = substr($result, $header_size);
@@ -117,109 +139,142 @@
 	$total_pages 	= $headers['X-WP-TotalPages'];
 
 	// Get stories
+	$api_url = "http://localhost/news/wp-json/wp/v2/posts?".$api_query_page.'&'.$api_query_per_page."&".$api_query_orderby."&".$api_query_order."&".$category_filter."&".$tag_filter."&".$status_filter."&".$exclude_cat_filter;
+
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_URL,$api_url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $api_headers);
 	$result = curl_exec($ch);
+
 	curl_close($ch);
 
 	$stories = json_decode($result, false);
-	//echo json_encode($stories, JSON_PRETTY_PRINT);
 
-    $output = '
-        <div class="department-news-stories">
-        	<div class="row gridlove-posts">
-	';
+	if($stories->code == NULL && count($stories) > 0) {
 
-    foreach ($stories as $story) {
-
-        $story_id		= $story->{'id'};
-
-        $story_date		= strtotime($story->{'date'});
-    	$story_day    	= date('j',$story_date);
-    	$story_month  	= date('F',$story_date);
-    	$story_year   	= date('Y',$story_date);
-    	$story_date   	= date('F j, Y',$story_date);
-
-    	$story_link		= $story->{'link'};
-    	$story_slug		= $story->{'slug'};
-    	$story_title	= $story->{'title'}->{'rendered'};
-    	$story_content	= $story->{'content'}->{'rendered'};
-
-    	$story_image	= $story->{'featured_image_url'};
-    	if($story_image=='') {
-    		$story_image = 'https://www.stonybrook.edu/commcms/_images/section-backgrounds/red-rays/rays-vertical-1.jpg';
-    	}
-
-    	$story_dept_title	= $story->{'acf'}->{'secondary_title'};
-    	$story_dept_content	= $story->{'acf'}->{'secondary_content'};
-
-    	$story_dept_image	= $story->{'acf'}->{'secondary_featured_image'}->{'url'};
-    	if($story_dept_image=='') {
-    		$story_dept_image = $story_image;
-    	}
-
-    	$story_dept_hide	= $story->{'acf'}->{'hide_on_department_feeds'};
-
-		$story_words = str_word_count( strip_tags( $story_dept_content ) );
-		$story_minutes = floor( $story_words / 120 );
-		$story_seconds = floor( $story_words % 120 / ( 120 / 60 ) );
-		$story_ert = jp_estimated_reading_time($story_words,$story_minutes,$story_seconds);
-
-        $output .= '
-		        <div class="col-lg-4 col-md-6 col-sm-12 layout-simple">
-		            <article class="gridlove-post gridlove-post-a gridlove-box post-2430 post type-post status-publish format-standard hentry category-alumni tag-advancement tag-campaign-for-stony-brook tag-donations tag-wolfstock">
-		                <div class="entry-image">
-		                    <a href="get-wordpress-story.php?slug='.$story_slug.'" title="'.$story_dept_title.'"><img alt="'.$story_dept_title.'" src="'.$story_image.'"></a>
-		                </div>
-		                <div class="box-inner-p">
-		                    <div class="box-inner-ellipsis">
-		                        <div style="margin: 0px; padding: 0px; border: 0px;">
-		                            <h2 class="entry-title h3"><a href="get-wordpress-story.php?slug='.$story_slug.'">'.$story_dept_title.'</a></h2>
-		                        </div>
-		                    </div>
-		                    <div class="entry-meta">
-		                        <div class="meta-item meta-date">
-		                            <span class="updated department-news-story_date">'.$story_date.'</span>
-		                        </div>
-		                    </div>
-		                </div>
-		            </article>
-		        </div>
+	    $output = '
+	        <div class="department-news-stories">
+	        	<div class="row gridlove-posts">
 		';
+
+	    foreach ($stories as $story) {
+
+	    	$story_dept_hide	= $story->{'acf'}->{'hide_on_department_feeds'};
+	    	if($story_dept_hide != 1) {
+
+	    		$story_id		= $story->{'id'};
+
+	    		//var_dump($stories);
+
+		        $story_date		= strtotime($story->{'date'});
+		    	$story_day    	= date('j',$story_date);
+		    	$story_month  	= date('F',$story_date);
+		    	$story_year   	= date('Y',$story_date);
+		    	$story_date   	= date('F j, Y',$story_date);
+
+		    	$story_link		= $story->{'link'};
+		    	$story_slug		= $story->{'slug'};
+		    	$story_title	= $story->{'title'}->{'rendered'};
+		    	$story_content	= $story->{'content'}->{'rendered'};
+
+		    	$story_dept_title	= $story->{'acf'}->{'secondary_title'};
+		    	$story_dept_content	= $story->{'acf'}->{'secondary_content'};
+
+				// Strip "Private: " from the title
+				$private_needle = 'Private: ';
+				if(substr($story_title, 0, strlen($private_needle)) === $private_needle) {
+					$story_title = str_replace('Private: ','',$story_title);
+				}
+
+		    	$story_image	= $story->{'featured_image_url'};
+		    	if($story_image=='') {
+		    		$story_image = 'https://www.stonybrook.edu/commcms/_images/section-backgrounds/red-rays/rays-vertical-1.jpg';
+		    	}
+
+		    	$story_dept_image	= $story->{'acf'}->{'secondary_featured_image'}->{'url'};
+		    	if($story_dept_image=='') {
+		    		$story_dept_image = $story_image;
+		    	}
+
+				$story_words = str_word_count( strip_tags( $story_dept_content ) );
+				$story_minutes = floor( $story_words / 120 );
+				$story_seconds = floor( $story_words % 120 / ( 120 / 60 ) );
+				$story_ert = jp_estimated_reading_time($story_words,$story_minutes,$story_seconds);
+
+		        $output .= '
+				        <div class="col-lg-4 col-md-6 col-sm-12 layout-simple">
+				            <article class="gridlove-post gridlove-post-a gridlove-box post-2430 post type-post status-publish format-standard hentry category-alumni tag-advancement tag-campaign-for-stony-brook tag-donations tag-wolfstock">
+				                <div class="entry-image">
+				                    <a href="get-wordpress-story.php?slug='.$story_slug.'" title="'.$story_dept_title.'"><img alt="'.$story_dept_title.'" src="'.$story_image.'"></a>
+				                </div>
+				                <div class="box-inner-p">
+				                    <div class="box-inner-ellipsis">
+				                        <div style="margin: 0px; padding: 0px; border: 0px;">
+				                            <h2 class="entry-title h3"><a href="get-wordpress-story.php?slug='.$story_slug.'">'.$story_dept_title.'</a></h2>
+				                        </div>
+				                    </div>
+				                    <div class="entry-meta">
+				                        <div class="meta-item meta-date">
+				                            <span class="updated department-news-story_date">'.$story_date.'</span>
+				                        </div>
+				                    </div>
+				                </div>
+				            </article>
+				        </div>
+				';
+
+	    	}
+	    }
+
+
+		$output .= '
+		    	</div>
+	    ';
+
+	    $pagination_out = '<nav class="gridlove-pagination">';
+
+	    $max_page = ($total_pages <= 8) ? $total_pages : (($total_pages - $current_page < 4) ? $total_pages : ($current_page + 4) );
+
+	    $min_page = ($current_page <= 4) ? 1 : ($current_page - 4);
+
+	    $next_page = ($current_page < $total_pages) ? ($current_page + 1) : 0;
+
+	    for($i=$min_page; $i<=$max_page; $i++) {
+	    	if($i == $current_page) {
+				$pagination_out .= '<span class="page-numbers current">'.$i.'</span>';
+	    	} else {
+	    		$pagination_out .= '<a class="page-numbers" href="?page='.$i.'&'.$api_query_per_page."&".$api_query_orderby."&".$api_query_order."&".$api_query_cat_label."&".$api_query_tag_label.'">'.$i.'</a>';
+	    	}
+	    }
+	    if($next_page > 0) {
+	    	$pagination_out .= '<a class="next page-numbers" href="?page='.$next_page.'&'.$api_query_per_page."&".$api_query_orderby."&".$api_query_order."&".$api_query_cat_label."&".$api_query_tag_label.'">Next</a>';
+	    }
+
+	    $pagination_out .= '</nav">';
+
+	    $output .= $pagination_out;
+		$output .= '
+		    </div>
+	    ';
+	}  else {
+
+		$output .= '
+			<div class="department-news-story">
+				<h1>No Stories Found</h1>
+				
+				<div class="department-news-story_content">
+					
+					<p><br>No search results found. Try going back, or visit SBU News to browse all news.</p>
+
+
+					<p class="clearfix"><a class="department-news-story_read-external" href="https://www.stonybrook.edu/happenings/" target="_blank">Browse SBU News</a></p>
+				</div>
+			</div>
+		';
+
     }
-
-
-	$output .= '
-	    	</div>
-    ';
-
-    $pagination_out = '<nav class="gridlove-pagination">';
-
-    $max_page = ($total_pages <= 8) ? $total_pages : (($total_pages - $current_page < 4) ? $total_pages : ($current_page + 4) );
-
-    $min_page = ($current_page <= 4) ? 1 : ($current_page - 4);
-
-    $next_page = ($current_page < $total_pages) ? ($current_page + 1) : 0;
-
-    for($i=$min_page; $i<=$max_page; $i++) {
-    	if($i == $current_page) {
-			$pagination_out .= '<span class="page-numbers current">'.$i.'</span>';
-    	} else {
-    		$pagination_out .= '<a class="page-numbers" href="?page='.$i.'&'.$api_query_per_page."&".$api_query_orderby."&".$api_query_order."&".$api_query_cat_label."&".$api_query_tag_label.'">'.$i.'</a>';
-    	}
-    }
-    if($next_page > 0) {
-    	$pagination_out .= '<a class="next page-numbers" href="?page='.$next_page.'&'.$api_query_per_page."&".$api_query_orderby."&".$api_query_order."&".$api_query_cat_label."&".$api_query_tag_label.'">Next</a>';
-    }
-
-    $pagination_out .= '</nav">';
-
-    $output .= $pagination_out;
-	$output .= '
-	    </div>
-    ';
 
     echo $output;
 
