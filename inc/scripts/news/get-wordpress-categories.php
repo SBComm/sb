fa57ad2a3dcfb19926d993036d66712cb6dde6f2
Josh Palmeri <palmerijosh@gmail.com>
2018-06-29 17:01:01 -0400

    Added category script

--- inc/scripts/news/get-wordpress-categories.php
+++ inc/scripts/news/get-wordpress-categories.php
@@ -1,34 +1,30 @@
 <?php
 
-	$wp_cats_url = "https://www.stonybrook.edu/happenings/wp-json/wp/v2/categories?parent=178&per_page=100";
+	//$api_url = "http://localhost/news/wp-json/wp/v2/categories?per_page=100";
+	$api_url = "https://www.stonybrook.edu/happenings/wp-json/wp/v2/categories?per_page=100&parent=197";
+
+	//  Initiate curl
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $wp_cats_url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-	//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-	//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-	//curl_setopt($ch, CURLOPT_CAINFO, "/usr/local/ssl/certs/cacert.pem");
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	$wp_cat_data = curl_exec($ch);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$api_url);
+	$result = curl_exec($ch);
 	curl_close($ch);
 
-	$wp_cat_data = json_decode($wp_cat_data);
+	$cats = json_decode($result);
 
-	$cats = $wp_cat_data;
+	//var_dump($cats); //this is for testing now
 
-	$html = '';
+	$cats_html = '';
 
 	foreach ($cats as $cat) {
-		$cat_name = str_replace('Magazine ','',$cat->name);
-		$cat_slug = $cat->description;
+		//$cat_name = str_replace('Magazine ','',$cat->name);
 
-		$html .= '
-			<span class="magazine-category-nav_link"><a href="/magazine/'.$cat_slug.'">'.$cat_name.'</a></span>
+		$cats_html .= '
+			<option value="'.$cat->slug.'">'.$cat->name.'</option>
 		';
 	}
 
-	echo $html;
-
-	$html = '';
+	echo $cats_html;
 
 ?>
