0cbf4c88537ba1e752b6dec27faf0c391884f056
Harbir Deol <harbir.deol@stonybrook.edu>
2018-10-29 14:13:32 -0400

    reverted back to bulletin

--- inc/scripts/get-academics-programs_grad.php
+++ inc/scripts/get-academics-programs_grad.php
@@ -76,7 +76,7 @@
 
 			$program_output .= "
 				<li>
-	                <a href=\"$program_url_details\">$program_title</a>
+	                <a href=\"$program_url_grad_bulletin\">$program_title</a>
 	                <div class=\"wider-badges-wrapper\">";
 
 	                foreach ($program->degrees->degree as $degree) {
