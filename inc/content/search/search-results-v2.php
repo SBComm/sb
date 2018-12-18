ae927d0d94be37e145c5445ba421b7ebbd910244
Harbir Deol <harbir.deol@stonybrook.edu>
2018-12-18 14:24:03 -0500

    updating url

--- inc/content/search/search-results-v2.php
+++ inc/content/search/search-results-v2.php
@@ -13,7 +13,7 @@
                     gcse.type = 'text/javascript';
                     gcse.async = true;
                     gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-                        '//www.google.com/cse/cse.js?cx=' + cx;
+                        '//cse.google.com/cse.js?cx=' + cx;
                     var s = document.getElementsByTagName('script')[0];
                     s.parentNode.insertBefore(gcse, s);
                     })();
