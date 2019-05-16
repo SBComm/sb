52f92f591293ee54c40803bb09db637427c02110
Harbir Deol <harbir.deol@stonybrook.edu>
2019-05-16 15:49:21 -0400

    working model, added sbumaster

--- inc/content/analytics/site-options.php
+++ inc/content/analytics/site-options.php
@@ -1,65 +1,83 @@
 <?php
-    function validateFolderName($string) {
-        $invalidTerms = array(
-            "old",
-            "OMNI-ASSETS",
-            "OMNI-RESOURCES",
-            "dont-use",
-            "-orig",
-            "test",
-            "hello"
-        );
-        
-        foreach ($invalidTerms as $invalidTerm){
-            /* contains invalid term */
-            if (strpos($string,$invalidTerm) !== false) {
-                return false;
-            }
-            /* starts with _ */
-            if (strpos($string,'_') === 0) {
-                return false;
+function validateFolderName($string)
+{
+    $invalidTerms = array(
+        "old",
+        "OMNI-ASSETS",
+        "OMNI-RESOURCES",
+        "dont-use",
+        "-orig",
+        "test",
+        "hello"
+    );
+    
+    foreach ($invalidTerms as $invalidTerm) {
+        /* contains invalid term */
+        if (strpos($string, $invalidTerm) !== false) {
+            return false;
+        }
+        /* starts with _ */
+        if (strpos($string, '_') === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+$directoryXML = 'http://www.stonybrook.edu/commcms/_data/folderListing.xml';
+$siteList     = simplexml_load_file($directoryXML);
+
+//var_dump($siteList);
+$k       = 0;
+$folders = array();
+foreach ($siteList->sbcms as $sbcms) {
+    foreach ($sbcms->list as $list) {
+        foreach ($list->directory as $dir) {
+            $dirString = (string) $dir;
+            if (validateFolderName($dirString)) {
+                $folders[$k] = $dirString;
+                echo ('<option id="commcms\/' . $folders[$k] . '">' . $folders[$k] . '</option>');
+                $k++;
             }
         }
-
-        return true;
     }
+}
 
-    $directoryXML = 'http://www.stonybrook.edu/commcms/_data/folderListing.xml';
-    $siteList = simplexml_load_file($directoryXML);
-    
-    //var_dump($siteList);
-    $k = 0;
-    $folders = array();
-    foreach ($siteList->sbcms as $sbcms){
-    	foreach ($sbcms->list as $list){
-	    	foreach ($list->directory as $dir){
-	        	$dirString = (string)$dir;
-	        	if(validateFolderName($dirString)) {
-	            	$folders[$k] = $dirString;
-	            	echo('<option id="commcms\/'.$folders[$k].'">'.$folders[$k].'</option>');
-	            	$k++;
-	        	}
-	    	}
-	   	}
-	}
-	
-	 foreach ($siteList->sbroot as $sbroot){
-    	foreach ($sbroot->list as $list){
-	    	foreach ($list->directory as $dir){
-	        	$dirString = (string)$dir;
-	        	if(validateFolderName($dirString)) {
-	            	$folders[$k] = $dirString;
-	            	echo('<option id="\/'.$folders[$k].'">'.$folders[$k].'</option>');
-	            	$k++;
-	        	}
-	    	}
-	   	}
-	}
-    asort($folders);
-    // foreach ($folders as $sbcms){
-        
-    // }
-    // foreach ($folders as $item){
-    //     echo('<option id="commcms\/'.$item.'">'.$item.'</option>');
-    // }
+foreach ($siteList->sbroot as $sbroot) {
+    foreach ($sbroot->list as $list) {
+        foreach ($list->directory as $dir) {
+            $dirString = (string) $dir;
+            if (validateFolderName($dirString)) {
+                $folders[$k] = $dirString;
+                echo ('<option id="\/' . $folders[$k] . '">' . $folders[$k] . '</option>');
+                $k++;
+            }
+        }
+    }
+}
+
+foreach ($siteList->sbumaster as $sbumaster) {
+    foreach ($sbumaster->list as $list) {
+        foreach ($list->directory as $dir) {
+            $dirString = (string) $dir;
+            if (validateFolderName($dirString)) {
+                $folders[$k] = $dirString;
+                echo ('<option id="\/' . $folders[$k] . '">' . $folders[$k] . '</option>');
+                $k++;
+            }
+        }
+    }
+}
+
+asort($folders);
+
+
+
+
+
+
+// foreach ($folders as $item){
+//     echo('<option id="commcms\/'.$item.'">'.$item.'</option>');
+// }
 ?>
