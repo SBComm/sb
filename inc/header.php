db0c77300e1264d4b43b40650fd98217994dd8b3
GB <gargi.bhowmick@stonybrook.edu>
2019-08-23 14:41:52 -0400

    test

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-       <!--Gargi checking here-->
+       
 
         <title><?php echo $page_title_full; ?></title>
 
