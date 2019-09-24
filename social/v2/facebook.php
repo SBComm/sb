d0b5be16c1539089e70d4f6f65e1fae54d9600c7
GB <gargi.bhowmick@stonybrook.edu>
2019-09-24 11:14:55 -0400

    Testing if this page is used

--- social/v2/facebook.php
+++ social/v2/facebook.php
@@ -1,4 +1,7 @@
 <?php
+
+echo'Gargi testing inc ';
+
 // SET YOUR FACEBOOK API DETAILS HERE
 $app_id 	= '1740688096184427';
 $app_secret = 'b9629d121450c3a3ce6ed93aa725f7c8';//'dfb5b4569fb74301a8487f7e3c5a14fb';
