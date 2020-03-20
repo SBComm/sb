b39dc462689d05c661ec97562a2f5ebe3910b819
GB <gargi.bhowmick@stonybrook.edu>
2020-03-20 11:05:32 -0400

    adding code for notes and prereqs for summer session

--- js/summer-session/summer-session-courses.js
+++ js/summer-session/summer-session-courses.js
@@ -50,4 +50,14 @@ $(document).ready(function() {
 		}
 	});
 
+	  $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+
 });
